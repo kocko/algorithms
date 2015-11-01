2e535a8bd0fe494070adff845f210385adec3c7c
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-11-01 18:14:43 +0200

    Solved Problem 12/A

--- src/codeforces/contests001_100/problemset012/SuperAgent.java
+++ src/codeforces/contests001_100/problemset012/SuperAgent.java
@@ -1,48 +1,54 @@
 package codeforces.contests001_100.problemset012;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-
-import static java.lang.Boolean.*;
 
 public class SuperAgent {
 
-	@SuppressWarnings("serial")
-	static final Map<Boolean, String> OUTCOMES = new HashMap<Boolean, String>() {
-		{
-			put(TRUE, "YES");
-			put(FALSE, "NO");
-		}
-	};
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String[] input = new String[3];
+		String[] password = new String[3];
 		for (int i = 0; i < 3; i++) {
-			input[i] = sc.nextLine();
+			password[i] = sc.nextLine();
 		}
-		System.out.println(isSymmetric(input));
+		if (isHorizontallySymmetric(password)
+				|| isVerticallySymmetric(password)
+				|| isDiagonallySymmetric(password)) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
 		sc.close();
 	}
-	
-	static String isSymmetric(String[] input) {
-		for (String s : input) {
-			if (s.charAt(0) != s.charAt(2)) {
-				return OUTCOMES.get(FALSE);
-			}
-		}
+
+	static boolean isHorizontallySymmetric(String[] password) {
 		for (int i = 0; i < 3; i++) {
-			if (input[0].charAt(0) != input[2].charAt(i)) {
-				return OUTCOMES.get(FALSE);
-			}
+			if (password[i].charAt(0) != password[i].charAt(2))
+				return false;
 		}
-		if (input[0].charAt(0) != input[2].charAt(2)) {
-			return OUTCOMES.get(FALSE);
-		}
-		if (input[0].charAt(2) != input[2].charAt(0)) {
-			return OUTCOMES.get(FALSE);
-		}
-		return OUTCOMES.get(TRUE);
+		if (password[0].charAt(1) != password[2].charAt(1))
+			return false;
+
+		return true;
 	}
+
+	static boolean isVerticallySymmetric(String[] password) {
+		for (int i = 0; i < 3; i++) {
+			if (password[0].charAt(i) != password[2].charAt(i))
+				return false;
+		}
+		if (password[1].charAt(0) != password[1].charAt(2))
+			return false;
+		return true;
+	}
+
+	static boolean isDiagonallySymmetric(String[] password) {
+		for (int i = 0; i < 3; i++) {
+			if (password[0].charAt(i) != password[2].charAt(2 - i))
+				return false;
+		}
+		if (password[1].charAt(0) != password[1].charAt(2))
+			return false;
+		return true;
+	}
+
 }
