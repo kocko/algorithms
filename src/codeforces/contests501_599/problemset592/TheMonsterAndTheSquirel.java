37a73094b42e84d2d299dfebfe4edab51bb60973
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-11-01 18:08:18 +0200

    Solved Problem 592/B

---
+++ src/codeforces/contests501_599/problemset592/TheMonsterAndTheSquirel.java
@@ -0,0 +1,14 @@
+package codeforces.contests501_599.problemset592;
+
+import java.util.Scanner;
+
+public class TheMonsterAndTheSquirel {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.println((n - 2) * (n - 2));
+		sc.close();
+	}
+	
+}
