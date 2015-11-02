05f1d4d174203e9510adaed8f9082ebf9b927140
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-11-02 13:15:56 +0200

    Solved 522/A

---
+++ src/codeforces/contests501_599/problemset522/PhotoToRemember.java
@@ -0,0 +1,8 @@
+package codeforces.contests501_599.problemset522;
+
+public class PhotoToRemember {
+
+	public static void main(String[] args) {
+		long l = 5000000000000000000l;
+	}
+}
---
+++ src/codeforces/contests501_599/problemset522/Reposts.java
@@ -0,0 +1,36 @@
+package codeforces.contests501_599.problemset522;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Reposts {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		Map<String, Integer> reposts = new HashMap<>();
+		reposts.put("polycarp", 1);
+		for (int i = 0; i < n; i++) {
+			String[] line = sc.nextLine().split("\\s+");
+			String author = line[2].toLowerCase();
+			String repost = line[0].toLowerCase();
+			if ("polycarp".equals(author)) {
+				reposts.put(repost, 2);
+			} else if (reposts.containsKey(author)) {
+				reposts.put(repost, reposts.get(author) + 1);
+			}
+		}
+
+		int chainSize = 0;
+		for (Map.Entry<String, Integer> entry : reposts.entrySet()) {
+			Integer value = entry.getValue();
+			if (value > chainSize) {
+				chainSize = value;
+			}
+		}
+		System.out.println(chainSize);
+		sc.close();
+	}
+
+}
