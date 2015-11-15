4d1fb906720b90045eb34ef66d7a008acbf066d3
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-11-15 17:57:42 +0200

    Solved 34D.

--- src/codeforces/contests001_100/problemset034/RoadMap.java
+++ src/codeforces/contests001_100/problemset034/RoadMap.java
@@ -1,61 +1,60 @@
 package codeforces.contests001_100.problemset034;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RoadMap { 
 
 	static int n;
-	static int[] parentMap;
 	static int[] newParentMap;
+	
 	static boolean[] visited;
-	int[][] x = new int[50000][50000];
+	static List<Integer>[] graph;
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		parentMap = new int[n + 1];
+		
 		newParentMap = new int[n + 1];
 		visited = new boolean[n + 1];
-
-		int root = sc.nextInt();
-		parentMap[root] = 0;
-
-		int newRoot = sc.nextInt();
+		
+		graph = new ArrayList[n + 1];
 		for (int i = 1; i <= n; i++) {
-			if (i != root) {
-				parentMap[i] = sc.nextInt();
-			} else {
-				parentMap[i] = -1;
-			}
+			graph[i] = new ArrayList<Integer>();
 		}
 
-		newParentMap[newRoot] = -1;
-		dfs(newRoot);
+		int root = sc.nextInt();
+		int newRoot = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			if (i != root) {
+				int c = sc.nextInt();
+				graph[i].add(c);
+				graph[c].add(i);
+			}
+		}
+		sc.close();
+		
+		dfs(newRoot, -1);
 
 		for (int i = 1; i <= n; i++) {
 			if (i != newRoot) {
 				System.out.print(newParentMap[i] + " ");
 			}
 		}
-		sc.close();
 	}
 
-	static void dfs(int vertice) {
-		for (int i = 1; i <= n; i++) {
-			if (parentMap[i] == vertice && !visited[i]) {
-				newParentMap[i] = vertice;
-				visited[i] = true;
-				dfs(i);
+	static void dfs(int vertice, int parent) {
+		newParentMap[vertice] = parent;
+		visited[vertice] = true;
+		for (int i = 0; i < graph[vertice].size(); i++) {
+			int x = graph[vertice].get(i);
+			if (!visited[x]) {
+				dfs(x, vertice);
 			}
 		}
-
-		int predcestor = parentMap[vertice];
-		if (predcestor > 0 && !visited[predcestor]) {
-			visited[predcestor] = true;
-			newParentMap[predcestor] = vertice;
-			dfs(predcestor);
-		}
-		
 	}
 
 }
