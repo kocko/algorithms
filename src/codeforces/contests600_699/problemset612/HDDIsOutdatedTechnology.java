7b4647728d4bcd562c5e5097bc0c1220e99cec62
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-12-25 20:25:54 +0200

    Solved 612/B.

---
+++ src/codeforces/contests600_699/problemset612/HDDIsOutdatedTechnology.java
@@ -0,0 +1,24 @@
+package codeforces.contests600_699.problemset612;
+
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class HDDIsOutdatedTechnology {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            f[sc.nextInt()] = i;
+        }
+        long result = 0;
+        for (int i = 2; i <= n; i++) {
+            result += abs(f[i] - f[i - 1]);
+        }
+        System.out.println(result);
+        sc.close();
+    }
+
+}
