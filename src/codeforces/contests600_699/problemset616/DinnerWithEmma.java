16d2c3a5c6fd0dde342461a0b0158fa9f007a22f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-12 11:15:56 +0200

    Solved 616/B.

---
+++ src/codeforces/contests600_699/problemset616/DinnerWithEmma.java
@@ -0,0 +1,24 @@
+package codeforces.contests600_699.problemset616;
+
+import java.util.Scanner;
+
+public class DinnerWithEmma {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                int next = sc.nextInt();
+                min = Math.min(next, min);
+            }
+            result = Math.max(result, min);
+        }
+        System.out.println(result);
+        sc.close();
+    }
+
+}
