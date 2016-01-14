c803050088c1a5a8cff6e0f7275d0c6b71fcb1f0
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-14 16:19:48 +0200

    Solved 510/A.

---
+++ src/codeforces/contests501_599/problemset510/FoxAndSnake.java
@@ -0,0 +1,37 @@
+package codeforces.contests501_599.problemset510;
+
+import java.util.Scanner;
+
+public class FoxAndSnake {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+        boolean first = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("#");
+                } else {
+                    if (first) {
+                        if (j == m - 1) {
+                            System.out.print("#");
+                        } else {
+                            System.out.print(".");
+                        }
+                    } else {
+                        if (j == 0) {
+                            System.out.print("#");
+                        } else {
+                            System.out.print(".");
+                        }
+                    }
+                }
+            }
+            if (i % 2 == 1) first = !first;
+            System.out.println();
+        }
+    }
+}
