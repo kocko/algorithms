e5e62289e4f7e1f1d61242574cce1ea88d5bc8e5
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-21 16:28:47 +0200

    Solved 246/B.

---
+++ src/codeforces/contests201_300/problemset246/IncreaseAndDecrease.java
@@ -0,0 +1,17 @@
+package codeforces.contests201_300.problemset246;
+
+import java.util.Scanner;
+
+public class IncreaseAndDecrease {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int total = 0;
+        for (int i = 0 ; i < n; i++) {
+            total += sc.nextInt();
+        }
+        System.out.println((total % n == 0) ? n : (n - 1));
+        sc.close();
+    }
+}
