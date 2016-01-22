28bda82547c4c4b7064cc5c781f87b2593a11331
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-22 12:00:23 +0200

    Minor fix

--- src/codeforces/contests600_699/problemset620/GrandfatherDovletsCalculator.java
+++ src/codeforces/contests600_699/problemset620/GrandfatherDovletsCalculator.java
@@ -1,30 +1,17 @@
 package codeforces.contests600_699.problemset620;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class GrandfatherDovletsCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>() {{
-            put(0, 6);
-            put(1, 2);
-            put(2, 5);
-            put(3, 5);
-            put(4, 4);
-            put(5, 5);
-            put(6, 6);
-            put(7, 3);
-            put(8, 7);
-            put(9, 6);
-        }};
+        int[] map = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
         long[] result = new long[1000001];
         for (int i = 1; i < result.length; i++) {
             String a = i + "";
             for (char c : a.toCharArray()) {
-                result[i] += map.get(c - '0');
+                result[i] += map[c - '0'];
             }
         }
         int a = sc.nextInt();
