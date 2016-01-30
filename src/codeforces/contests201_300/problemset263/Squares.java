9e9156de7291e7cefcacea7c86fb25d688b40551
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-31 00:06:10 +0200

    Solved 263/A. and 263/B.

---
+++ src/codeforces/contests201_300/problemset263/Squares.java
@@ -0,0 +1,77 @@
+package codeforces.contests201_300.problemset263;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Squares implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        int k = in.ni();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = in.ni();
+        Arrays.sort(a);
+        int result = -1;
+        if (n - k >= 0) {
+            result = a[n - k];
+        }
+        out.print(result);
+        if (result != -1) {
+            out.print(" " + 0);
+        }
+        out.println();
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Squares().solve();
+    }
+}
