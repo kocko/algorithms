b7898561804a680edd11a894f6e77212305d401d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-31 01:01:59 +0200

    Solved 527/A.

---
+++ src/codeforces/contests501_599/problemset527/PlayingWithPaper.java
@@ -0,0 +1,74 @@
+package codeforces.contests501_599.problemset527;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class PlayingWithPaper implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        long a = in.nl(), b = in.nl();
+        out.println(calculate(a, b));
+    }
+
+    long calculate(long a, long b) {
+        if (a == 0 || b == 0) return 0;
+        else {
+            long max = max(a, b), min = min(a, b);
+            return (max / min) + calculate(max % min, min);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new PlayingWithPaper().solve();
+    }
+}
