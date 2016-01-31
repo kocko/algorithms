176b0912c4b9e1cdc44e47fff948272b23398a16
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-31 20:37:06 +0200

    Solved 155A/.

---
+++ src/codeforces/contests101_200/problemset155/ILoveUsername.java
@@ -0,0 +1,77 @@
+package codeforces.contests101_200.problemset155;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ILoveUsername implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        int[] c = new int[n];
+        c[0] = in.ni();
+        int max = c[0], min = c[0], result = 0;
+        for (int i = 1; i < n; i++) {
+            c[i] = in.ni();
+            if (c[i] > max) {
+                result++;
+                max = c[i];
+            } else if (c[i] < min) {
+                min = c[i];
+                result++;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ILoveUsername().solve();
+    }
+}
