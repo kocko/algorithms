4895b7607accadc42fd77dedeb635235a0243bf9
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-01-31 14:36:56 +0200

    Solved 600/B.

--- src/codeforces/contests600_699/problemset600/QueriesAboutLessOrEqualElements.java
+++ src/codeforces/contests600_699/problemset600/QueriesAboutLessOrEqualElements.java
@@ -1,50 +1,108 @@
 package codeforces.contests600_699.problemset600;
 
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class QueriesAboutLessOrEqualElements {
+public class QueriesAboutLessOrEqualElements implements Closeable {
 
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] a = new int[n];
+    List<Integer> a;
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        a = new ArrayList<>(n);
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+            int next = in.ni();
+            a.add(next);
+            if (next > max) max = next;
+            if (next < min) min = next;
         }
-        Arrays.sort(a);
+        Collections.sort(a);
         for (int i = 0; i < m; i++) {
-            int b = sc.nextInt();
-            int count = findCount(a, b);
-            System.out.print(count + " ");
-        }
-        sc.close();
-        System.out.println();
-    }
-    
-    static int findCount(int[] a, int b) {
-        int index = a.length / 2;
-        return binary(a, b, index);
-    }
-    
-    static int binary(int[] a, int b, int index) {
-        int lo = 0;
-        int hi = a.length - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (a[mid] > b) {
-                if (mid == 0) return 0;
-                if (a[mid - 1] <= b) return mid;
-                else hi = mid - 1;
+            int b = in.ni();
+            int count;
+            if (b < min) {
+                count = 0;
+            } else if (b >= max) {
+                count = n;
             } else {
-                if (mid == a.length - 1) return mid + 1;
-                if (a[mid + 1] <= b) lo = mid + 1;
-                else return mid + 1;
+                count = binary(b);
+            }
+            out.print(count + " ");
+        }
+        out.println();
+    }
+
+    int binary(int b) {
+        int left = 0;
+        int right = a.size() - 1;
+        while (left < right - 1) {
+            int mid = (left + right) / 2;
+            if (a.get(mid) > b) {
+                right = mid;
+            } else {
+                left = mid;
             }
         }
-        return 0;
+        while (left < a.size() && a.get(left) <= b) {
+            left++;
+        }
+        return left;
     }
-    
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new QueriesAboutLessOrEqualElements().solve();
+    }
 }
