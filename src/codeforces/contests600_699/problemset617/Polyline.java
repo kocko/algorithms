3fda6a5b45616b6de34c4c67f8aebc4bed223904
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-02-01 16:08:20 +0200

    Solved 617/D.

--- src/codeforces/contests600_699/problemset617/Polyline.java
+++ src/codeforces/contests600_699/problemset617/Polyline.java
@@ -7,19 +7,25 @@ public class Polyline {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x1 = sc.nextInt(), y1 = sc.nextInt(),
-            x2 = sc.nextInt(), y2 = sc.nextInt(),
-            x3 = sc.nextInt(), y3 = sc.nextInt();
-        int a = parallel(x1, x2) + parallel(x2, x3) + parallel(x1, x3) +
-                parallel(y1, y2) + parallel(y2, y3) + parallel(y1, y3);
-        int result = 3;
-        if (a == 3) result = 1;
-        else if (a == 2) result = 2;
-        System.out.println(result);
+                x2 = sc.nextInt(), y2 = sc.nextInt(),
+                x3 = sc.nextInt(), y3 = sc.nextInt();
         sc.close();
+        int result = 1;
+        if ((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)) result = 1;
+        else
+            if ((x1 == x2 && !between(y3, y1, y2)) ||
+                (x2 == x3 && !between(y1, y3, y2)) ||
+                (x1 == x3 && !between(y2, y1, y3)) ||
+                (y1 == y2 && !between(x3, x1, x2)) ||
+                (y2 == y3 && !between(x1, x2, x3)) ||
+                (y3 == y1 && !between(x2, x1, x3))) result = 2;
+        else result = 3;
+        System.out.println(result);
     }
 
-    static int parallel(int m, int n) {
-        return (m == n) ? 1 : 0;
+    static boolean between(int a, int b, int c) {
+        int min = Math.min(b, c), max = Math.max(b, c);
+        return a > min && a < max;
     }
 
 }
