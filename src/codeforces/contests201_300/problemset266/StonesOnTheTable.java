3dfeca118c003027b2324d34b7ec7772ee66fd19
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-02-10 17:35:51 +0200

    Solved 266/A.

---
+++ src/codeforces/contests201_300/problemset266/StonesOnTheTable.java
@@ -0,0 +1,74 @@
+package codeforces.contests201_300.problemset266;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class StonesOnTheTable implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        char[] stones = in.next().toCharArray();
+        char last = stones[0];
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            if (stones[i] == last) {
+                result++;
+            } else {
+                last = stones[i];
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new StonesOnTheTable().solve();
+    }
+}
