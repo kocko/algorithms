8ea3047a522c15f9a21ca04c29de7e6ce3bf4a8f
kocko <konstantin.yovkov@gmail.com>
2016-02-11 23:41:35 +0200

    Solved 133/A.

---
+++ src/codeforces/contests101_200/problemset133/HQ9.java
@@ -0,0 +1,62 @@
+package codeforces.contests101_200.problemset133;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class HQ9 implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        String word = in.next();
+        if (word.contains("H") || word.contains("Q") || word.contains("9")) {
+            out.println("YES");
+        } else {
+            out.println("NO");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new HQ9().solve();
+    }
+}
