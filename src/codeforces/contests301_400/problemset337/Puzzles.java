30ef5e28d42bcc7407598f92d44ba073f17d5106
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-02-16 16:52:01 +0200

    Solved 337/A.

---
+++ src/codeforces/contests301_400/problemset337/Puzzles.java
@@ -0,0 +1,75 @@
+package codeforces.contests301_400.problemset337;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Puzzles implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int[] puzzles = new int[m];
+        for (int i = 0; i < puzzles.length; i++) {
+            puzzles[i] = in.ni();
+        }
+        Arrays.sort(puzzles);
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i <= m - n; i++) {
+            result = Math.min(result, puzzles[n + i - 1] - puzzles[i]);
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Puzzles().solve();
+    }
+}
