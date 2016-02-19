574026c358df4efe709dbfbc7a22fd726643790f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-02-19 17:25:27 +0200

    Solved 630/J and 630/K.

---
+++ src/codeforces/contests600_699/problemset630/Divisibility.scala
@@ -0,0 +1,13 @@
+package codeforces.contests600_699.problemset630
+
+import scala.io.StdIn
+
+object Divisibility {
+
+  def main(args: Array[String]) {
+    val n = StdIn.readLong()
+    val x = 2 * 3 * 2 * 5 * 7 * 2 * 3
+    print(n / x)
+  }
+
+}
