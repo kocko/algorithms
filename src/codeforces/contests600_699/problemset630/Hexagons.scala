5adf21be7e94a66fd2b07251c2c569374d943bce
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-02-19 16:11:25 +0200

    Solved 630/D.

---
+++ src/codeforces/contests600_699/problemset630/Hexagons.scala
@@ -0,0 +1,12 @@
+package codeforces.contests600_699.problemset630
+
+import scala.io.StdIn
+
+object Hexagons {
+
+  def main(args: Array[String]) {
+    val n = StdIn.readLong()
+    print(1 + 3 * n * (n + 1))
+  }
+  
+}
