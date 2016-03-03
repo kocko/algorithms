9b1c1daaeda3aaaaa7f4a981dfd211ef7fb9fa5e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-03-03 23:18:03 +0200

    Solved 631/A and 631/B.

---
+++ src/codeforces/contests600_699/problemset631/PrintCheck.java
@@ -0,0 +1,83 @@
+package codeforces.contests600_699.problemset631;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PrintCheck implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), k = in.ni();
+        int[] r = new int[n + 1], c = new int[m + 1], a = new int[k + 1];
+        for (int i = 1; i <= k; i++) {
+            int command = in.ni();
+            if (command == 1) {
+                int row = in.ni();
+                a[i] = in.ni();
+                r[row] = i;
+            } else {
+                int col = in.ni();
+                a[i] = in.ni();
+                c[col] = i;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int color = Math.max(r[i], c[j]);
+                out.print(a[color] + " ");
+            }
+            out.println();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new PrintCheck().solve();
+    }
+}
