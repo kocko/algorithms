976326063a66c2a8adb189dc9170376df0201ff5
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-03-14 14:03:30 +0200

    Solved a simple hash problem.

---
+++ src/hackerearth/datastructures/UniqueMaximumNumber.java
@@ -0,0 +1,81 @@
+package hackerearth.datastructures;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class UniqueMaximumNumber implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = in.ni();
+        }
+        int result = -1;
+        int[] used = new int[1000001];
+        int max = 0;
+        for (int i : list) {
+            used[i]++;
+            if (i > max) max = i;
+        }
+        for (int i = max; i >= 0; i--) {
+            if (used[i] == 1) {
+                result = i;
+                break;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new UniqueMaximumNumber().solve();
+    }
+}
