e388f75ad54014245e8e2c3324d62458346f80a2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-03-31 10:46:30 +0300

    Solved 659/C.

---
+++ src/codeforces/contests600_699/problemset659/TanyaAndToys.java
@@ -0,0 +1,89 @@
+package codeforces.contests600_699.problemset659;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+
+public class TanyaAndToys implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        Set<Integer> has = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            has.add(in.ni());
+        }
+        int total = 0;
+        List<Integer> result = new ArrayList<>();
+        int next = 1;
+        while (total + next <= m) {
+            if (!has.contains(next)) {
+                total += next;
+                result.add(next);
+            }
+            next++;
+        }
+        out.println(result.size());
+        for (Integer i : result) {
+            out.print(i + " ");
+        }
+        out.println();
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TanyaAndToys().solve();
+    }
+}
