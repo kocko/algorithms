b4d7dbc3c65ddffd33931af2122c4d74c5250b2a
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-04-04 14:34:48 +0300

    Solved 540/A.

---
+++ src/codeforces/contests501_599/problemset540/CombinationLock.java
@@ -0,0 +1,73 @@
+package codeforces.contests501_599.problemset540;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CombinationLock implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        String x = in.next(), y = in.next();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            char a = x.charAt(i), b = y.charAt(i);
+            if (a != b) {
+                int c = Math.min(a, b), d = Math.max(a, b);
+                result += Math.min(Math.abs(a - b), c + 10 - d);
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new CombinationLock().solve();
+    }
+}
