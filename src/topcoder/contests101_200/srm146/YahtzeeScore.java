b95ef684e0584c434f6ffe4e75e9576d1072072a
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-04-08 22:24:20 +0300

    Renamed a class.

---
+++ src/topcoder/contests101_200/srm146/YahtzeeScore.java
@@ -0,0 +1,17 @@
+package topcoder.contests101_200.srm146;
+
+public class YahtzeeScore {
+
+    public int maxPoints(int[] toss) {
+        int[] count = new int[7];
+        for (int i : toss) {
+            count[i]++;
+        }
+        int best = 0;
+        for (int i = 1; i < 7; i++) {
+            best = Math.max(count[i] * i, best);
+        }
+        return best;
+    }
+
+}
