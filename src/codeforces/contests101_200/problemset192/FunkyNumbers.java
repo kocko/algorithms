c51c86c2390571819927104a8a70fefd68a8eb07
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-04-29 19:29:55 +0300

    Solved 192/A.

---
+++ src/codeforces/contests101_200/problemset192/FunkyNumbers.java
@@ -0,0 +1,77 @@
+package codeforces.contests101_200.problemset192;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class FunkyNumbers implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        Set<Integer> set = new HashSet<>();
+        int[] seed = new int[60000];
+        for (int i = 1; i <= 60000; i++) {
+            set.add(seed[i - 1] = (int)((long) i * (i + 1) / 2));
+        }
+        for (int i = 0; i < 60000; i++) {
+            if (set.contains(n - seed[i])) {
+                out.println("YES");
+                return;
+            }
+        }
+        out.println("NO");  
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new FunkyNumbers().solve();
+    }
+}
