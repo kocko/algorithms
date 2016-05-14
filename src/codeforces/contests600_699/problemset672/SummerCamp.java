d7c2c42a6480778c09bf6020c45390cf337a752e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-05-14 17:01:26 +0300

    Solved 672/A.

---
+++ src/codeforces/contests600_699/problemset672/SummerCamp.java
@@ -0,0 +1,68 @@
+package codeforces.contests600_699.problemset672;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class SummerCamp implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        StringBuilder sb = new StringBuilder();
+        int next = 1;
+        while (sb.length() <= 1000) {
+            sb.append(String.valueOf(next++));
+        }
+        out.println(sb.toString().charAt(in.ni() - 1));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new SummerCamp().solve();
+    }
+}
