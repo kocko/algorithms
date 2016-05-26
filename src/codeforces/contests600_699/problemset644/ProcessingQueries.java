8735c261da1a0203f0545d4e18b211bfc9e59e8b
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-05-26 14:28:17 +0300

    Minor change.

--- src/codeforces/contests600_699/problemset644/ProcessingQueries.java
+++ src/codeforces/contests600_699/problemset644/ProcessingQueries.java
@@ -8,8 +8,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
