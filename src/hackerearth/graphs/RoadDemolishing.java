a6e8f54d882398091cf59bf56c092a1e9f40dbaa
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-05-29 18:15:22 +0300

    Solved a HackerRank problem with the Turan`s theorem.

---
+++ src/hackerearth/graphs/RoadDemolishing.java
@@ -0,0 +1,71 @@
+package hackerearth.graphs;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class RoadDemolishing implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            long n = in.ni(), q = in.ni() - 1;
+            long group_size = n / q;
+            long big_group = n % q;
+            long small_group = q - big_group;
+            long res = big_group * ((group_size * (group_size + 1)) / 2) + small_group * ((group_size * (group_size - 1)) / 2);
+            out.println(res);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new RoadDemolishing().solve();
+    }
+}
