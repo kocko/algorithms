d447cf7e56c2ba86368e56122d1b7694224c5647
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-05-29 23:52:39 +0300

    Solved a HackerEarth problem.

---
+++ src/hackerearth/math/modulus/BigValues.java
@@ -0,0 +1,71 @@
+package hackerearth.math.modulus;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BigValues implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        final BigInteger fraction = BigInteger.valueOf(720), MOD = BigInteger.valueOf((long)1e9 + 7);
+        BigInteger a = BigInteger.valueOf(1);
+        for (int i = n; i >= n - 5; i--) {
+            BigInteger b = BigInteger.valueOf(i);
+            a = (a.multiply(b));
+        }
+        out.println(a.divide(fraction).mod(MOD));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new BigValues().solve();
+    }
+}
