9da705eeed31068d2db3c9de5001239179f882ae
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-05-29 11:18:02 +0300

    Solved two simple problems from HackerEarth.

---
+++ src/hackerearth/graphs/EdgeExistence.java
@@ -0,0 +1,77 @@
+package hackerearth.graphs;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class EdgeExistence implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int[][] graph = new int[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = in.ni(), b = in.ni();
+            graph[a][b] = graph[b][a] = 1;
+        }
+        int q = in.ni();
+        for (int i = 0; i < q; i++) {
+            int a = in.ni(), b = in.ni();
+            if (graph[a][b] == 1) {
+                out.println("YES");
+            } else {
+                out.println("NO");
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new EdgeExistence().solve();
+    }
+}
---
+++ src/hackerearth/graphs/MonkInTheRealEstate.java
@@ -0,0 +1,80 @@
+package hackerearth.graphs;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MonkInTheRealEstate implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            int e = in.ni();
+            boolean[] has = new boolean[10001];
+            int result = 0;
+            for (int i = 0; i < e; i++) {
+                int a = in.ni(), b = in.ni();
+                if (!has[a]) {
+                    result++;
+                    has[a] = true;
+                }
+                if (!has[b]) {
+                    result++;
+                    has[b] = true;
+                }
+            }
+            out.println(result);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new MonkInTheRealEstate().solve();
+    }
+}
