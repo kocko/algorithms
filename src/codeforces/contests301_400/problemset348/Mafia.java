dc5f4f5ce574745f593acd77cab04ac09d0c66d8
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-06-03 13:49:03 +0300

    Solved 348/A.

---
+++ src/codeforces/contests301_400/problemset348/Mafia.java
@@ -0,0 +1,72 @@
+package codeforces.contests301_400.problemset348;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Mafia implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        int max = 0;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            max = Math.max(next, max);
+            sum += next;
+        }
+        int result = (int) Math.ceil((double) sum / (n - 1));
+        out.println(Math.max(max, result));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Mafia().solve();
+    }
+}
