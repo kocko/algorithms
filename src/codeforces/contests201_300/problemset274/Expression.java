92c095bbacd4049a6e2e8d2f834b51519069f645
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-06-14 17:42:55 +0300

    Solved 274/A.

---
+++ src/codeforces/contests201_300/problemset274/Expression.java
@@ -0,0 +1,69 @@
+package codeforces.contests201_300.problemset274;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.max;
+
+public class Expression implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int a = in.ni(), b = in.ni(), c = in.ni();
+        int x = a + b + c, y = a * b * c; int f = max(x, y);
+        int z = a + (b * c), k = (a + b) * c; int g = max(z, k);
+        int t = a * (b + c), m = (a * b) + c; int s = max(t, m);
+        out.println(max(f, max(g, s)));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Expression().solve();
+    }
+}
