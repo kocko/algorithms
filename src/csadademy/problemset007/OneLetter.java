7795fe16305fbc5d511d8af977ad88311550ac2d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-06-15 13:50:48 +0300

    Solved a CSAdademy problem.

---
+++ src/csadademy/problemset007/OneLetter.java
@@ -0,0 +1,78 @@
+package csadademy.problemset007;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class OneLetter implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+
+    public void solve() {
+        int n = in.ni();
+        int[] count = new int[26];
+        while (n-- > 0) {
+            String next = in.next();
+            char min = 'z';
+            for (char c : next.toCharArray()) {
+                if (c < min) min = c;
+            }
+            count[min - 'a']++;
+        }
+        for (int i = 0; i < count.length; i++) {
+            for (int k = 0; k < count[i]; k++) {
+                out.print((char)('a' + i));
+            }
+        }
+        out.println();
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new OneLetter().solve();
+    }
+}
