6ee457626a9e91e0b8b3e76e09e0d9dad82837c0
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-06-17 17:35:29 +0300

    Solved a HackerEarch problem.

---
+++ src/hackerearth/search/RestInPeace.java
@@ -0,0 +1,74 @@
+package hackerearth.search;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class RestInPeace implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        final String lives = "The streak lives still in our heart!", broken = "The streak is broken!";
+        while (n-- > 0) {
+            int next = in.ni();
+            String s = String.valueOf(next);
+            if (next % 21 != 0 && !s.contains("21")) {
+                out.println(lives);
+            } else {
+                out.println(broken);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (RestInPeace instance = new RestInPeace()) {
+            instance.solve();
+        }
+    }
+}
