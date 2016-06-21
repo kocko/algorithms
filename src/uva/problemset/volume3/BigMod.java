b52af05729a1267de4cb1d06498122f57d468cec
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-06-21 17:05:04 +0300

    Solved Volume3/374.

---
+++ src/uva/problemset/volume3/BigMod.java
@@ -0,0 +1,26 @@
+package uva.problemset.volume3;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BigMod {
+
+    int mod(int a, int b, int c) {
+        if (b == 0) return 1;
+        else if (b % 2 == 0) {
+            int x = mod (a, b / 2, c);
+            return (x * x) % c;
+        } else {
+            return (a % c * mod (a, b - 1, c)) % c;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int b = sc.nextInt(), p = sc.nextInt(), m = sc.nextInt();
+            System.out.println(new BigMod().mod(b, p, m));
+        }
+    }
+    
+}
