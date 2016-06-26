fe7040b3ddfc409ccb0763d75d9fc32d2a05ce8c
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-06-26 13:05:47 +0300

    Solved 378/A.

---
+++ src/codeforces/contests301_400/problemset378/PlayingWithDice.java
@@ -0,0 +1,76 @@
+package codeforces.contests301_400.problemset378;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PlayingWithDice implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int first = 0, draw = 0, second = 0;
+        for (int i = 1; i <= 6; i++) {
+            int x = Math.abs(n - i), y = Math.abs(m - i);
+            if (x < y) {
+                first++;
+            } else if (x == y) {
+                draw++;
+            } else {
+                second++;
+            }
+        }
+        out.println(first + " " + draw + " " + second);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PlayingWithDice instance = new PlayingWithDice()) {
+            instance.solve();
+        }
+    }
+}
