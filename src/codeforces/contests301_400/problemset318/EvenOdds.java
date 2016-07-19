88c971aa67dbda6a1fa6aeecad3d788a0be523fa
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-07-19 13:50:41 +0300

    Solved 318/A.

---
+++ src/codeforces/contests301_400/problemset318/EvenOdds.java
@@ -0,0 +1,69 @@
+package codeforces.contests301_400.problemset318;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class EvenOdds implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), k = in.nl();
+        if (2 * k - 1 <= n) {
+            out.println(2 * k - 1);
+        } else {
+            out.println(2 * (k - (n + 1) / 2));
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (EvenOdds instance = new EvenOdds()) {
+            instance.solve();
+        }
+    }
+}
