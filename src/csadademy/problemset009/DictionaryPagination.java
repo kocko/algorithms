50af06f3f96bb6b6701a32324408dd4b71427b47
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-07-26 23:44:30 +0300

    Solved 009/B.

---
+++ src/csadademy/problemset009/DictionaryPagination.java
@@ -0,0 +1,89 @@
+package csadademy.problemset009;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class DictionaryPagination implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), q = in.ni();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(in.next());
+        }
+        Collections.sort(list);
+        for (int i = 0; i < q; i++) {
+            String word = in.next();
+            int pages = in.ni();
+            int index = Collections.binarySearch(list, word);
+            if (pages >= n) {
+                out.println(index);
+            } else {
+                int rem = index % pages;
+                int result = 0;
+                if (rem != 0) {
+                    index -= rem;
+                    result = 1;
+                }
+                result += (index / pages);
+                out.println(result);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (DictionaryPagination instance = new DictionaryPagination()) {
+            instance.solve();
+        }
+    }
+}
