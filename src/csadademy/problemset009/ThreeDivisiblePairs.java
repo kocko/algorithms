765c47e32fa18a1770c00bb77ce2d5c0e55a37a5
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-07-26 23:44:22 +0300

    Solved 009/A.

---
+++ src/csadademy/problemset009/ThreeDivisiblePairs.java
@@ -0,0 +1,71 @@
+package csadademy.problemset009;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ThreeDivisiblePairs implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        long[] rem = new long[3];
+        for (int i = 0; i < n; i++) {
+            rem[in.ni() % 3]++;
+        }
+        long result = (rem[0] - 1) * (rem[0]) / 2;
+        result += rem[1] * rem[2];
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ThreeDivisiblePairs instance = new ThreeDivisiblePairs()) {
+            instance.solve();
+        }
+    }
+}
