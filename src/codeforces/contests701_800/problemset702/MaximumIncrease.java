c3542a38ff03f4968f5a15a49ece7785be6e61df
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-08-04 10:16:59 +0300

    Solved 702/A.

---
+++ src/codeforces/contests701_800/problemset702/MaximumIncrease.java
@@ -0,0 +1,77 @@
+package codeforces.contests701_800.problemset702;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MaximumIncrease implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int result = 0, current = 0;
+        int last = -1;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            if (next > last) {
+                current++;
+            } else {
+                result = Math.max(current, result);
+                current = 1;
+            }
+            last = next;
+        }
+        out.println(Math.max(result, current));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MaximumIncrease instance = new MaximumIncrease()) {
+            instance.solve();
+        }
+    }
+}
