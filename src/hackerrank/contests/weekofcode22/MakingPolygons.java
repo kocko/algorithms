256bc198bd5e70c3c0e34246e3144605dd4cd06b
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-08-09 11:00:23 +0300

    Solved WeekOfCode22/MakingPolygons.

---
+++ src/hackerrank/contests/weekofcode22/MakingPolygons.java
@@ -0,0 +1,83 @@
+package hackerrank.contests.weekofcode22;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class MakingPolygons implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        Arrays.sort(x);
+        if (n == 1) {
+            out.println(3);
+        } else if (n == 2) {
+            out.println(1);
+        } else if (n == 3) {
+            if (x[0] * x[0] + x[1] * x[1] != x[2] * x[2]) {
+                out.println(1);
+            } else {
+                out.println(0);
+            }
+        } else {
+            out.println(0);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MakingPolygons instance = new MakingPolygons()) {
+            instance.solve();
+        }
+    }
+}
