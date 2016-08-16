a51a749650c6188638ae8bfac3f4f5a3ab6e37d9
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-08-16 15:54:19 +0300

    Solved Strings/Z-Algorithm/CyclicPermutations.

---
+++ src/hackerearth/string/CyclicPermutations.java
@@ -0,0 +1,97 @@
+package hackerearth.string;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CyclicPermutations implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int tests = in.ni();
+        while (tests-- > 0) {
+            String a = in.next(), b = in.next();
+            char[] t = (a + b + b).toCharArray();
+            int n = t.length;
+            int[] z = new int[n];
+            int l = 0, r = 0, result = 0;
+            for (int i = 1; i < n; i++) {
+                if (i > r) {
+                    l = r = i;
+                    while (r < n && t[r - l] == t[r]) {
+                        r++;
+                    }
+                    z[i] = r - l; 
+                    r--;
+                } else {
+                    int k = i - l;
+                    if (z[k] < r - i + 1) {
+                        z[i] = z[k];
+                    } else {
+                        l = i;
+                        while (r < n && t[r - l] == t[r]) {
+                            r++;
+                        }
+                        z[i] = r - l;
+                        r--;
+                    }
+                }
+                if (i > n / 3 && z[i] >= n / 3) {
+                    result++;
+                }
+            }
+            out.println(result);
+        }
+    }
+    
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (CyclicPermutations instance = new CyclicPermutations()) {
+            instance.solve();
+        }
+    }
+}
