76ba58365e410a188133f5cd2535f53eddd8980e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-08-24 11:08:40 +0300

    Solved 624/B.

---
+++ src/codeforces/contests601_700/problemset624/MakingAString.java
@@ -0,0 +1,80 @@
+package codeforces.contests601_700.problemset624;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class MakingAString implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        Set<Integer> used = new HashSet<>();
+        long result = 0L;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            if (used.contains(next)) {
+                do {
+                    next--;
+                    if (next == 0) break;
+                } while (used.contains(next));
+            }
+            result += next;
+            used.add(next);
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MakingAString instance = new MakingAString()) {
+            instance.solve();
+        }
+    }
+}
