80e2ba4305859c0802cc644c11564e2446adbf67
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-08-25 10:02:13 +0300

    Solved 709/A.

---
+++ src/codeforces/contests701_800/problemset709/Juicer.java
@@ -0,0 +1,79 @@
+package codeforces.contests701_800.problemset709;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Juicer implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), b = in.ni(), d = in.ni();
+        int current = 0, result = 0;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            if (next <= b) {
+                current += next;
+            }
+            if (current > d) {
+                result++;
+                current = 0;
+            }
+        }
+        if (current > d) {
+            result++;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Juicer instance = new Juicer()) {
+            instance.solve();
+        }
+    }
+}
