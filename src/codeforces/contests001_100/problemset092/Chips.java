3c6c05a757053aa8cf1de85d7991c56eec73048e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-08-31 12:53:05 +0300

    Solved 092/A.

---
+++ src/codeforces/contests001_100/problemset092/Chips.java
@@ -0,0 +1,72 @@
+package codeforces.contests001_100.problemset092;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Chips implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int current = 1;
+        while (true) {
+            if (current > m) break;
+            m -= current;
+            current = (current + 1);
+            if (current == n + 1) current -= n;
+        }
+        out.println(m);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Chips instance = new Chips()) {
+            instance.solve();
+        }
+    }
+}
