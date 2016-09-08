4f917b28219cb35ae225ee38afdc86bb58a09af2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-09-08 18:53:20 +0300

    Solved 466/A.

---
+++ src/codeforces/contests401_500/problemset466/CheapTravel.java
@@ -0,0 +1,72 @@
+package codeforces.contests401_500.problemset466;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CheapTravel implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), a = in.ni(), b = in.ni();
+        if (m * a <= b) {
+            out.println(n * a);
+        } else {
+            int x = n / m;
+            int rem = n % m;
+            int result = x * b + Math.min(rem * a, b);
+            out.println(result);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (CheapTravel instance = new CheapTravel()) {
+            instance.solve();
+        }
+    }
+}
