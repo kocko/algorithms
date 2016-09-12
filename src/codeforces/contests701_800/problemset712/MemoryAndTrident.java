0950f9e654be15b43b18a61ff5e0efa92c06291f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-09-12 12:52:14 +0300

    Solved 712/B.

---
+++ src/codeforces/contests701_800/problemset712/MemoryAndTrident.java
@@ -0,0 +1,82 @@
+package codeforces.contests701_800.problemset712;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.abs;
+
+public class MemoryAndTrident implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] a = in.next().toCharArray();
+        int n = a.length;
+        if (n % 2 == 1) {
+            out.println(-1);
+        } else {
+            int x = 0, y = 0;
+            for (char c : a) {
+                switch (c) {
+                    case 'U': { y++; break; }
+                    case 'D': { y--; break; }
+                    case 'R': { x++; break; }
+                    case 'L': { x--; break; }
+                }
+            }
+            x = abs(x); y = abs(y);
+            out.println((x + y) >> 1);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MemoryAndTrident instance = new MemoryAndTrident()) {
+            instance.solve();
+        }
+    }
+}
