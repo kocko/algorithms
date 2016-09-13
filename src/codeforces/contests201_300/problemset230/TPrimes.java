98eae8bf9c5f2b801eb3f424a8be23d23d864a76
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-09-13 17:14:20 +0300

    Solved 230/A.

---
+++ src/codeforces/contests201_300/problemset230/TPrimes.java
@@ -0,0 +1,87 @@
+package codeforces.contests201_300.problemset230;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class TPrimes implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        for (int i = 0; i < n; i++) {
+            if (isTPrime(in.nl())) {
+                out.println("YES");
+            } else {
+                out.println("NO");
+            }
+        }
+    }
+
+    private boolean isTPrime(long n) {
+        long s = (long) Math.sqrt(n);
+        return s * s == n && isPrime(s);
+    }
+    
+    private boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2)
+            if (n % i == 0) return false;
+        return true;
+
+    }
+    
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TPrimes instance = new TPrimes()) {
+            instance.solve();
+        }
+    }
+}
