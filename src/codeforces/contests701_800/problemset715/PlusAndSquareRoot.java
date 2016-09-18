2007fde24b79caf2681fb155682c3e548f60e535
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-09-18 14:03:21 +0300

    Solved 715/A.

---
+++ src/codeforces/contests701_800/problemset715/PlusAndSquareRoot.java
@@ -0,0 +1,70 @@
+package codeforces.contests701_800.problemset715;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PlusAndSquareRoot implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl();
+        long x = 2;
+        for (long i = 1; i <= n; i++) {
+            long next = i * (i + 1);
+            out.println((next / i) * next - x / i);
+            x = next;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PlusAndSquareRoot instance = new PlusAndSquareRoot()) {
+            instance.solve();
+        }
+    }
+}
