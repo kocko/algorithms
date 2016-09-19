11c3271d5a9e82dadaac6a113a4fd98987ab3de6
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-09-19 16:17:21 +0300

    Solved WeekOfCode23/GearsOfWar.

---
+++ src/hackerrank/contests/weekofcode23/GearsOfWar.java
@@ -0,0 +1,67 @@
+package hackerrank.contests.weekofcode23;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class GearsOfWar implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        for (int i = 0; i < n; i++) {
+            out.println(in.ni() % 2 == 0 ? "Yes" : "No");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (GearsOfWar instance = new GearsOfWar()) {
+            instance.solve();
+        }
+    }
+}
