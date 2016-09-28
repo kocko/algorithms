8415707cbd865af59ae57758a4e6d28f795dc17b
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-09-28 17:39:10 +0300

    Solved a HackerEarth problem.

---
+++ src/hackerearth/basic/bitmanipulation/TheCastleGate.java
@@ -0,0 +1,76 @@
+package hackerearth.basic.bitmanipulation;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TheCastleGate implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            int n = in.ni();
+            int count = 0;
+            for (int i = 1; i < n; i++) {
+                for (int j = i + 1; j <= n; j++) {
+                    if ((i ^ j) <= n) {
+                        count++;
+                    }
+                }
+            }
+            out.println(count);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TheCastleGate instance = new TheCastleGate()) {
+            instance.solve();
+        }
+    }
+}
