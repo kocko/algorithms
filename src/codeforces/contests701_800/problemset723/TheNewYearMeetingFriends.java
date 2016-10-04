310cbca502b2378d70f0384b26d61bf9e90ca753
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-10-04 11:08:45 +0300

    Solved 723/A.

---
+++ src/codeforces/contests701_800/problemset723/TheNewYearMeetingFriends.java
@@ -0,0 +1,70 @@
+package codeforces.contests701_800.problemset723;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TheNewYearMeetingFriends implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int a = in.ni(), b = in.ni(), c = in.ni();
+        int min = 10000;
+        for (int i = 1; i <= 100; i++) {
+            int current = Math.abs(a - i) + Math.abs(b - i) + Math.abs(c - i);
+            min = Math.min(current, min);
+        }
+        out.println(min);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TheNewYearMeetingFriends instance = new TheNewYearMeetingFriends()) {
+            instance.solve();
+        }
+    }
+}
