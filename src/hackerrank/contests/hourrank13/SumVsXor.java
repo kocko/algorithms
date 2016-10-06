fc1a60d21bd9dbdb605223108ebc455479cef1d1
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-10-06 17:40:56 +0300

    Solved HourRank13/SumVsXor.

---
+++ src/hackerrank/contests/hourrank13/SumVsXor.java
@@ -0,0 +1,76 @@
+package hackerrank.contests.hourrank13;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class SumVsXor implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl();
+        if (n == 0) {
+            out.println(1);
+            return;
+        }
+        String binary = Long.toBinaryString(n);
+        long zeroes = 0L;
+        for (char c : binary.toCharArray()) {
+            if (c == '0') {
+                zeroes++;
+            }
+        }
+        out.println(1L << zeroes);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (SumVsXor instance = new SumVsXor()) {
+            instance.solve();
+        }
+    }
+}
