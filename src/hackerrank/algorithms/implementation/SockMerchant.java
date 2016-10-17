656981edda5cfcb570bd8bd2f476c07889acd2d9
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-10-17 11:59:28 +0300

    Solved a HackerRank problem.

---
+++ src/hackerrank/algorithms/implementation/SockMerchant.java
@@ -0,0 +1,75 @@
+package hackerrank.algorithms.implementation;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class SockMerchant implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] has = new int[101];
+        for (int i = 0; i < n; i++) {
+            has[in.ni()]++;
+        }
+        int result = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (has[i] > 0) {
+                result += has[i] / 2;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (SockMerchant instance = new SockMerchant()) {
+            instance.solve();
+        }
+    }
+}
