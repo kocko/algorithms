fba3e6c2ab7855facae9b541ea6f5cb977051a6f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-10-21 15:02:58 +0300

    Solved 731/B.

---
+++ src/codeforces/contests701_800/problemset731/CouponsAndDiscounts.java
@@ -0,0 +1,84 @@
+package codeforces.contests701_800.problemset731;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CouponsAndDiscounts implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        boolean ok = true;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 && x[i] % 2 == 1) {
+                ok = false;
+            } else if (i < n - 1) {
+                if (x[i] % 2 == 1) {
+                    if (x[i + 1] == 0) {
+                        ok = false;
+                        break;
+                    } else {
+                        x[i + 1]--;    
+                    }
+                }
+            }
+        }
+        out.println(ok ? "YES" : "NO");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (CouponsAndDiscounts instance = new CouponsAndDiscounts()) {
+            instance.solve();
+        }
+    }
+}
