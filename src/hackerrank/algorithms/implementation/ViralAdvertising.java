630413de93cc3824d5317a8db673bf108887c16f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-11-02 17:37:16 +0200

    Solved a HackerRank problem.

---
+++ src/hackerrank/algorithms/implementation/ViralAdvertising.java
@@ -0,0 +1,72 @@
+package hackerrank.algorithms.implementation;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ViralAdvertising implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int result = 0;
+        int green = 5;
+        for (int i = 1; i <= n; i++) {
+            green /= 2;
+            result += green;
+            green *= 3; 
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ViralAdvertising instance = new ViralAdvertising()) {
+            instance.solve();
+        }
+    }
+}
