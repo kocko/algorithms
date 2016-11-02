6c1f99ab512c2e4c1faac0c6dad5f8751ae65e07
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-11-02 19:09:04 +0200

    Solved a HackerRank problem.

---
+++ src/hackerrank/algorithms/string/TwoStrings.java
@@ -0,0 +1,80 @@
+package hackerrank.algorithms.string;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TwoStrings implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int q = in.ni();
+        while (q-- > 0) {
+            char[] a = in.next().toCharArray();
+            boolean[] x = new boolean[26];
+            for (char c : a) {
+                x[c - 'a'] = true;
+            }
+            char[] b = in.next().toCharArray();
+            boolean ok = false;
+            for (char c : b) {
+                if (x[c - 'a']) {
+                    ok = true;
+                    break;
+                }
+            }
+            out.println(ok ? "YES" : "NO");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TwoStrings instance = new TwoStrings()) {
+            instance.solve();
+        }
+    }
+}
