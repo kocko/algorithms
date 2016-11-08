221eaaee6bbf5eab239c08fa8972a204c1890feb
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-11-08 14:45:40 +0200

    Solved WeekofCode25/BabyStepGiantStep.

---
+++ src/hackerrank/contests/weekofcode25/BabyStepGiantStep.java
@@ -0,0 +1,81 @@
+package hackerrank.contests.weekofcode25;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BabyStepGiantStep implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int q = in.ni();
+        while (q-- > 0) {
+            int a = in.ni(), b = in.ni(), d = in.ni();
+            if (d == 0) {
+                out.println(0);
+            } else if (d == a || d == b) {
+                out.println(1);  
+            } else if (d < a || d < b) {
+                out.println(2);
+            } else {
+                int result = d / b;
+                if (d % b == 0) {
+                    out.println(result);
+                } else {
+                    out.println(result + 1);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BabyStepGiantStep instance = new BabyStepGiantStep()) {
+            instance.solve();
+        }
+    }
+}
