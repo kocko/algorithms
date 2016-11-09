7acb0b0af7fd62a02e2684f4b1ae75956ac1d2b7
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-11-09 16:53:33 +0200

    Solved 313/A.

---
+++ src/codeforces/contests301_400/problemset313/IlyaAndBankAccount.java
@@ -0,0 +1,73 @@
+package codeforces.contests301_400.problemset313;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class IlyaAndBankAccount implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        if (n == 0) {
+            out.println(0);
+        } else if (n > 0) {
+            out.println(n);
+        } else {
+            int x = (n - n % 10) / 10;
+            int y = ((n - n % 100) / 10) + n % 10;
+            out.println(Math.max(x, y));
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (IlyaAndBankAccount instance = new IlyaAndBankAccount()) {
+            instance.solve();
+        }
+    }
+}
