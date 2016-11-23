757fe99e7b44c69df99d52681b5bb3ed47357ff9
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-11-23 18:11:00 +0200

    Solved 439/B.

---
+++ src/codeforces/contests401_500/problemset439/DevuTheDumbGuy.java
@@ -0,0 +1,80 @@
+package codeforces.contests401_500.problemset439;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class DevuTheDumbGuy implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), x = in.nl();
+        List<Long> s = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            s.add(in.nl());
+        }
+        Collections.sort(s);
+        long result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            result += (s.get(i) * x);
+            if (x > 1) {
+                x--;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (DevuTheDumbGuy instance = new DevuTheDumbGuy()) {
+            instance.solve();
+        }
+    }
+}
