980cacdf14ff51cfafde84359592bd7fd5beb4a3
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-11-28 18:08:13 +0200

    Solved 467/B.

---
+++ src/codeforces/contests401_500/problemset467/FedorAndNewGame.java
@@ -0,0 +1,76 @@
+package codeforces.contests401_500.problemset467;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FedorAndNewGame implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), k = in.ni();
+        int[] x = new int[m];
+        for (int i = 0; i < m; i++) {
+            x[i] = in.ni();
+        }
+        int f = in.ni();
+        int count = 0;
+        for (int i : x) {
+            if (Integer.bitCount(f ^ i) <= k) {
+                count++;
+            }
+        }
+        out.println(count);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (FedorAndNewGame instance = new FedorAndNewGame()) {
+            instance.solve();
+        }
+    }
+}
