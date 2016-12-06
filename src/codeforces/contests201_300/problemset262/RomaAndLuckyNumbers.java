57edd32adeffb5606857b75276e5d500948212bc
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-12-06 12:21:50 +0200

    Solved 262/A.

---
+++ src/codeforces/contests201_300/problemset262/RomaAndLuckyNumbers.java
@@ -0,0 +1,74 @@
+package codeforces.contests201_300.problemset262;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class RomaAndLuckyNumbers implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        int count = 0;
+        while (n-- > 0) {
+            String next = in.next();
+            int p = 0;
+            for (char c : next.toCharArray()) {
+                if (c == '4' || c == '7') p++;
+            }
+            if (p <= k) count++;
+        }
+        out.println(count);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (RomaAndLuckyNumbers instance = new RomaAndLuckyNumbers()) {
+            instance.solve();
+        }
+    }
+}
