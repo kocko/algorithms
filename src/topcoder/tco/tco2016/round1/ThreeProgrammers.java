5dbac0d0b1d0d18cf6c0e9f7ac52c965d602eb57
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-12-06 16:01:05 +0200

    Solved TCO2016/Round1/ThreeProgrammers.

--- src/topcoder/tco/tco2016/round1/ThreeProgrammers.java
+++ src/topcoder/tco/tco2016/round1/ThreeProgrammers.java
@@ -4,77 +4,47 @@ public class ThreeProgrammers {
 
     public String validCodeHistory(String code) {
         char[] ch = code.toCharArray();
-        int a = 0, b = 0, c = 0;
-        int n = ch.length;
+        n = ch.length;
         for (char x : ch) {
             if (x == 'A') a++;
-            if (x == 'B') b++;
-            if (x == 'C') c++;
+            else if (x == 'B') b++;
+            else if (x == 'C') c++;
         }
-        if (3 * c - 2 > n) return "impossible";
-        if (2 * b - 1 > n) return "impossible";
-
-        char[] result = new char[n];
-        int next = 0;
-        if (c >= b) {
-            for (int i = 0; i < c; i++) {
-                if (next >= n) return "impossible";
-                else {
-                    result[next] = 'C';
-                    next += 3;
-                }
-            }
-            next = 0;
-            for (int i = 0; i < b; i++) {
-                if (next >= n) return "impossible";
-                else {
-                    if (result[next] == 'C') {
-                        i--;
-                        next++;
-                        continue;
-                    }
-                    result[next] = 'B';
-                    next += 2;
-                }
-            }
-        } else {
-            for (int i = 0; i < b; i++) {
-                if (next >= n) return "impossible";
-                else {
-                    result[next] = 'B';
-                    next += 2;
-                }
-            }
-            next = 0;
-            for (int i = 0; i < c; i++) {
-                if (next >= n) return "impossible";
-                else {
-                    if (result[next] == 'B') {
-                        i--;
-                        next++;
-                        continue;
-                    }
-                    result[next] = 'C';
-                    next += 3;
-                }
-            }
-        }
-
-        next = 0;
-        for (int i = 0; i < a; i++) {
-            if (result[next] == 'C' || result[next] == 'B') {
-                i--;
-                next++;
-                continue;
-            }
-            result[next] = 'A';
-            next++;
-        }
-        return new String(result);
+        this.dp = new boolean[a + 1][b + 1][c + 1][4][4];
+        result = "impossible";
+        recurse(0, 0, 0, 3, 3, "");
+        return result;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new ThreeProgrammers().validCodeHistory("CCCCCCCCCCCCCCCCCBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAA"));
+    private int a;
+    private int b;
+    private int c;
+    private boolean[][][][][] dp;
+    
+    private String result;
+    private boolean found;
+    private int n;
+    
+    private void recurse(int a, int b, int c, int prev, int pprev, String s) {
+        if (found) return;
+        dp[a][b][c][prev][pprev] = true;
+        int idx = a + b + c;
+        if (idx == n) {
+            result = s;
+            found = true;
+            return;
+        }
+        if (idx < n) {
+            if (a < this.a && !dp[a + 1][b][c][0][prev]) {
+                recurse(a + 1, b, c, 0, prev, s + "A");
+            }
+            if (b < this.b && prev != 1 && !dp[a][b + 1][c][1][prev]) {
+                recurse(a, b + 1, c, 1, prev, s + "B");
+            }
+            if (c < this.c && prev != 2 && pprev != 2 && !dp[a][b][c + 1][2][prev]) {
+                recurse(a, b, c + 1, 2, prev, s + "C");
+            }
+        }
     }
 
 }
