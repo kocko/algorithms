4fa539054e1e953d1ba0fe1ed458d9aeefb1e68c
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-12-07 14:01:35 +0200

    Solved 710/C.

---
+++ src/codeforces/contests701_800/problemset710/MagicOddSquare.java
@@ -0,0 +1,87 @@
+package codeforces.contests701_800.problemset710;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MagicOddSquare implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    private int[][] result;
+    public void solve() {
+        int n = in.ni();
+        result = new int[n][n];
+        int nextOdd = 1, nextEven = 2;
+        for (int i = 0; i < n; i++) {
+            int odds = 2 * Math.min(i, n - i - 1) + 1;
+            int evens = (n - odds) / 2;
+            for (int j = 0; j < evens; j++) {
+                result[i][j] = nextEven;
+                result[i][n - evens + j] = nextEven + 2;
+                nextEven += 4;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (result[i][j] == 0) {
+                    out.print(nextOdd + " ");
+                    nextOdd += 2;
+                } else {
+                    out.print(result[i][j] + " ");
+                }
+            }
+            out.println();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MagicOddSquare instance = new MagicOddSquare()) {
+            instance.solve();
+        }
+    }
+}
