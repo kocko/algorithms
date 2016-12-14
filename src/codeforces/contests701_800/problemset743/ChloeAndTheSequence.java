7302cf11ef5eb010e4c4ede418e8e2788f6c6525
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2016-12-14 19:18:31 +0200

    Solved 743/B.

---
+++ src/codeforces/contests701_800/problemset743/ChloeAndTheSequence.java
@@ -0,0 +1,77 @@
+package codeforces.contests701_800.problemset743;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ChloeAndTheSequence implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), k = in.nl();
+        out.println(recurse(n, k));
+    }
+
+    private long recurse(long n, long k) {
+        long x = 1L << (n - 1);
+        if (k == x) {
+            return n;
+        } else if (k % 2 == 1){
+            return 1;
+        } else {
+            long dist = Math.abs(k - x);
+            return recurse(n - 1, dist);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ChloeAndTheSequence instance = new ChloeAndTheSequence()) {
+            instance.solve();
+        }
+    }
+}
