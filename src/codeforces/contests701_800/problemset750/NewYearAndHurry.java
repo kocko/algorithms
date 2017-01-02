99de1fc31faca9d5951af464caf8e1e6818bb517
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-02 13:35:10 +0200

    Solved 750/A.

---
+++ src/codeforces/contests701_800/problemset750/NewYearAndHurry.java
@@ -0,0 +1,73 @@
+package codeforces.contests701_800.problemset750;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class NewYearAndHurry implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        int time = 0, count = 0, result = 0;
+        while (true) {
+            if (count == n) break;
+            if (time + 5 * (count + 1) + k > 240) break; 
+            time += 5 * (count + 1);
+            count++;
+            result++;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (NewYearAndHurry instance = new NewYearAndHurry()) {
+            instance.solve();
+        }
+    }
+}
