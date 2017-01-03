6db992c31562204c4852fefced7a2069f7005322
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-03 10:55:00 +0200

    Solved 753/A.

---
+++ src/codeforces/contests701_800/problemset753/SantaClausAndCandies.java
@@ -0,0 +1,78 @@
+package codeforces.contests701_800.problemset753;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class SantaClausAndCandies implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int t = 0;
+        while (n > t) {
+            t++;
+            n -= t;
+        }
+        out.println(t);
+        for (int i = 1; i < t; i++) {
+            out.print(i + " ");
+        }
+        out.println(n + t);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (SantaClausAndCandies instance = new SantaClausAndCandies()) {
+            instance.solve();
+        }
+    }
+}
