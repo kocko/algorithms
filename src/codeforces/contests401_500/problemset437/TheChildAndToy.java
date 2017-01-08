9274fe104cc6ddc21acd212ead53ff5ae9b5ae56
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-08 22:34:34 +0200

    Solved 437/C.

---
+++ src/codeforces/contests401_500/problemset437/TheChildAndToy.java
@@ -0,0 +1,74 @@
+package codeforces.contests401_500.problemset437;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class TheChildAndToy implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int[] v = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            v[i] = in.ni();
+        }
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            result += Math.min(v[in.ni()], v[in.ni()]);
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TheChildAndToy instance = new TheChildAndToy()) {
+            instance.solve();
+        }
+    }
+}
