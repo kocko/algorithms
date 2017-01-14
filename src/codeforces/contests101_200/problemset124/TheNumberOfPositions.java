838b4179342c266a34ec84084bed927589562a4c
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-15 00:50:12 +0200

    Solved 124/A.

---
+++ src/codeforces/contests101_200/problemset124/TheNumberOfPositions.java
@@ -0,0 +1,69 @@
+package codeforces.contests101_200.problemset124;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TheNumberOfPositions implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), a = in.ni(), b = in.ni();
+        int result = 0;
+        for (int i = a + 1; i <= n; i++) {
+            if (n - i <= b) result++;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TheNumberOfPositions instance = new TheNumberOfPositions()) {
+            instance.solve();
+        }
+    }
+}
