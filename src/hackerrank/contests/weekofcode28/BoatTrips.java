b15d7e8491e8da4f512a7eb5b46c211dcc58c1b8
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-15 23:38:53 +0200

    Solved WeekOfCode28/BoatTrips.

---
+++ src/hackerrank/contests/weekofcode28/BoatTrips.java
@@ -0,0 +1,71 @@
+package hackerrank.contests.weekofcode28;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BoatTrips implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), c = in.ni(), m = in.ni();
+        int max = m * c;
+        boolean can = true;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            can &= (next <= max);
+        }
+        out.println(can ? "Yes" : "No");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BoatTrips instance = new BoatTrips()) {
+            instance.solve();
+        }
+    }
+}
