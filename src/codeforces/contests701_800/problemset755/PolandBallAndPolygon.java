fc3a9b4acca72216c663c7d08b722df9fe2f6633
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-20 17:05:40 +0200

    Solved 755/D. with using a segment tree.

--- src/codeforces/contests701_800/problemset755/PolandBallAndPolygon.java
+++ src/codeforces/contests701_800/problemset755/PolandBallAndPolygon.java
@@ -13,23 +13,63 @@ public class PolandBallAndPolygon implements Closeable {
     private InputReader in = new InputReader(System.in);
     private PrintWriter out = new PrintWriter(System.out);
     
+    private class SegmentTree {
+        
+        private long[] tree;
+        private int n;
+        
+        private SegmentTree(int n) {
+            this.n = n;
+            this.tree = new long[n << 1];
+        }
+        
+        private void update(int p) {
+            for (tree[p += n] += 1L; p > 1; p >>= 1) {
+                tree[p >> 1] = tree[p] + tree[p ^ 1];
+            }
+        }
+        
+        private long query(int left, int right) {
+            long result = 0L;
+            for (left += n, right += n; left < right; left >>= 1, right >>= 1) {
+                if ((left & 1) == 1) result += tree[left++];
+                if ((right & 1) == 1) result += tree[--right];
+            }
+            return result;
+        }
+    }
+    
     public void solve() {
         int n = in.ni(), k = in.ni();
-        long p = 1;
-        long d = 0;
-        k = Math.min(k, n - k);
-        int a = 0;
-        for (int i = 0; i < n; i++) {
-            if (a + k > n) {
-                d += 2L;
-                p += d;
-                a = (a + k) % n;
-            } else {
-                p += d + 1;
-                a += k;
-            }
-            out.print(p + " ");
+        long[] result = new long[n];
+        if (k > n / 2) {
+            k = n - k;
         }
+        int a = 1;
+        int b = (a + k) > n ? a + k - n : (a + k);
+        result[0] = 2;
+        SegmentTree tree = new SegmentTree(n + 1);
+        tree.update(a);
+        tree.update(b);
+        for (int i = 1; i < n; i++) {
+            a = b;
+            b = (a + k) > n ? a + k - n : (a + k);
+            result[i] = result[i - 1] + 1L;
+            long d = 0L;
+            if (a > b) {
+                d += tree.query(a + 1, n + 1);
+                d += tree.query(0, b);
+            } else {
+                d += tree.query(a + 1, b + 1);
+            }
+            result[i] += d;
+            tree.update(a); 
+            tree.update(b);
+        }
+        for (long i : result) {
+            out.print(i + " ");
+        }
+
     }
 
     @Override
