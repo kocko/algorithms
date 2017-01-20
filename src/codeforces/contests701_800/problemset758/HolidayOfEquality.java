70aa74a0e00017e76f930f19dd908667a7963624
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-20 11:25:29 +0200

    Solved 758/A.

---
+++ src/codeforces/contests701_800/problemset758/HolidayOfEquality.java
@@ -0,0 +1,71 @@
+package codeforces.contests701_800.problemset758;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class HolidayOfEquality implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int s = 0, max = 0;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            s += next;
+            if (next > max) max = next;
+        }
+        out.println(n * max - s);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (HolidayOfEquality instance = new HolidayOfEquality()) {
+            instance.solve();
+        }
+    }
+}
