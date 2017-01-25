4b03b64ce92a06106cd0ed1dc5d611fbb425a2da
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-01-25 14:41:58 +0200

    Solved 343/B.

---
+++ src/codeforces/contests301_400/problemset343/AlternatingCurrent.java
@@ -0,0 +1,76 @@
+package codeforces.contests301_400.problemset343;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class AlternatingCurrent implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : x) {
+            if (!stack.isEmpty()) {
+                if (stack.peek().equals(c)) {
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(c);
+        }
+        out.println(stack.isEmpty() ? "Yes" : "No");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (AlternatingCurrent instance = new AlternatingCurrent()) {
+            instance.solve();
+        }
+    }
+}
