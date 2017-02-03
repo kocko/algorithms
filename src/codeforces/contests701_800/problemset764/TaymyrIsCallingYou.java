81fb241145866d926914a32dcd54c836a18cd28f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-03 11:11:53 +0200

    Solved 764/A.

---
+++ src/codeforces/contests701_800/problemset764/TaymyrIsCallingYou.java
@@ -0,0 +1,79 @@
+package codeforces.contests701_800.problemset764;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TaymyrIsCallingYou implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), z = in.ni();
+        boolean[] busy = new boolean[z + 1];
+        int t = 1;
+        while (t * n <= z) {
+            busy[t * n] = true;
+            t++;
+        }
+        int result = 0;
+        t = 1;
+        while (t * m <= z) {
+            if (busy[t * m]) {
+                result++;
+            }
+            t++;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TaymyrIsCallingYou instance = new TaymyrIsCallingYou()) {
+            instance.solve();
+        }
+    }
+}
