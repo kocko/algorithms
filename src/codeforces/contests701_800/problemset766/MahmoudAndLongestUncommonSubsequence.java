b98effe61eb92dcb4e68017e928d07976c70b706
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-15 15:41:00 +0200

    Solved 766/A.

---
+++ src/codeforces/contests701_800/problemset766/MahmoudAndLongestUncommonSubsequence.java
@@ -0,0 +1,66 @@
+package codeforces.contests701_800.problemset766;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MahmoudAndLongestUncommonSubsequence implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        String a = in.next(), b = in.next();
+        int result = a.equals(b) ? -1 : Math.max(a.length(), b.length());
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MahmoudAndLongestUncommonSubsequence instance = new MahmoudAndLongestUncommonSubsequence()) {
+            instance.solve();
+        }
+    }
+}
