afba93a7b4ea62a46f1d1c6fbe6acfc3a358436d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-02-26 13:32:01 +0200

    Solved 779/B.

---
+++ src/codeforces/contests701_800/problemset779/WeirdRounding.java
@@ -0,0 +1,83 @@
+package codeforces.contests701_800.problemset779;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class WeirdRounding implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        int n = x.length;
+        int k = in.ni();
+        int zeroes = 0;
+        int delete = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (x[i] == '0') {
+                zeroes++;
+            } else {
+                delete++;
+            }
+            if (zeroes == k) {
+                break;
+            }
+        }
+        if (zeroes < k) {
+            out.println(n - 1);
+        } else {
+            out.println(delete);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (WeirdRounding instance = new WeirdRounding()) {
+            instance.solve();
+        }
+    }
+}
