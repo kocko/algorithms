b0fcdd93f18291735b8fa58ddecfd201cf263f72
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-03-06 10:12:35 +0200

    Solved 782/A.

---
+++ src/codeforces/contests701_800/problemset782/AndryushaAndSocks.java
@@ -0,0 +1,76 @@
+package codeforces.contests701_800.problemset782;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AndryushaAndSocks implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), max = 0, current = 0;
+        boolean[] stack = new boolean[n + 1];
+        for (int i = 0; i < 2 * n; i++) {
+            int next = in.ni();
+            if (stack[next]) {
+                current--;
+            } else {
+                current++;
+                max = Math.max(current, max);
+            }
+            stack[next] = !stack[next];
+        }
+        out.println(max);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (AndryushaAndSocks instance = new AndryushaAndSocks()) {
+            instance.solve();
+        }
+    }
+}
