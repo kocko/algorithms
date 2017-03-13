a93e662473eaa2bdece0fb928d6aa6fc9494dfdb
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-03-13 17:32:39 +0200

    Solved 427/B.

---
+++ src/codeforces/contests401_500/problemset427/PrisonTransfer.java
@@ -0,0 +1,86 @@
+package codeforces.contests401_500.problemset427;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class PrisonTransfer implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), t = in.ni(), c = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        int size = 0, result = 0;
+        for (int i = 0; i < n; i++) {
+            int current = x[i];
+            if (current > t) {
+                if (size == c) result++;
+                size = 0;
+            } else {
+                size++;
+                if (size == c + 1) {
+                    result++;
+                    size--;
+                }
+            }
+        }
+        if (size == c) result++;
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PrisonTransfer instance = new PrisonTransfer()) {
+            instance.solve();
+        }
+    }
+}
