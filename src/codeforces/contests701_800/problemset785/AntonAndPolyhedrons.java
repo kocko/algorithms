c07d98a9c5a07a40a01a4347d0d3b2cba9dd6aaa
kocko <konstantin.yovkov@gmail.com>
2017-03-15 21:29:35 +0200

    Solved 785/A.

---
+++ src/codeforces/contests701_800/problemset785/AntonAndPolyhedrons.java
@@ -0,0 +1,77 @@
+package codeforces.contests701_800.problemset785;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AntonAndPolyhedrons implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            String p = in.next();
+            switch (p) {
+                case "Tetrahedron": result += 4; break;
+                case "Cube": result += 6; break;
+                case "Octahedron": result += 8; break;
+                case "Dodecahedron": result += 12; break;
+                case "Icosahedron": result += 20; break;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (AntonAndPolyhedrons instance = new AntonAndPolyhedrons()) {
+            instance.solve();
+        }
+    }
+}
