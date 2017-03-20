f1c48cdec1cf0c21798c1ba1d034bd941132937b
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-03-20 11:25:01 +0200

    Solved WeekOfCode30/FindTheMinimumNumber

---
+++ src/hackerrank/contests/weekofcode30/FindTheMinimumNumber.java
@@ -0,0 +1,73 @@
+package hackerrank.contests.weekofcode30;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FindTheMinimumNumber implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        out.println(recurse(n, "int"));
+    }
+    
+    private String recurse(int n, String a) {
+        if (n > 1) {
+            return recurse(n - 1, "min(int, " + a + ")");
+        } else {
+            return a;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (FindTheMinimumNumber instance = new FindTheMinimumNumber()) {
+            instance.solve();
+        }
+    }
+}
