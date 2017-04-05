bc671fcab6cd818f5d66594e7e011f8b1656ed22
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-04-05 17:33:26 +0300

    Solved 546/B.

---
+++ src/codeforces/contests501_600/problemset546/SoldierAndBadges.java
@@ -0,0 +1,78 @@
+package codeforces.contests501_600.problemset546;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SoldierAndBadges implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] cnt = new int[6005];
+        for (int i = 0; i < n; i++) {
+            cnt[in.ni()]++;
+        }
+        int result = 0;
+        for (int i = 1; i <= 2 * n; i++) {
+            if (cnt[i] > 1) {
+                int diff = (cnt[i] - 1);
+                result += diff;
+                cnt[i + 1] += diff;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (SoldierAndBadges instance = new SoldierAndBadges()) {
+            instance.solve();
+        }
+    }
+}
