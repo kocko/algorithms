85f53aeed2b3b2a5e7517c58f6918e21a5784f02
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-04-07 10:18:52 +0300

    Solved 463/B.

---
+++ src/codeforces/contests401_500/problemset463/CaisaAndPylons.java
@@ -0,0 +1,75 @@
+package codeforces.contests401_500.problemset463;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CaisaAndPylons implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), last = 0, result = 0;
+        int energy = 0;
+        for (int i = 1; i <= n; i++) {
+            int current = in.ni();
+            energy += (last - current);
+            if (energy < 0) {
+                result += -energy;
+                energy = 0;
+            }
+            last = current;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (CaisaAndPylons instance = new CaisaAndPylons()) {
+            instance.solve();
+        }
+    }
+}
