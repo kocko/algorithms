0b8d2632c461a7d27e14644cf3931450d5e94256
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-04-11 10:25:00 +0300

    Solved 796/B.

---
+++ src/codeforces/contests701_800/problemset796/FindTheBone.java
@@ -0,0 +1,80 @@
+package codeforces.contests701_800.problemset796;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FindTheBone implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), k = in.ni();
+        boolean[] holes = new boolean[n + 1];
+        for (int i = 0; i < m; i++) {
+            holes[in.ni()] = true;
+        }
+        int pos = 1;
+        for (int i = 0; i < k; i++) {
+            if (holes[pos]) break;
+            int u = in.ni(), v = in.ni();
+            if (u != pos && v != pos) continue;
+            if (u == pos) {
+                pos = v;
+            } else {
+                pos = u;
+            }
+        }
+        out.println(pos);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (FindTheBone instance = new FindTheBone()) {
+            instance.solve();
+        }
+    }
+}
