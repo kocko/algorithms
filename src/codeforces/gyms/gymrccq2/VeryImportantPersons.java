725d21d7294dc485b3a9aafc8f3a677a266e5511
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-04-16 14:45:30 +0300

    Solved RussianCodeCup2017/QualificationRound2/A.

---
+++ src/codeforces/gyms/gymrccq2/VeryImportantPersons.java
@@ -0,0 +1,95 @@
+package codeforces.gyms.gymrccq2;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class VeryImportantPersons implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            int n = in.ni(), m = in.ni();
+            int[][] matrix = new int[n][m];
+
+            int number = 0;
+
+            for (int i = 0; number < matrix.length * matrix[0].length; i++) {
+                int row = 0;
+                int col = i;
+
+                do {
+                    if (row < matrix.length && col < matrix[row].length) {
+                        matrix[row][col] = m * n - number;
+                        number++;
+                    }
+
+                    row++;
+                    col--;
+                } while (row >= 0 && col >= 0);
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    out.print(matrix[i][j]);
+                    out.print(' ');
+                }
+                out.println();
+            }
+        }
+
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (VeryImportantPersons instance = new VeryImportantPersons()) {
+            instance.solve();
+        }
+    }
+}
