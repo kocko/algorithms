f9bcf5d13df2f67764871476c8ac0c18c9c94fe1
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-05-21 10:19:09 +0300

    Solved 810/C.

--- src/codeforces/contests801_900/problemset810/DoYouWantADate.java
+++ src/codeforces/contests801_900/problemset810/DoYouWantADate.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import static java.util.Arrays.sort;
@@ -22,13 +25,13 @@ public class DoYouWantADate implements Closeable {
     public void solve() {
         init();
         int n = in.ni();
-        int x[] = new int[n + 1];
-        for (int i = 1; i <= n; i++) x[i] = in.ni();
-        sort(x, 1, n + 1);
+        List<Integer> x = new ArrayList<>();
+        for (int i = 1; i <= n; i++) x.add(in.ni());
+        Collections.sort(x);
         long result = 0;
         for (int i = 1; i <= n; i++) {
-            long a = (pow[i - 1] * x[i]) % MOD;
-            long b = (pow[n - i] * x[i]) % MOD;
+            long a = (pow[i - 1] * x.get(i - 1)) % MOD;
+            long b = (pow[n - i] * x.get(i - 1)) % MOD;
             result += (a - b + MOD) % MOD;
             result %= MOD;
         }
