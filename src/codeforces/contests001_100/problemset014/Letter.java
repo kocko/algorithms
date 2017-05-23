b7524f1ad25dd68488f88a1ded259723a9416a04
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-05-23 11:46:38 +0300

    Solved 014/A.

---
+++ src/codeforces/contests001_100/problemset014/Letter.java
@@ -0,0 +1,83 @@
+package codeforces.contests001_100.problemset014;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Letter implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int minX = n + 1, maxX = -1, minY = m + 1, maxY = -1;
+        char[][] grid = new char[n][m];
+        for (int x = 0; x < n; x++) {
+            grid[x] = in.next().toCharArray();
+            for (int y = 0; y < m; y++) {
+                if (grid[x][y] == '*') {
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
+                }
+            }
+        }
+        for (int i = minX; i <= maxX; i++) {
+            for (int j = minY; j <= maxY; j++) {
+                out.print(grid[i][j]);
+            }
+            out.println();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Letter instance = new Letter()) {
+            instance.solve();
+        }
+    }
+}
