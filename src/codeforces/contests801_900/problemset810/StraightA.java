31f3225990c2d9c74fa74b90ef755f40f9058f2e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-05-25 12:20:16 +0300

    Solved 810/A.

---
+++ src/codeforces/contests801_900/problemset810/StraightA.java
@@ -0,0 +1,78 @@
+package codeforces.contests801_900.problemset810;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class StraightA implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += in.ni();
+        }
+        int count = 0;
+        double avg = total / (double) n;
+        while (Math.round(avg) < k) {
+            total += k;
+            n++;
+            avg = total / (double) n;
+            count++;
+        }
+        out.println(count);
+    }
+    
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (StraightA instance = new StraightA()) {
+            instance.solve();
+        }
+    }
+}
