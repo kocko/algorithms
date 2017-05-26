0fb82a42861f4fd63659eea9e50044da10a13c3b
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-05-26 19:51:17 +0300

    Solved 417/C.

---
+++ src/codeforces/contests401_500/problemset417/Football.java
@@ -0,0 +1,76 @@
+package codeforces.contests401_500.problemset417;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Football implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        if (2 * k >= n) {
+            out.println(-1);
+            return;
+        }
+        out.println(n * k);
+        for (int a = 1; a <= n; a++) {
+            for (int j = 1; j <= k; j++) {
+                int b = a + j;
+                if (b > n) b -= n;
+                out.println(a + " " + b);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Football instance = new Football()) {
+            instance.solve();
+        }
+    }
+}
