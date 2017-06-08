3c3d5e0fa16e93ad1d958f6e635c35f06acb7758
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-06-08 17:17:25 +0300

    Solved 379/B.

---
+++ src/codeforces/contests301_400/problemset379/NewYearPresent.java
@@ -0,0 +1,84 @@
+package codeforces.contests301_400.problemset379;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class NewYearPresent implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+    
+    public void solve() {
+        final String PUT = "P", LEFT = "L", RIGHT = "R";
+        int n = in.ni();
+        for (int i = 0; i < n; i++) {
+            int next = in.ni(), cnt = 0;
+            while (cnt < next) {
+                out.print(PUT);
+                cnt++;
+                if (cnt + 1 <= next) {
+                    if (i < n - 1) {
+                        out.print(RIGHT);
+                        out.print(LEFT);
+                    } else {
+                        out.print(LEFT);
+                        out.print(RIGHT);
+                    }
+                }
+            }
+            if (i < n - 1) {
+                out.print(RIGHT);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (NewYearPresent instance = new NewYearPresent()) {
+            instance.solve();
+        }
+    }
+}
