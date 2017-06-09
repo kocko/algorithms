69ae5b53634f3b7e7989e8c6bb24afd667d65cf8
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-06-09 13:46:22 +0300

    Solved 276/A.

---
+++ src/codeforces/contests201_300/problemset276/LunchRush.java
@@ -0,0 +1,77 @@
+package codeforces.contests201_300.problemset276;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LunchRush implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        long k = in.nl();
+        long max = -((long) 1e10); 
+        for (int i = 0; i < n; i++) {
+            long f = in.nl(), t = in.nl();
+            long joy;
+            if (t > k) {
+                joy = f - (t - k);
+            } else {
+                joy = f;
+            }
+            max = Math.max(max, joy);
+        }
+        out.println(max);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LunchRush instance = new LunchRush()) {
+            instance.solve();
+        }
+    }
+}
