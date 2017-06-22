a03432d087e090a80615a91e9d2edae9f411197c
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-06-22 12:01:04 +0300

    Solved 252/A.

---
+++ src/codeforces/contests201_300/problemset252/LittleXor.java
@@ -0,0 +1,79 @@
+package codeforces.contests201_300.problemset252;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LittleXor implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        int max = 0;
+        for (int start = 0; start < n; start++) {
+            for (int end = start; end < n; end++) {
+                int xor = 0;
+                for (int i = start; i <= end; i++) {
+                    xor ^= x[i];
+                }
+                if (xor > max) max = xor;
+            }
+        }
+        out.println(max);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LittleXor instance = new LittleXor()) {
+            instance.solve();
+        }
+    }
+}
