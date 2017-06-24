37ec572c678b3efaf01e5be479688858ade950f5
kocko <konstantin.yovkov@gmail.com>
2017-06-25 01:55:32 +0300

    Solved Volume03/Flags.

---
+++ src/timus/volume03/Flags.java
@@ -0,0 +1,74 @@
+package timus.volume03;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Flags implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        dp = new long[n + 1];
+        out.println(recurse(n));
+    }
+
+    private long dp[];
+    private long recurse(int n) {
+        if (dp[n] != 0) return dp[n];
+
+        if (n <= 2) return 2;
+        return dp[n] = (recurse(n - 1) + recurse(n - 2));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Flags instance = new Flags()) {
+            instance.solve();
+        }
+    }
+}
