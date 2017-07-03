c9c9aec5f9ca003edd237f5dd2e19de6fced422a
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-07-03 17:28:52 +0300

    Solved 035/RefrigeratorLetters.

---
+++ src/csadademy/problemset035/RefrigeratorLetters.java
@@ -0,0 +1,78 @@
+package csadademy.problemset035;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class RefrigeratorLetters implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt[in.next().charAt(0) - 'a']++;
+        }
+        char[] target = in.next().toCharArray();
+        int result = 0;
+        for (char c : target) {
+            if (cnt[c - 'a'] > 0) {
+                cnt[c - 'a']--;
+            } else {
+                result++;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (RefrigeratorLetters instance = new RefrigeratorLetters()) {
+            instance.solve();
+        }
+    }
+}
