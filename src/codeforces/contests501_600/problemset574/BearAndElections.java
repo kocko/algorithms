ea8f3212e08fd73418788b48c6b123d98aa0b410
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-07-07 14:31:51 +0300

    Solved 574/A.

---
+++ src/codeforces/contests501_600/problemset574/BearAndElections.java
@@ -0,0 +1,79 @@
+package codeforces.contests501_600.problemset574;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BearAndElections implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n - 1];
+        int limak = in.ni();
+        int result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            x[i] = in.ni();
+        }
+        Arrays.sort(x);
+        while (limak <= x[n - 2]) {
+            limak++;
+            result++;
+            x[n - 2]--;
+            Arrays.sort(x);
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BearAndElections instance = new BearAndElections()) {
+            instance.solve();
+        }
+    }
+}
