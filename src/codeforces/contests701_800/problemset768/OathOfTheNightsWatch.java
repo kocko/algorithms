57fcdcbde3a5e874eb351fd9839812af2f3cf175
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-07-08 10:56:46 +0300

    Solved 768/A.

---
+++ src/codeforces/contests701_800/problemset768/OathOfTheNightsWatch.java
@@ -0,0 +1,73 @@
+package codeforces.contests701_800.problemset768;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class OathOfTheNightsWatch implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) x[i] = in.ni();
+        Arrays.sort(x);
+        int min = x[0], max = x[n - 1], cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (x[i] > min && x[i] < max) cnt++;
+        }
+        out.println(cnt);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (OathOfTheNightsWatch instance = new OathOfTheNightsWatch()) {
+            instance.solve();
+        }
+    }
+}
