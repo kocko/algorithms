e8d42611627eb7119dc37f78ab060694b5e0e453
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-07-08 16:44:39 +0300

    Solved 557/B.

---
+++ src/codeforces/contests501_600/problemset557/PashaAndTea.java
@@ -0,0 +1,75 @@
+package codeforces.contests501_600.problemset557;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class PashaAndTea implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), w = in.ni();
+        List<Long> cups = new ArrayList<>();
+        for (int i = 0; i < 2 * n; i++) cups.add(in.nl());
+        Collections.sort(cups);
+        double min = cups.get(n) / 2.0;
+        if (min > cups.get(0)) {
+            min = cups.get(0);
+        }
+        out.println(Math.min(w, 3 * min * n));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PashaAndTea instance = new PashaAndTea()) {
+            instance.solve();
+        }
+    }
+}
