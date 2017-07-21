6b1a34a914362829ab82169b6e164047a0f45d22
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-07-21 17:17:44 +0300

    Solved 825/A.

---
+++ src/codeforces/contests801_900/problemset825/BinaryProtocol.java
@@ -0,0 +1,77 @@
+package codeforces.contests801_900.problemset825;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BinaryProtocol implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        char[] x = new char[n + 1];
+        x[n] = '0';
+        System.arraycopy(in.next().toCharArray(), 0, x, 0, n);
+        StringBuilder result = new StringBuilder();
+        int current = 0;
+        for (int i = 0; i <= n; i++) {
+            if (x[i] == '1') current++;
+            else {
+                result.append(current);
+                current = 0;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BinaryProtocol instance = new BinaryProtocol()) {
+            instance.solve();
+        }
+    }
+}
