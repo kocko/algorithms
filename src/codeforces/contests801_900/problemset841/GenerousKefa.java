cb7b90c53cc7d23520c55be0cff0a23f1a1f5c3c
kocko <konstantin.yovkov@gmail.com>
2017-08-18 21:54:40 +0300

    Solved 841/A.

---
+++ src/codeforces/contests801_900/problemset841/GenerousKefa.java
@@ -0,0 +1,74 @@
+package codeforces.contests801_900.problemset841;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class GenerousKefa implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        char[] x = in.next().toCharArray();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt[x[i] - 'a']++;
+        }
+        boolean can = true;
+        for (int i = 0; i < 26; i++) {
+            can &= (cnt[i] % k == 0);
+        }
+        out.println(can ? "YES" : "NO");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (GenerousKefa instance = new GenerousKefa()) {
+            instance.solve();
+        }
+    }
+}
