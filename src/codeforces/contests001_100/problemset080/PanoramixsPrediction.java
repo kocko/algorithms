75eb51faebdb035f18f5fc30c53899e494b1f7f2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-09-08 12:01:22 +0300

    Solved 80/A.

---
+++ src/codeforces/contests001_100/problemset080/PanoramixsPrediction.java
@@ -0,0 +1,78 @@
+package codeforces.contests001_100.problemset080;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PanoramixsPrediction implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), next = -1;
+        for (int i = n + 1; i <= m; i++) {
+            if (isPrime(i)) {
+                next = i;
+                break;
+            }
+        }
+        out.println(next == m ? "YES" : "NO");
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PanoramixsPrediction instance = new PanoramixsPrediction()) {
+            instance.solve();
+        }
+    }
+}
