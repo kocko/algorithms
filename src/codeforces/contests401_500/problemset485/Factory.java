f023bd4e602ebba28edab5b8ed94402d3931401a
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-09-15 12:48:29 +0300

    Solved 485/A.

---
+++ src/codeforces/contests401_500/problemset485/Factory.java
@@ -0,0 +1,79 @@
+package codeforces.contests401_500.problemset485;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Factory implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int a = in.ni(), x = in.ni();
+        boolean[] used = new boolean[100001];
+        String result = "No";
+        while (true) {
+            if (a != 0 && used[a]) break;
+            else if (a == 0) {
+                result = "Yes";
+                break;
+            } else {
+                used[a] = true;
+                int mod = a % x;
+                a += mod;
+                a %= x;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Factory instance = new Factory()) {
+            instance.solve();
+        }
+    }
+}
