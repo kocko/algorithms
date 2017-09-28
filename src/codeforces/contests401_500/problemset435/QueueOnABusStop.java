ad3dc20108ce5871a7fa76b6afde538f6222311e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-09-28 17:18:27 +0300

    Solved 435/A.

---
+++ src/codeforces/contests401_500/problemset435/QueueOnABusStop.java
@@ -0,0 +1,76 @@
+package codeforces.contests401_500.problemset435;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class QueueOnABusStop implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), result = 1;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = in.ni();
+        int current = 0, idx = -1;
+        while (++idx < n) {
+            if (current + a[idx] <= m) {
+                current += a[idx];
+            } else {
+                result++;
+                current = a[idx];
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (QueueOnABusStop instance = new QueueOnABusStop()) {
+            instance.solve();
+        }
+    }
+}
