f311fe54247b2114e54b081a51751a0a9a5a0096
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-03 09:48:16 +0300

    Solved 389/A.

---
+++ src/codeforces/contests301_400/problemset389/FoxAndNumberGame.java
@@ -0,0 +1,73 @@
+package codeforces.contests301_400.problemset389;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FoxAndNumberGame implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int gcd = 0;
+        for (int i = 0; i < n; i++) {
+            gcd = gcd(gcd, in.ni());
+        }
+        out.println(gcd * n);
+    }
+    
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (FoxAndNumberGame instance = new FoxAndNumberGame()) {
+            instance.solve();
+        }
+    }
+}
