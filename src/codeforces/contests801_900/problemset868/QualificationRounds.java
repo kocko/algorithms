3bbbb7f8e0be1afc6d1e6c7e09a99405a48db0ca
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-05 13:28:25 +0300

    Solved 868/C.

---
+++ src/codeforces/contests801_900/problemset868/QualificationRounds.java
@@ -0,0 +1,91 @@
+package codeforces.contests801_900.problemset868;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class QualificationRounds implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+    
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        boolean[] has = new boolean[1 << k];
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < k; j++) {
+                sb.append(in.ni());
+            }
+            has[Integer.valueOf(sb.toString(), 2)] = true;
+        }
+        for (int i = 0; i < has.length; i++) {
+            if (has[i]) {
+                for (int j = 0; j < has.length; j++) {
+                    if (has[j] && ((i & j) == 0)) {
+                        out.println("YES");
+                        return;
+                    }
+                }
+            }
+//            for (int j = 0; j < has.length; j++) {
+//                if (has[i] && has[j] && i != j) {
+//                    if ((i ^ j) == (i | j)) {
+//                        out.println("YES");
+//                        return;
+//                    }
+//                }
+//            }
+        }
+        out.println("NO");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (QualificationRounds instance = new QualificationRounds()) {
+            instance.solve();
+        }
+    }
+}
