3e003c0442453a258156a3db4637b4b740f3659e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-16 17:27:21 +0300

    Solved 876/C.

---
+++ src/codeforces/contests801_900/problemset876/ClassroomWatch.java
@@ -0,0 +1,86 @@
+package codeforces.contests801_900.problemset876;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ClassroomWatch implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        List<Integer> result = new ArrayList<>();
+        for (int i = Math.max(1, n - 100); i <= n; i++) {
+            if (i + sumOfDigits(i) == n) {
+                result.add(i);
+            }
+        }
+        out.println(result.size());
+        for (Integer value : result) {
+            out.print(value);
+            out.print(' ');
+        }
+    }
+    
+    private int sumOfDigits(int n) {
+        int ans = 0;
+        while (n > 0) {
+            ans += n % 10;
+            n /= 10;
+        }
+        return ans;
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ClassroomWatch instance = new ClassroomWatch()) {
+            instance.solve();
+        }
+    }
+}
