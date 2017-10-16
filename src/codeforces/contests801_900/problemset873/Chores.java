292814a32e3f1d5af58347cfb04afa7dbb7585cf
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-16 12:53:23 +0300

    Solved 873/A.

---
+++ src/codeforces/contests801_900/problemset873/Chores.java
@@ -0,0 +1,67 @@
+package codeforces.contests801_900.problemset873;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Chores implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni(), x = in.ni(), total = 0;
+        for (int i = 0; i < n - k; i++) total += in.ni();
+        for (int i = 0; i < k; i++) total += x;
+        out.println(total);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Chores instance = new Chores()) {
+            instance.solve();
+        }
+    }
+}
