256debf25a25cf08adccc0e4ce7eb9bea9359479
kocko <konstantin.yovkov@gmail.com>
2017-10-21 20:47:50 +0300

    Solved 390/A.

---
+++ src/codeforces/contests301_400/problemset390/InnaAndAlarmClock.java
@@ -0,0 +1,71 @@
+package codeforces.contests301_400.problemset390;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.BitSet;
+import java.util.StringTokenizer;
+
+public class InnaAndAlarmClock implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        BitSet a = new BitSet(), b = new BitSet();
+        for (int i = 0; i < n; i++) {
+            a.set(in.ni());
+            b.set(in.ni());
+        }
+        out.println(Math.min(a.cardinality(), b.cardinality()));
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (InnaAndAlarmClock instance = new InnaAndAlarmClock()) {
+            instance.solve();
+        }
+    }
+}
