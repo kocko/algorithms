b9ab84cc4853dab9d2bbf46f4587aba2bf1012ff
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-23 16:14:50 +0300

    Solved 122/B.

---
+++ src/codeforces/contests101_200/problemset122/LuckySubstring.java
@@ -0,0 +1,82 @@
+package codeforces.contests101_200.problemset122;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class LuckySubstring implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        int four = 0, seven = 0;
+        for (char c : x) {
+            if (c == '4') four++;
+            else if (c == '7') seven++;
+        }
+        String result;
+        if (four > seven) {
+            result = "4";
+        } else if (four < seven) {
+            result = "7";
+        } else {
+            if (four > 0) result = "4";
+            else result = "-1";
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LuckySubstring instance = new LuckySubstring()) {
+            instance.solve();
+        }
+    }
+}
