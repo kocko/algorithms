2f04bf9009177a744e5c0283cf4849964a54cf11
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-30 10:44:25 +0200

    Solved 884/A.

---
+++ src/codeforces/contests801_900/problemset884/BookReading.java
@@ -0,0 +1,71 @@
+package codeforces.contests801_900.problemset884;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BookReading implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), t = in.ni();
+        int day;
+        for (day = 1; day <= n; day++) {
+            int work = in.ni();
+            t -= (86400 - work);
+            if (t <= 0) break;
+        }
+        out.println(day);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BookReading instance = new BookReading()) {
+            instance.solve();
+        }
+    }
+}
