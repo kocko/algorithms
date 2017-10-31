3db12488c9c1789459d54e0b559c7adc498ee97f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-10-31 13:19:32 +0200

    Solved 359/B.

---
+++ src/codeforces/contests301_400/problemset359/Permutation.java
@@ -0,0 +1,77 @@
+package codeforces.contests301_400.problemset359;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Permutation implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        int[] result = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            result[i] = i + 1;
+        }
+        for (int i = 0; i < k; i++) {
+            int t = result[2 * i + 1];
+            result[2 * i + 1] = result[2 * i];
+            result[2 * i] = t;
+        }
+        for (int i = 0; i < 2 * n; i++) {
+            out.print(result[i]);
+            out.print(' ');
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Permutation instance = new Permutation()) {
+            instance.solve();
+        }
+    }
+}
