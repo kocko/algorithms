648f0f7af24e458664dc1ea50b04d3d47c624a62
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-11-01 09:57:20 +0200

    Solved 205/B.

---
+++ src/codeforces/contests201_300/problemset205/LittleElephantAndSorting.java
@@ -0,0 +1,75 @@
+package codeforces.contests201_300.problemset205;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LittleElephantAndSorting implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        long result = 0, top = -1;
+        for (int i = 0; i < n; i++) {
+            long next = in.ni();
+            if (next + result >= top) {
+                top = next + result;
+            } else {
+                long diff = top - next - result;
+                result += diff;
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LittleElephantAndSorting instance = new LittleElephantAndSorting()) {
+            instance.solve();
+        }
+    }
+}
