27533747447877cff9b115bd6ffb50970a94837f
kocko <konstantin.yovkov@gmail.com>
2017-11-03 21:12:59 +0200

    Solved 887/A.

---
+++ src/codeforces/contests801_900/problemset887/Div64.java
@@ -0,0 +1,76 @@
+package codeforces.contests801_900.problemset887;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Div64 implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        int n = x.length;
+        int zeroes = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (x[i] == '0') zeroes++;
+            else {
+                if (zeroes >= 6) {
+                    out.println("yes");
+                    return;
+                }
+            }
+        }
+        out.println("no");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Div64 instance = new Div64()) {
+            instance.solve();
+        }
+    }
+}
