38e47c59966abfa288d135b6bc2ddeb6d76e4e6d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-11-15 14:08:45 +0200

    Solved 009/C.

---
+++ src/codeforces/contests001_100/problemset009/HexadecimalsNumbers.java
@@ -0,0 +1,80 @@
+package codeforces.contests001_100.problemset009;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class HexadecimalsNumbers implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        n = in.ni();
+        recurse(1);
+        out.println(count);
+    }
+    
+    private int n, count;
+    
+    private void recurse(int num) {
+        count++;
+        if (num * 10 <= n) {
+            recurse(num * 10);
+        }
+        if (num * 10 + 1 <= n) {
+            recurse(num * 10 + 1);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (HexadecimalsNumbers instance = new HexadecimalsNumbers()) {
+            instance.solve();
+        }
+    }
+}
