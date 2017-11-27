0df2cacd63e1aa8682004a189b4a2f26de1fc02e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-11-27 10:25:16 +0200

    Solved 895/A.

---
+++ src/codeforces/contests801_900/problemset895/PizzaSeparation.java
@@ -0,0 +1,83 @@
+package codeforces.contests801_900.problemset895;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
+
+public class PizzaSeparation implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        int min = 361;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = 0;
+                for (int k = i; k <= j; k++) {
+                    temp += x[k];
+                }       
+                int rem = 360 - temp;
+                min = min(min, abs(temp - rem));
+            }
+        }
+        out.println(min);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PizzaSeparation instance = new PizzaSeparation()) {
+            instance.solve();
+        }
+    }
+}
