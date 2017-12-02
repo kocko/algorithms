c2fe4fd4cc763f992f39737e2b129b155a64f087
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-12-02 22:35:21 +0200

    Solved 897/B.

---
+++ src/codeforces/contests801_900/problemset897/ChthollysRequest.java
@@ -0,0 +1,75 @@
+package codeforces.contests801_900.problemset897;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ChthollysRequest implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int k = in.ni();
+        long p = in.ni();
+        long result = 0;
+        for (long i = 1; i <= k; i++) {
+            StringBuilder a = new StringBuilder(), b = new StringBuilder();
+            a.append(i);
+            b = b.append(i).reverse();
+            a.append(b);
+            result += Long.valueOf(a.toString());
+            result %= p;
+        }
+        out.println(result % p);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ChthollysRequest instance = new ChthollysRequest()) {
+            instance.solve();
+        }
+    }
+}
