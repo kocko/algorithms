b609662a12f4eea05f44037633256b18430879fc
kocko <konstantin.yovkov@gmail.com>
2017-12-09 20:33:32 +0200

    Minor fix.

--- src/topcoder/contests701_800/srm725/FiveRooksDiv2.java
+++ src/topcoder/contests701_800/srm725/FiveRooksDiv2.java
@@ -1,26 +1,25 @@
 package topcoder.contests701_800.srm725;
 
-import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class FiveRooksDiv2 {
 	
 	public int findMax(String[] board) {
 		int[] row = new int[8], col = new int[8];
-		int result = 0;
 		for (int i = 0; i < 8; i++) {
-		    int ans = 0;
-		    int bans = 0;
 			for (int j = 0; j < 8; j++) {
 				if (board[i].charAt(j) == 'R') {
-				    ans++;
+				    row[i]++;
+				    col[j]++;
 				}
-                if (board[j].charAt(i) == 'R') {
-                    bans++;
-                }
 			}
-			result = max(ans, result);
-			result = max(bans, result);
+		}
+		int result = 0;
+		for (int i = 0; i < 8; i++) {
+			result = min(row[i], result);
+			result = min(col[i], result);
 		}
 		return result;
 	}
+	
 }
