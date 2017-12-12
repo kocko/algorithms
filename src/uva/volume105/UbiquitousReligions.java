66766ba54aa7b4a14b208d56c6e6cb2689282ee5
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-12-12 14:24:34 +0200

    Solved UVa/10583.

---
+++ src/uva/volume105/UbiquitousReligions.java
@@ -0,0 +1,103 @@
+package uva.volume105;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    private class DisjointSet {
+        private int[] root;
+        private int[] size;
+
+        private DisjointSet(int n) {
+            root = new int[n + 1];
+            size = new int[n + 1];
+            for (int i = 0; i < root.length; i++) {
+                root[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        private int root(int x) {
+            return (x == root[x]) ? x : (root[x] = root(root[x]));
+        }
+
+        private void join(int a, int b) {
+            int x = root(a), y = root(b);
+            if (x != y) {
+                if (size[x] < size[y]) y = (x ^ y) ^ (x = y);
+                size[x] += size[y];
+                root[y] = x;
+            }
+        }
+    }
+    
+    public void solve() {
+        int n, m, testCase = 0;
+        while ((n = in.ni()) != 0 | (m = in.ni()) != 0) {
+            DisjointSet dsu = new DisjointSet(n);
+            for (int i = 0; i < m; i++) {
+                int u = in.ni() - 1, v = in.ni() - 1;
+                dsu.join(u, v);
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (dsu.root(i) == i) count++;
+            }
+            out.printf("Case %d: %d\n", ++testCase, count);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Main instance = new Main()) {
+            instance.solve();
+        }
+    }
+}
