30966a0026cfab2f00a1431014fd7f39d648b8b3
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2017-12-20 10:37:42 +0200

    Solved 902/A.

---
+++ src/codeforces/contests901_1000/problemset902/VisitingAFriend.java
@@ -0,0 +1,76 @@
+package codeforces.contests901_1000.problemset902;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class VisitingAFriend implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        boolean[] reachable = new boolean[101];
+        for (int i = 0; i < n; i++) {
+            int x = in.ni(), y = in.ni();
+            for (int j = x + 1; j <= y; j++) {
+                reachable[j] = true;
+            }
+        }
+        boolean ok = true;
+        for (int i = 1; i <= m; i++) {
+            ok &= reachable[i];
+        }
+        out.println(ok ? "YES" : "NO");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (VisitingAFriend instance = new VisitingAFriend()) {
+            instance.solve();
+        }
+    }
+}
