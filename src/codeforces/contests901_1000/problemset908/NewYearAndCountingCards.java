917a3a46aece7bbd23f093939db3072f59a1f4d8
kocko <konstantin.yovkov@gmail.com>
2017-12-30 00:09:05 +0200

    Solved 908/A.

---
+++ src/codeforces/contests901_1000/problemset908/NewYearAndCountingCards.java
@@ -0,0 +1,75 @@
+package codeforces.contests901_1000.problemset908;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class NewYearAndCountingCards implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        int result = 0;
+        for (char c : x) {
+            if (c >= '0' && c <= '9') {
+                if ((c - '0') % 2 == 1) result++;
+            } else if (isVowel(c)) result++;
+        }
+        out.println(result);
+    }
+
+    private boolean isVowel(char c) {
+        return "aeiou".indexOf(c) >= 0;
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (NewYearAndCountingCards instance = new NewYearAndCountingCards()) {
+            instance.solve();
+        }
+    }
+}
