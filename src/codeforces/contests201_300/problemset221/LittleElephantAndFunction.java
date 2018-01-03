669c219fb055668fe658ea3ac32f9da827165ca9
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-01-03 15:18:56 +0200

    Solved 221/A.

---
+++ src/codeforces/contests201_300/problemset221/LittleElephantAndFunction.java
@@ -0,0 +1,69 @@
+package codeforces.contests201_300.problemset221;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LittleElephantAndFunction implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        out.print(n);
+        for (int i = 1; i < n; i++) {
+            out.print(' ');
+            out.print(i);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LittleElephantAndFunction instance = new LittleElephantAndFunction()) {
+            instance.solve();
+        }
+    }
+}
