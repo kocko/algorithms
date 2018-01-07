c9e84659956ef521df8768f7d362937b463af573
kocko <konstantin.yovkov@gmail.com>
2018-01-07 22:21:40 +0200

    Solved 304/A.

---
+++ src/codeforces/contests301_400/problemset304/PythagoreanTheoremII.java
@@ -0,0 +1,74 @@
+package codeforces.contests301_400.problemset304;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PythagoreanTheoremII implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), result = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = a; b <= n; b++) {
+                int c = a * a + b * b;
+                int sqrt = (int) Math.sqrt(c);
+                if (sqrt * sqrt == c && sqrt <= n) {
+                    result++;
+                }
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PythagoreanTheoremII instance = new PythagoreanTheoremII()) {
+            instance.solve();
+        }
+    }
+}
