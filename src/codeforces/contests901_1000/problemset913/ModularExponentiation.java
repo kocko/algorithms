16e8ab98d2f1a593bfe5a38ddf3bcc5bca352192
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-01-08 17:16:37 +0200

    Solved 913/A.

---
+++ src/codeforces/contests901_1000/problemset913/ModularExponentiation.java
@@ -0,0 +1,70 @@
+package codeforces.contests901_1000.problemset913;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ModularExponentiation implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int bits = Integer.toBinaryString(m).length();
+        if (n <= bits + 1) {
+            out.println(m % (1 << n));
+        } else {
+            out.println(m);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ModularExponentiation instance = new ModularExponentiation()) {
+            instance.solve();
+        }
+    }
+}
