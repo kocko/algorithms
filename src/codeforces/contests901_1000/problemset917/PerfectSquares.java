40ea94527bc65165ad5905a688da7e9f751cc994
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-01-20 22:33:11 +0200

    Solved 917/A.

---
+++ src/codeforces/contests901_1000/problemset917/PerfectSquares.java
@@ -0,0 +1,77 @@
+package codeforces.contests901_1000.problemset917;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class PerfectSquares implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            if (next <= 0) {
+                int sqrt = (int) Math.sqrt(n);
+                if (sqrt * sqrt != n) {
+                    ans = Math.max(ans, n);
+                }
+            }
+        }
+        out.println(ans);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PerfectSquares instance = new PerfectSquares()) {
+            instance.solve();
+        }
+    }
+}
