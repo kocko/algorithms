35a503789c98443ca5c272f3cd09989db10a0b1f
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-01-22 15:01:48 +0200

    Solved 202/A.

---
+++ src/codeforces/contests201_300/problemset202/LLPS.java
@@ -0,0 +1,79 @@
+package codeforces.contests201_300.problemset202;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class LLPS implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        int[] cnt = new int[26];
+        int idx = -1;
+        for (char ch : x) {
+            int c = ch - 'a';
+            cnt[c]++;
+            if (c > idx) {
+                idx = c;
+            }
+        }
+        char c = (char) ('a' + idx);
+        for (int i = 0; i < cnt[c - 'a']; i++) {
+            out.print(c);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LLPS instance = new LLPS()) {
+            instance.solve();
+        }
+    }
+}
