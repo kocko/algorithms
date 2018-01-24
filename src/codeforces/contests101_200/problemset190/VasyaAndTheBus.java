efc33d53c99407dae39afcdfd1a152bced2e93f7
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-01-24 23:10:11 +0200

    Solved 190/A.

---
+++ src/codeforces/contests101_200/problemset190/VasyaAndTheBus.java
@@ -0,0 +1,78 @@
+package codeforces.contests101_200.problemset190;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class VasyaAndTheBus implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        if (n == 0 && m > 0) {
+            out.println("Impossible");
+            return;
+        }
+        int min = n, diff = m - n, max = n;
+        if (diff >= 0) {
+            min = n + diff;
+        }
+        if (m > 0) {
+            max = n + m - 1;
+        }
+        out.println(min + " " + max);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (VasyaAndTheBus instance = new VasyaAndTheBus()) {
+            instance.solve();
+        }
+    }
+}
