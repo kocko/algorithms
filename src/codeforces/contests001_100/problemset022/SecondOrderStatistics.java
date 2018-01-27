d53bae657d424e97db1d974a41c56a6168d3a295
kocko <konstantin.yovkov@gmail.com>
2018-01-28 00:41:23 +0200

    Solved 22/A.

---
+++ src/codeforces/contests001_100/problemset022/SecondOrderStatistics.java
@@ -0,0 +1,73 @@
+package codeforces.contests001_100.problemset022;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class SecondOrderStatistics implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(in.ni());
+        }
+        Iterator<Integer> iterator = set.iterator();
+        iterator.next();
+        out.println(set.size() == 1 ? "NO" : iterator.next());
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (SecondOrderStatistics instance = new SecondOrderStatistics()) {
+            instance.solve();
+        }
+    }
+}
