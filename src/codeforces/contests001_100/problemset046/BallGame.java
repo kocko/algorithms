3d07e11c270e15f9099f8e35477cebe9c9173713
kocko <konstantin.yovkov@gmail.com>
2018-01-28 21:21:16 +0200

    Solved 046/A.

---
+++ src/codeforces/contests001_100/problemset046/BallGame.java
@@ -0,0 +1,71 @@
+package codeforces.contests001_100.problemset046;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BallGame implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int idx = 1, step = 1;
+        for (int i = 0; i < n - 1; i++) {
+            idx += step++;
+            if (idx > n) idx -= n;
+            out.print(idx);
+            out.print(' ');
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BallGame instance = new BallGame()) {
+            instance.solve();
+        }
+    }
+}
