16eeea9692aae165caaace55f12346caaa5d50d5
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-01-31 17:11:10 +0200

    Solved 919/A.

---
+++ src/codeforces/contests901_1000/problemset919/Supermarket.java
@@ -0,0 +1,72 @@
+package codeforces.contests901_1000.problemset919;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+
+public class Supermarket implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        double min = Double.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            double a = parseDouble(in.next()), b = parseDouble(in.next());
+            min = Math.min(min, a / b);
+        }
+        out.println(min * m);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Supermarket instance = new Supermarket()) {
+            instance.solve();
+        }
+    }
+}
