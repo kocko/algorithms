48d5e389073f9bf9583041a4a7b4f42f401942de
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-02-08 11:51:59 +0200

    Solved 922/C.

---
+++ src/codeforces/contests901_1000/problemset922/CavePainting.java
@@ -0,0 +1,79 @@
+package codeforces.contests901_1000.problemset922;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class CavePainting implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), k = in.nl();
+        Set<Long> set = new HashSet<>();
+        for (long i = 1; i <= k; i++) {
+            long rem = n % i;
+            if (set.contains(rem)) {
+                out.println("No");
+                return;
+            } else {
+                set.add(rem);
+            }
+        }
+        out.println("Yes");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (CavePainting instance = new CavePainting()) {
+            instance.solve();
+        }
+    }
+}
