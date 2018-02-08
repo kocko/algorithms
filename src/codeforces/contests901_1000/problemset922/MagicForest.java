a0bce88ad94a6a2cc2b8e9b4a307e1c9c0c421ef
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-02-08 11:51:53 +0200

    Solved 922/B.

---
+++ src/codeforces/contests901_1000/problemset922/MagicForest.java
@@ -0,0 +1,75 @@
+package codeforces.contests901_1000.problemset922;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class MagicForest implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), result = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = a + 1; b <= n; b++) {
+                int c = a ^ b;
+                if (c > b && c <= n && a + b > c && a + c > b && b + c > a) {
+                    result++;
+                }
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MagicForest instance = new MagicForest()) {
+            instance.solve();
+        }
+    }
+}
