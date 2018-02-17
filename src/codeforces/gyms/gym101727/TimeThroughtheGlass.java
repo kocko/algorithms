085fb62083218ea86d161317a84f546982dfb641
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-02-17 22:47:09 +0200

    Solved Yandex Algorithm 2018/Warm-up/A.

---
+++ src/codeforces/gyms/gym101727/TimeThroughtheGlass.java
@@ -0,0 +1,69 @@
+package codeforces.gyms.gym101727;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class TimeThroughtheGlass implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int h = in.ni(), m = in.ni();
+        out.print((12 - h) % 12);
+        out.print(' ');
+        out.print((60 - m) % 60);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (TimeThroughtheGlass instance = new TimeThroughtheGlass()) {
+            instance.solve();
+        }
+    }
+}
