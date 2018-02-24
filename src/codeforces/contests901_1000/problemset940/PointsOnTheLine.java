285cb9cd1de30f4410c5d37987b7e285403516dc
kocko <konstantin.yovkov@gmail.com>
2018-02-24 17:57:47 +0200

    Solved 940/A.

---
+++ src/codeforces/contests901_1000/problemset940/PointsOnTheLine.java
@@ -0,0 +1,78 @@
+package codeforces.contests901_1000.problemset940;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class PointsOnTheLine implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), d = in.ni(), result = 1000;
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        Arrays.sort(x);
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (x[j] - x[i] > d) break;
+            }
+            result = Math.min(n - j + i, result);
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PointsOnTheLine instance = new PointsOnTheLine()) {
+            instance.solve();
+        }
+    }
+}
