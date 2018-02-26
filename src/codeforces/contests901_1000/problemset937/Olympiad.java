9b0c6556d939563884089ab962a56759d8caa4b2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-02-26 11:28:57 +0200

    Solved 937/A.

---
+++ src/codeforces/contests901_1000/problemset937/Olympiad.java
@@ -0,0 +1,75 @@
+package codeforces.contests901_1000.problemset937;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class Olympiad implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), result = 0;
+        boolean[] has = new boolean[601];
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            if (next > 0 && !has[next]) {
+                result++;
+            }
+            has[next] = true;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Olympiad instance = new Olympiad()) {
+            instance.solve();
+        }
+    }
+}
