1c16148728d30d6bb81e36b82044c0497b20e852
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-03-11 02:02:54 +0200

    Solved Codechef/ClosingTheTweets.

---
+++ src/codechef/medium/ClosingTheTweets.java
@@ -0,0 +1,75 @@
+package codechef.medium;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.BitSet;
+import java.util.StringTokenizer;
+
+public class ClosingTheTweets implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        BitSet set = new BitSet(n);
+        while (k-- > 0) {
+            String operation = in.next();
+            if ("CLOSEALL".equals(operation)) {
+                set.clear();
+            } else {
+                set.flip(in.ni());
+            }
+            out.println(set.cardinality());
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ClosingTheTweets instance = new ClosingTheTweets()) {
+            instance.solve();
+        }
+    }
+}
