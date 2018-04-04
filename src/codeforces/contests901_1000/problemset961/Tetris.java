129736974ccb4a5602210c8390ce0c1cef6abdbf
kocko <konstantin.yovkov@gmail.com>
2018-04-04 23:25:38 +0300

    Solved 961/A.

---
+++ src/codeforces/contests901_1000/problemset961/Tetris.java
@@ -0,0 +1,80 @@
+package codeforces.contests901_1000.problemset961;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Tetris implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni(), result = 0;
+        int[] score = new int[n];
+        for (int i = 0; i < m; i++) {
+            int column = in.ni() - 1;
+            score[column]++;
+            boolean ok = true;
+            for (int j = 0; j < n; j++) {
+                ok &= score[j] > 0;
+            }
+            if (ok) {
+                result++;
+                for (int j = 0; j < n; j++) {
+                    score[j]--;
+                }
+            }
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Tetris instance = new Tetris()) {
+            instance.solve();
+        }
+    }
+}
