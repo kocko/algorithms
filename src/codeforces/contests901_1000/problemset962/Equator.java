97ac5362f0a759179cad101525b0673bfd523ed6
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-04-10 17:44:14 +0300

    Solved 962/A.

---
+++ src/codeforces/contests901_1000/problemset962/Equator.java
@@ -0,0 +1,80 @@
+package codeforces.contests901_1000.problemset962;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class Equator implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        long sum = 0;
+        long[] x = new long[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.nl();
+            sum += x[i];
+        }
+        long half = sum / 2, current = 0;
+        for (int i = 0; i < n; i++) {
+            current += x[i];
+            if (current >= half) {
+                out.println(i + 1);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Equator instance = new Equator()) {
+            instance.solve();
+        }
+    }
+}
