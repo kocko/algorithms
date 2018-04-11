3823d15a634d264975bb70debf002c98b682a887
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-04-11 18:25:55 +0300

    Solved 471/C.

---
+++ src/codeforces/contests401_500/problemset471/MUHAndHouseOfCards.java
@@ -0,0 +1,69 @@
+package codeforces.contests401_500.problemset471;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MUHAndHouseOfCards implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), result = 0, h = 0;
+        while (2 * (h + 1) + 3 * (h + 1) * h / 2 <= n) {
+            h++;
+            if ((n - 2 * h - 3 * (h - 1) * h / 2) % 3 == 0) result++;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MUHAndHouseOfCards instance = new MUHAndHouseOfCards()) {
+            instance.solve();
+        }
+    }
+}
