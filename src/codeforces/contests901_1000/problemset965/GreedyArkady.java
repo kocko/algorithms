d4f5e6fb3b645638a61b96aafec11f0735b5845d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-04-26 12:18:50 +0300

    Solved 965/C.

---
+++ src/codeforces/contests901_1000/problemset965/GreedyArkady.java
@@ -0,0 +1,74 @@
+package codeforces.contests901_1000.problemset965;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class GreedyArkady implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), k = in.nl(), m = in.nl(), d = in.nl();
+        long result = m;
+        for (long i = 2; i <= d; i++) {
+            long total = (i - 1) * k + 1;
+            if (total > n) break;
+            long x = min(m, n / total);
+            result = max(result, x * i);
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (GreedyArkady instance = new GreedyArkady()) {
+            instance.solve();
+        }
+    }
+}
