e801232cb0d8c16899f4f4d049e346da21377238
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-05-08 11:30:41 +0300

    Solved 977/A.

---
+++ src/codeforces/contests901_1000/problemset977/WrongSubstraction.java
@@ -0,0 +1,75 @@
+package codeforces.contests901_1000.problemset977;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class WrongSubstraction implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        while (k-- > 0) {
+            int last = n % 10;
+            if (last != 0) {
+                n--;
+            } else {
+                n /= 10;
+            }
+        }
+        out.println(n);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (WrongSubstraction instance = new WrongSubstraction()) {
+            instance.solve();
+        }
+    }
+}
