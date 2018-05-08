865b4b83bac34cf16961c739f727407e0c34c477
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-05-08 11:30:54 +0300

    Solved 977/C.

---
+++ src/codeforces/contests901_1000/problemset977/LessOrEqual.java
@@ -0,0 +1,92 @@
+package codeforces.contests901_1000.problemset977;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+import static java.lang.Math.*;
+
+public class LessOrEqual implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(in.ni());
+        }
+        list.sort(Comparator.naturalOrder());
+        if (k == 0) {
+            if (list.get(0) == 1) {
+                out.println(-1);
+            } else {
+                out.println(list.get(0) - 1);
+            }
+        } else if (k < n) {
+            if (list.get(k - 1).equals(list.get(k))) {
+                out.println(-1);
+            } else {
+                out.println(list.get(k - 1));
+            }
+        } else if (k == n) {
+            out.println(list.get(n - 1));
+        } else {
+            out.println(-1);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LessOrEqual instance = new LessOrEqual()) {
+            instance.solve();
+        }
+    }
+}
