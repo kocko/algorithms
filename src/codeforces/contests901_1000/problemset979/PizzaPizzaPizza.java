4018bc1e03997abb5b3f71490a27fadba3bfd1f2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-05-16 10:54:30 +0300

    Solved 979/A.

---
+++ src/codeforces/contests901_1000/problemset979/PizzaPizzaPizza.java
@@ -0,0 +1,72 @@
+package codeforces.contests901_1000.problemset979;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PizzaPizzaPizza implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl() + 1, result;
+        if (n == 1) {
+            result = 0;
+        } else if (n % 2 == 0) {
+            result = n / 2;
+        } else {
+            result = n;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PizzaPizzaPizza instance = new PizzaPizzaPizza()) {
+            instance.solve();
+        }
+    }
+}
