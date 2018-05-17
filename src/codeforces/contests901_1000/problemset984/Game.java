ddf4b33ea58e54c8d4196faa696c21d0364e4c15
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-05-17 16:54:55 +0300

    Solved 984/A.

---
+++ src/codeforces/contests901_1000/problemset984/Game.java
@@ -0,0 +1,83 @@
+package codeforces.contests901_1000.problemset984;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+import static java.util.Arrays.sort;
+
+public class Game implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        sort(x);
+        int left = 0, right = n - 1;
+        boolean isRight = true;
+        while (right > left) {
+            if (isRight) {
+                right--;
+            } else {
+                left++;
+            }
+            isRight = !isRight;
+        }
+        out.println(x[left]);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Game instance = new Game()) {
+            instance.solve();
+        }
+    }
+}
