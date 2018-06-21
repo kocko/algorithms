8543260b971cc7294f29c0cbf5929c5e0bd55ec0
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-06-21 12:24:54 +0300

    Solved 291/A.

---
+++ src/codeforces/contests201_300/problemset291/SpykeTalks.java
@@ -0,0 +1,83 @@
+package codeforces.contests201_300.problemset291;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class SpykeTalks implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] x = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        Arrays.sort(x);
+        for (int i = 0; i < n - 2; i++) {
+            if (x[i] != 0 && (x[i] == x[i + 1] && x[i + 1] == x[i + 2])) {
+                out.println(-1);
+                return;
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (x[i] != 0 && x[i] == x[i + 1]) result++;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (SpykeTalks instance = new SpykeTalks()) {
+            instance.solve();
+        }
+    }
+}
