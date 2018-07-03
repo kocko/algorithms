ce8ab25a2c6ba258431b3b1739ed74fb8206aede
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-07-03 17:53:00 +0300

    Solved 1003/A.

---
+++ src/codeforces/contests1001_1100/problemset1003/PolycarpsPockets.java
@@ -0,0 +1,72 @@
+package codeforces.contests1001_1100.problemset1003;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PolycarpsPockets implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] cnt = new int[101];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int next = in.ni();
+            cnt[next]++;
+            if (cnt[next] > max) max = cnt[next];
+        }
+        out.println(max);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (PolycarpsPockets instance = new PolycarpsPockets()) {
+            instance.solve();
+        }
+    }
+}
