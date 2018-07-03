231b52b1d0e08aa60e1c1c7ea3d9a96e70b51237
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-07-03 17:53:05 +0300

    Solved 1003/C.

---
+++ src/codeforces/contests1001_1100/problemset1003/IntenseHeat.java
@@ -0,0 +1,86 @@
+package codeforces.contests1001_1100.problemset1003;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class IntenseHeat implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), k = in.ni();
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = in.ni();
+        }
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + x[i - 1];
+        }
+        double max = 0d;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                double size = j - i + 1.;
+                if (size >= k) {
+                    int total = prefix[j];
+                    if (i > 0) total -= prefix[i - 1];
+                    double d = total / size;
+                    if (d > max) max = d;
+                }
+            }
+        }
+        out.println(max);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (IntenseHeat instance = new IntenseHeat()) {
+            instance.solve();
+        }
+    }
+}
