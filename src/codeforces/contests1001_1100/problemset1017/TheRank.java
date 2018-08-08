9dcb087494246c13e4ed63bd8ef68ee391552b3d
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-08-08 18:36:47 +0300

    Solved 1017/A.

---
+++ src/codeforces/contests1001_1100/problemset1017/TheRank.java
@@ -0,0 +1,75 @@
+package codeforces.contests1001_1100.problemset1017;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class A implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        int[] sums = new int[n];
+        for (int i = 0; i < n; i++) {
+            sums[i] = in.ni() + in.ni() + in.ni() + in.ni();
+        }
+        int rank = 1;
+        for (int i = 1; i < n; i++) {
+            if (sums[i] > sums[0]) rank++;
+        }
+        out.println(rank);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (A instance = new A()) {
+            instance.solve();
+        }
+    }
+}
