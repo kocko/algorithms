6d8578bcab11f31c739c5c64f25f699629ac1510
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-08-28 11:52:23 +0300

    Solved 1028/B.

---
+++ src/codeforces/contests1001_1100/problemset1028/UnnaturalConditions.java
@@ -0,0 +1,75 @@
+package codeforces.contests1001_1100.problemset1028;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class UnnaturalConditions implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), m = in.ni();
+        int fours = (n / 4) + (n % 4 != 0 ? 1 : 0);
+        for (int i = 0; i < fours; i++) {
+            out.print(5);
+        }
+        out.println();
+        for (int i = 0; i < fours - 1; i++) {
+            out.print(4);
+        }
+        out.println(5);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (UnnaturalConditions instance = new UnnaturalConditions()) {
+            instance.solve();
+        }
+    }
+}
