2efc3c2de29b3b04553197a551cfe67b15ea96f1
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-09-13 10:13:51 +0300

    Solved 1025/A.

---
+++ src/codeforces/contests1001_1100/problemset1025/DoggoRecoloring.java
@@ -0,0 +1,73 @@
+package codeforces.contests1001_1100.problemset1025;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DoggoRecoloring implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        char[] x = in.next().toCharArray();
+        int[] cnt = new int[26];
+        for (char c : x) cnt[c - 'a']++;
+        int bigger = 0;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > 1) bigger++;
+        }
+        boolean ok = n == 1 || bigger > 0;
+        out.println(ok ? "Yes" : "No");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (DoggoRecoloring instance = new DoggoRecoloring()) {
+            instance.solve();
+        }
+    }
+}
