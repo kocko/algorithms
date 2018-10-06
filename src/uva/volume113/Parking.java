28f269a275cb6e3519fc43f990b47d27313eb9da
kocko <konstantin.yovkov@gmail.com>
2018-10-07 01:19:01 +0300

    Solved UVa/11364.

---
+++ src/uva/volume113/Parking.java
@@ -0,0 +1,74 @@
+package uva.volume113;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Parking implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            int n = in.ni();
+            int[] x = new int[n];
+            for (int i = 0; i < n; i++) {
+                x[i] = in.ni();
+            }
+            Arrays.sort(x);
+            out.println(2 * (x[n - 1] - x[0]));
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Parking instance = new Parking()) {
+            instance.solve();
+        }
+    }
+}
