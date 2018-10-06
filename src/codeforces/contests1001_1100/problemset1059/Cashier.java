7d206d3e634dcc9b52cf14505e8950a1d6c097cd
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-10-06 13:08:16 +0300

    Solved 1059/A.

---
+++ src/codeforces/contests1001_1100/problemset1059/Cashier.java
@@ -0,0 +1,82 @@
+package codeforces.contests1001_1100.problemset1059;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Cashier implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni(), L = in.ni(), a = in.ni();
+        long[] t = new long[n], l = new long[n];
+        for (int i = 0; i < n; i++) {
+            t[i] = in.nl();
+            l[i] = in.nl();
+        }
+
+        long result = 0;
+        if (n == 0) {
+            result = L / a;
+        } else {
+            long last = 0;
+            for (int i = 0; i < n; i++) {
+                result += (t[i] - last) / a;
+                last = t[i] + l[i];
+            }
+            result += (L - last) / a;
+        }
+        out.println(result);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (Cashier instance = new Cashier()) {
+            instance.solve();
+        }
+    }
+}
