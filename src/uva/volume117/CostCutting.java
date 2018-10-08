55d2559cabe7bc71d64edc4cb16d99f085f72700
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-10-08 13:18:47 +0300

    Solved UVa/11727.

---
+++ src/uva/volume117/CostCutting.java
@@ -0,0 +1,74 @@
+package uva.volume117;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CostCutting implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        for (int testCase = 1; testCase <= t; testCase++) {
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, xor = 0;
+            for (int i = 0; i < 3; i++) {
+                int next = in.ni();
+                min = Math.min(next, min);
+                max = Math.max(next, max);
+                xor ^= next;
+            }
+            out.printf("Case %d: %d\n", testCase, xor ^ min ^ max);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (CostCutting instance = new CostCutting()) {
+            instance.solve();
+        }
+    }
+}
