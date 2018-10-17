85a89ccea7ab963226e4ca106ba068319b5cc5c2
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-10-17 17:52:23 +0300

    Solved 1066/A.

---
+++ src/codeforces/contests1001_1100/problemset1066/VovaAndTrain.java
@@ -0,0 +1,74 @@
+package codeforces.contests1001_1100.problemset1066;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class VovaAndTrain implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            int L = in.ni(), v = in.ni(), left = in.ni(), right = in.ni();
+            out.println(f(L, v) - f(right, v) + f (left - 1, v));
+        }
+    }
+
+    private int f(int max, int divisor) {
+        int rem = max % divisor;
+        max -= rem;
+        return max / divisor;
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (VovaAndTrain instance = new VovaAndTrain()) {
+            instance.solve();
+        }
+    }
+}
