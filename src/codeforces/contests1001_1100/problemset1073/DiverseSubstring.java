e1c92ee1493afff04befb4e61b852d5b70203a61
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-10-25 18:16:32 +0300

    Solved 1073/A.

---
+++ src/codeforces/contests1001_1100/problemset1073/DiverseSubstring.java
@@ -0,0 +1,73 @@
+package codeforces.contests1001_1100.problemset1073;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DiverseSubstring implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        char[] x = in.next().toCharArray();
+        for (int i = 1; i < n; i++) {
+            if (x[i] != x[i - 1]) {
+                out.println("YES");
+                out.println(x[i - 1] + "" + x[i]);
+                return;
+            }
+        }
+        out.println("NO");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (DiverseSubstring instance = new DiverseSubstring()) {
+            instance.solve();
+        }
+    }
+}
