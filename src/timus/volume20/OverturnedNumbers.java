b613a20e6b9bd0403300802cf9395573f0457c6a
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-11-06 18:59:57 +0200

    Solved Timus/2031.

---
+++ src/timus/volume20/OverturnedNumbers.java
@@ -0,0 +1,75 @@
+package timus.volume20;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class OverturnedNumbers implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        if (n == 1) {
+            out.println("11");
+        } else if (n == 2) {
+            out.println("11 01");
+        } else if (n == 3) {
+            out.println("11 01 60");
+        } else if (n == 4) {
+            out.println("16 06 68 88");
+        } else {
+            out.println("Glupenky Pierre");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (OverturnedNumbers instance = new OverturnedNumbers()) {
+            instance.solve();
+        }
+    }
+}
