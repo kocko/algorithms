0e04fd351b295dec1920cfb851bab28fb8932160
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-11-06 19:31:26 +0200

    Solved Timus/2025.

---
+++ src/timus/volume20/LineFighting.java
@@ -0,0 +1,80 @@
+package timus.volume20;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LineFighting implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            int n = in.ni(), k = in.ni();
+            int teamSize = n / k, rem = n % k;
+
+            int ordinaryTeams = k - rem;
+            int ordinaryFighters = ordinaryTeams * teamSize;
+            int ordinaryOpponents = n - teamSize;
+
+            int unordinaryTeamSize = teamSize + 1;
+            int unordinaryFighters = rem * unordinaryTeamSize;
+            int unordinaryOpponents = n - unordinaryTeamSize;
+
+            int result = (ordinaryFighters * ordinaryOpponents + unordinaryFighters * unordinaryOpponents) >> 1;
+
+            out.println(result);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (LineFighting instance = new LineFighting()) {
+            instance.solve();
+        }
+    }
+}
