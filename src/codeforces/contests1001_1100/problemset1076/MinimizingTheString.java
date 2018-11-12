f5510455e4716f3301754e41c6e73472e07522fe
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-11-12 17:44:45 +0200

    Solved 1076/A.

---
+++ src/codeforces/contests1001_1100/problemset1076/MinimizingTheString.java
@@ -0,0 +1,76 @@
+package codeforces.contests1001_1100.problemset1076;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MinimizingTheString implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int n = in.ni();
+        char[] x = in.next().toCharArray();
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (x[i] > x[idx]) {
+                idx = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i != idx) {
+                out.print(x[i]);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MinimizingTheString instance = new MinimizingTheString()) {
+            instance.solve();
+        }
+    }
+}
