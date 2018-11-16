a19ddabbcdc10d555151cd5d4e3af07e1f900ed1
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2018-11-16 18:38:36 +0200

    Solved 1077/A.

---
+++ src/codeforces/contests1001_1100/problemset1077/FrogJumping.java
@@ -0,0 +1,76 @@
+package codeforces.contests1001_1100.problemset1077;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FrogJumping implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        int t = in.ni();
+        while (t-- > 0) {
+            long a = in.nl(), b = in.nl(), k = in.nl();
+            long result = 0;
+            if (k % 2 == 1) {
+                long right = (k + 1) / 2, left = (k - 1) / 2;
+                result = right * a - left * b;
+            } else {
+                long right = k / 2, left = k / 2;
+                result = right * a - left * b;
+            }
+            out.println(result);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (FrogJumping instance = new FrogJumping()) {
+            instance.solve();
+        }
+    }
+}
