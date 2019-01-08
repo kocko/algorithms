447466ba417dd4a44ba4f0a9de15aeda5b342369
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2019-01-08 18:04:12 +0200

    Solved 448/D.

---
+++ src/codeforces/contests401_500/problemset448/MultiplicationTable.java
@@ -0,0 +1,82 @@
+package codeforces.contests401_500.problemset448;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MultiplicationTable implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), m = in.nl(), k = in.nl();
+        long left = 1, right = n * m;
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (!enough(mid, n, m, k)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        out.println(left);
+    }
+
+    private boolean enough(long x, long n, long m, long k) {
+        long count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(x / i, n);
+        }
+        return count >= k;
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (MultiplicationTable instance = new MultiplicationTable()) {
+            instance.solve();
+        }
+    }
+}
