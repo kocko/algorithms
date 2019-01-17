166f69367df6741130faf45ab5728c570915098a
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2019-01-17 11:48:04 +0200

    Solved 1102/A.

---
+++ src/codeforces/contests1101_1200/problemset1102/IntegerSequenceDividing.java
@@ -0,0 +1,65 @@
+package codeforces.contests1101_1200.problemset1102;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class IntegerSequenceDividing implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        long n = in.nl(), sum = (n * (n + 1) / 2);
+        out.println(sum % 2);
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (IntegerSequenceDividing instance = new IntegerSequenceDividing()) {
+            instance.solve();
+        }
+    }
+}
