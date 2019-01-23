98ac3ae33b8a53aa10d84160ea153626d0de7447
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2019-01-23 13:12:51 +0200

    Solved 1104/C.

---
+++ src/codeforces/contests1101_1200/problemset1104/GridGame.java
@@ -0,0 +1,74 @@
+package codeforces.contests1101_1200.problemset1104;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class GridGame implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        int vertical = 1, horizontal = 1;
+        for (char c : x) {
+            if (c == '0') {
+                out.println(vertical + " " + 1);
+                vertical ^= 2;
+            } else {
+                out.println(horizontal++ + " " + 3);
+                if (horizontal == 5) horizontal -= 4;
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (GridGame instance = new GridGame()) {
+            instance.solve();
+        }
+    }
+}
