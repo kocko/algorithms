be4710bd768ed6272168614630ddf12bea93a513
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2019-01-23 13:31:48 +0200

    Solved 1104/B.

---
+++ src/codeforces/contests1101_1200/problemset1104/GameWithString.java
@@ -0,0 +1,76 @@
+package codeforces.contests1101_1200.problemset1104;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class GameWithString implements Closeable {
+
+    private InputReader in = new InputReader(System.in);
+    private PrintWriter out = new PrintWriter(System.out);
+
+    public void solve() {
+        char[] x = in.next().toCharArray();
+        ArrayDeque<Character> deque = new ArrayDeque<>();
+        int pairs = 0;
+        for (char c : x) {
+            if (deque.peekLast() != null && deque.peekLast() == c) {
+                deque.pollLast();
+                pairs++;
+            } else {
+                deque.offerLast(c);
+            }
+        }
+        out.println(pairs % 2 == 0 ? "No" : "Yes");
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int ni() {
+            return Integer.parseInt(next());
+        }
+
+        public long nl() {
+            return Long.parseLong(next());
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (GameWithString instance = new GameWithString()) {
+            instance.solve();
+        }
+    }
+}
