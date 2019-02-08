abf7a1fbaf81f51ced54275e527a429d506fece4
kocko <konstantin.yovkov@gmail.com>
2019-02-08 10:07:49 +0200

    Solved 1110/A.

--- src/codeforces/contests1101_1200/problemset1110/Parity.java
+++ src/codeforces/contests1101_1200/problemset1110/Parity.java
@@ -6,35 +6,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-public class MeaninglessGame implements Closeable {
+public class Parity implements Closeable {
 
   private InputReader in = new InputReader(System.in);
   private PrintWriter out = new PrintWriter(System.out);
 
   public void solve() {
-    final int[] arr = {1, 1, 5, 1, 21, 1, 85, 73, 341, 89, 1365, 1, 5461, 4681, 21845, 1, 87381, 1, 349525, 299593, 1398101, 178481, 5592405, 1082401};
-    Map<Integer, Integer> map = new HashMap<>();
-    int idx = 0;
-    for (int i = 3; i <= (1 << 25) - 1; i = i * 2 + 1) {
-      map.put(i, arr[idx++]);
+    int b = in.ni(), k = in.ni();
+    int n = 0;
+    for (int i = 0; i < k; i++) {
+      int x = in.ni();
+      n = (n * b + x) % 2;
     }
-    int q = in.ni();
-    while (q-- > 0) {
-      int n = in.ni();
-      if (map.containsKey(n)) {
-        out.println(map.get(n));
-      } else {
-        int x = 1;
-        while (x < n) {
-          x = 2 * x + 1;
-        }
-        out.println(x);
-      }
-    }
+    out.println((n % 2 == 0) ? "even" : "odd");
   }
 
   @Override
@@ -77,7 +63,7 @@ public class MeaninglessGame implements Closeable {
   }
 
   public static void main(String[] args) throws IOException {
-    try (MeaninglessGame instance = new MeaninglessGame()) {
+    try (Parity instance = new Parity()) {
       instance.solve();
     }
   }
