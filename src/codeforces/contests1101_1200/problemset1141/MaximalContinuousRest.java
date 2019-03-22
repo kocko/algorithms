7e4deb4ee6c881f0ada3032a11e46991433ff98a
kocko <konstantin.yovkov@gmail.com>
2019-03-22 09:16:12 +0200

    Solved 1141/B.

---
+++ src/codeforces/contests1101_1200/problemset1141/MaximalContinuousRest.java
@@ -0,0 +1,78 @@
+package codeforces.contests1101_1200.problemset1141;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MaximalContinuousRest implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    int[] x = new int[2 * n];
+    for (int i = 0; i < n; i++) {
+      x[i] = x[n + i] = in.ni();
+    }
+    int current = 0, max = 0;
+    for (int i = 0; i < 2 * n; i++) {
+      if (x[i] == 1) {
+        current++;
+      } else {
+        max = Math.max(current, max);
+        current = 0;
+      }
+    }
+    out.println(max);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MaximalContinuousRest instance = new MaximalContinuousRest()) {
+      instance.solve();
+    }
+  }
+}
