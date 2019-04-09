f1926a1cf972679371f09a76153899086040e321
kocko <konstantin.yovkov@gmail.com>
2019-04-09 11:33:18 +0300

    Solved 1140/A.

---
+++ src/codeforces/contests1101_1200/problemset1140/DetectiveBook.java
@@ -0,0 +1,77 @@
+package codeforces.contests1101_1200.problemset1140;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DetectiveBook implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), result = 0;
+    int[] x = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+      x[i] = in.ni();
+    }
+    int idx = 1;
+    while (idx <= n) {
+      int max = x[idx];
+      while (idx <= max) {
+        max = Math.max(max, x[idx++]);
+      }
+      result++;
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (DetectiveBook instance = new DetectiveBook()) {
+      instance.solve();
+    }
+  }
+}
