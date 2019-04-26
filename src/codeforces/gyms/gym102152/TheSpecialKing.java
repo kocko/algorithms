7c547c8a0d2fb21b45a17cf6287e5aa081ed914d
kocko <konstantin.yovkov@gmail.com>
2019-04-26 18:53:08 +0300

    Solved Gym102152/G.

---
+++ src/codeforces/gyms/gym102152/TheSpecialKing.java
@@ -0,0 +1,65 @@
+package codeforces.gyms.gym102152;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.abs;
+
+public class TheSpecialKing implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int x1 = in.ni(), y1 = in.ni(), x2 = in.ni(), y2 = in.ni();
+      out.println(abs(x1 - x2) + abs(y1 - y2));
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (TheSpecialKing instance = new TheSpecialKing()) {
+      instance.solve();
+    }
+  }
+}
