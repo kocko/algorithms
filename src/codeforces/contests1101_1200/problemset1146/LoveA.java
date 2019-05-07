3ea81c1aee0d32524c43606d795bc613daff098f
kocko <konstantin.yovkov@gmail.com>
2019-05-07 16:49:24 +0300

    Solved 1146/A.

---
+++ src/codeforces/contests1101_1200/problemset1146/LoveA.java
@@ -0,0 +1,75 @@
+package codeforces.contests1101_1200.problemset1146;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class LoveA implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    char[] x = in.next().toCharArray();
+    int a = 0, n = x.length;
+    for (char c : x) {
+      if (c == 'a') {
+        a++;
+      }
+    }
+    if (2 * a > n) {
+      out.println(n);
+    } else {
+      out.println(2 * a - 1);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (LoveA instance = new LoveA()) {
+      instance.solve();
+    }
+  }
+}
