8181b7e526d65f52392921bd6d0a222a3b1b102a
kocko <konstantin.yovkov@gmail.com>
2019-05-15 19:08:07 +0300

    Solved 1167/A.

---
+++ src/codeforces/contests1101_1200/problemset1167/TelephoneNumber.java
@@ -0,0 +1,73 @@
+package codeforces.contests1101_1200.problemset1167;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TelephoneNumber implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      char[] x = in.next().toCharArray();
+      int i;
+      for (i = 0; i < n; i++) {
+        if (x[i] == '8') break;
+      }
+      out.println(n - i >= 11 ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (TelephoneNumber instance = new TelephoneNumber()) {
+      instance.solve();
+    }
+  }
+}
