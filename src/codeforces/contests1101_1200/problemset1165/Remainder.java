6e3fde686132590e33f95b27a5db8f447fcfcca6
kocko <konstantin.yovkov@gmail.com>
2019-06-04 12:39:17 +0300

    Solved 1165/A.

---
+++ src/codeforces/contests1101_1200/problemset1165/Remainder.java
@@ -0,0 +1,72 @@
+package codeforces.contests1101_1200.problemset1165;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Remainder implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), x = in.ni(), y = in.ni(), result = 0;
+    char[] num = in.next().toCharArray();
+    for (int i = 0; i < x; i++) {
+      int value = num[n - i - 1] - '0', expected = i == y ? 1 : 0;
+      if (value != expected) {
+        result++;
+      }
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Remainder instance = new Remainder()) {
+      instance.solve();
+    }
+  }
+}
