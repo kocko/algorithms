1f0dd35ab0e2909106c3dcbc97eaf736c6743874
kocko <konstantin.yovkov@gmail.com>
2019-06-13 11:39:15 +0300

    Solved 1176/B.

---
+++ src/codeforces/contests1101_1200/problemset1176/MergeIt.java
@@ -0,0 +1,80 @@
+package codeforces.contests1101_1200.problemset1176;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MergeIt implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      int[] rem = new int[3];
+      for (int i = 0; i < n; i++) {
+        int next = in.ni();
+        rem[next % 3]++;
+      }
+      int result = rem[0];
+      int pairs = Math.min(rem[1], rem[2]);
+      result += pairs;
+      rem[1] -= pairs;
+      rem[2] -= pairs;
+      result += rem[1] / 3;
+      result += rem[2] / 3;
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MergeIt instance = new MergeIt()) {
+      instance.solve();
+    }
+  }
+}
