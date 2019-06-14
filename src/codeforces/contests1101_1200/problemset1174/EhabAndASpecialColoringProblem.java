57509949f87384a89fac31e4e7ae1cf33143c683
kocko <konstantin.yovkov@gmail.com>
2019-06-14 16:08:13 +0300

    Solved 1174/C.

---
+++ src/codeforces/contests1101_1200/problemset1174/EhabAndASpecialColoringProblem.java
@@ -0,0 +1,78 @@
+package codeforces.contests1101_1200.problemset1174;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class EhabAndASpecialColoringProblem implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    int[] color = new int[n + 1];
+    int c = 1;
+    for (int i = 2; i <= n; i++) {
+      if (color[i] == 0) {
+        for (int j = i; j <= n; j += i) {
+          color[j] = c;
+        }
+        c++;
+      }
+    }
+    for (int i = 2; i <= n; i++) {
+      out.print(color[i]);
+      out.print(' ');
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (EhabAndASpecialColoringProblem instance = new EhabAndASpecialColoringProblem()) {
+      instance.solve();
+    }
+  }
+}
