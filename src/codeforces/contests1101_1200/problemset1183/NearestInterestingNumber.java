d19b655426825bb98dd83580bee025695289329d
kocko <konstantin.yovkov@gmail.com>
2019-06-26 17:15:39 +0300

    Solved 1183/A.

---
+++ src/codeforces/contests1101_1200/problemset1183/NearestInterestingNumber.java
@@ -0,0 +1,77 @@
+package codeforces.contests1101_1200.problemset1183;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class NearestInterestingNumber implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    while (sum(n) % 4 != 0) {
+      n++;
+    }
+    out.println(n);
+  }
+
+  private int sum(int n) {
+    int sum = 0;
+    while (n > 0) {
+      sum += n % 10;
+      n /= 10;
+    }
+    return sum;
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (NearestInterestingNumber instance = new NearestInterestingNumber()) {
+      instance.solve();
+    }
+  }
+}
