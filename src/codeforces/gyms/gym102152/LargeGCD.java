7b6ae99374c5661445a2faf92736411d42154baf
kocko <konstantin.yovkov@gmail.com>
2019-08-03 19:24:51 +0300

    Solved Gym102152/C.

---
+++ src/codeforces/gyms/gym102152/LargeGCD.java
@@ -0,0 +1,63 @@
+package codeforces.gyms.gym102152;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class LargeGCD implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int sum = (in.ni() + in.ni()) % 2;
+      out.println(sum == 0 ? 12 : 2);
+    }
+  }
+  
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (LargeGCD instance = new LargeGCD()) {
+      instance.solve();
+    }
+  }
+}
