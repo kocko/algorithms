93ba0f5cf792481220f5f774c54c4f31e7dff4a2
kocko <konstantin.yovkov@gmail.com>
2019-09-04 15:51:35 +0300

    Solved 1214/A.

---
+++ src/codeforces/contests1201_1300/problemset1214/OptimalCurrencyExchange.java
@@ -0,0 +1,71 @@
+package codeforces.contests1201_1300.problemset1214;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.min;
+
+public class OptimalCurrencyExchange implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), d = in.ni(), e = in.ni(), result = Integer.MAX_VALUE;
+    for (int x = 0; n >= 5 * x * e; x++) {
+      int dollars = n - 5 * x * e, rem = dollars % d;
+      result = min(result, rem);
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (OptimalCurrencyExchange instance = new OptimalCurrencyExchange()) {
+      instance.solve();
+    }
+  }
+}
