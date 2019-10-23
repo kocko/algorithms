ad81ddbc897422123f8956a6fdb6d0311f2a5678
kocko <konstantin.yovkov@gmail.com>
2019-10-23 20:40:04 +0300

    Solved Gym102215/I.

---
+++ src/codeforces/gyms/gym102215/PaintingASquare.java
@@ -0,0 +1,75 @@
+package codeforces.gyms.gym102215;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PaintingASquare implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    long a = in.nl(), b = in.nl();
+    long result = 0;
+    while (a > 0) {
+      if (2 * b >= a) {
+        result += 3 * (a - b);
+        break;
+      } else {
+        result += 4 * (a - b);
+        a -= 2 * b;
+      }
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (PaintingASquare instance = new PaintingASquare()) {
+      instance.solve();
+    }
+  }
+}
