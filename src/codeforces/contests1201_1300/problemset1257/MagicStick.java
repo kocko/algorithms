eedbf574ff525d6ecba9ef736fb65c35b5d2c65e
kocko <konstantin.yovkov@gmail.com>
2019-11-13 18:02:36 +0200

    Solved 1257/B.

---
+++ src/codeforces/contests1201_1300/problemset1257/MagicStick.java
@@ -0,0 +1,71 @@
+package codeforces.contests1201_1300.problemset1257;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MagicStick implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int x = in.ni(), y = in.ni();
+      boolean bad = false;
+      if (x == 1) bad = y != 1;
+      else if (x == 2 || x == 3) bad = y > 3;
+      out.println(bad ? "NO" : "YES");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MagicStick instance = new MagicStick()) {
+      instance.solve();
+    }
+  }
+}
