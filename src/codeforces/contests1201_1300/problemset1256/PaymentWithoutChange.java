a49acc75f460aee8f3c0535529ff4dfde75d6a63
kocko <konstantin.yovkov@gmail.com>
2019-11-15 17:59:03 +0200

    Solved 1256/A.

---
+++ src/codeforces/contests1201_1300/problemset1256/PaymentWithoutChange.java
@@ -0,0 +1,72 @@
+package codeforces.contests1201_1300.problemset1256;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class PaymentWithoutChange implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long a = in.nl(), ones = in.nl(), n = in.nl(), S = in.nl();
+      if (S % n <= ones && a * n + ones >= S) {
+        out.println("YES");
+      } else {
+        out.println("NO");
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (PaymentWithoutChange instance = new PaymentWithoutChange()) {
+      instance.solve();
+    }
+  }
+}
