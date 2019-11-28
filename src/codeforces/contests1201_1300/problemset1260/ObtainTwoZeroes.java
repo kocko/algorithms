cfb1343c95edf6d8290bb65892a853a414bbe48d
kocko <konstantin.yovkov@gmail.com>
2019-11-28 09:49:53 +0200

    Solved 1260/B.

---
+++ src/codeforces/contests1201_1300/problemset1260/ObtainTwoZeroes.java
@@ -0,0 +1,75 @@
+package codeforces.contests1201_1300.problemset1260;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ObtainTwoZeroes implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int a = in.ni(), b = in.ni();
+      boolean ok = true;
+      if ((a + b) % 3 != 0) {
+        ok = false;
+      }
+      if (a > 2 * b || b > 2 * a) {
+        ok = false;
+      }
+      out.println(ok ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (ObtainTwoZeroes instance = new ObtainTwoZeroes()) {
+      instance.solve();
+    }
+  }
+}
