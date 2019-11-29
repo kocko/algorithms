af03df97f9aef1a9296260093f96b6ef209ec004
kocko <konstantin.yovkov@gmail.com>
2019-11-29 14:26:37 +0200

    Solved 1249/A.

---
+++ src/codeforces/contests1201_1300/problemset1249/YetAnotherDividingIntoTeams.java
@@ -0,0 +1,81 @@
+package codeforces.contests1201_1300.problemset1249;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class YetAnotherDividingIntoTeams implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      Arrays.sort(x);
+      int ans = 1;
+      for (int i = 1; i < n; i++) {
+        if (x[i] == x[i - 1] + 1) {
+          ans = 2;
+          break;
+        }
+      }
+      out.println(ans);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (YetAnotherDividingIntoTeams instance = new YetAnotherDividingIntoTeams()) {
+      instance.solve();
+    }
+  }
+}
