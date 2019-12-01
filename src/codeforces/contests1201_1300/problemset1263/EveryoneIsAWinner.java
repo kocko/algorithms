1460e1070081ad9b613a82cacf842b78988bd333
kocko <konstantin.yovkov@gmail.com>
2019-12-01 12:23:43 +0200

    Solved 1263/C.

---
+++ src/codeforces/contests1201_1300/problemset1263/EveryoneIsAWinner.java
@@ -0,0 +1,81 @@
+package codeforces.contests1201_1300.problemset1263;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class EveryoneIsAWinner implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      Set<Integer> result = new TreeSet<>();
+      result.add(0);
+      for (int i = 1; i * i <= n; i++) {
+        result.add(i);
+        result.add(n / i);
+      }
+      out.println(result.size());
+      for (int value : result) {
+        out.print(value);
+        out.print(' ');
+      }
+      out.println();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (EveryoneIsAWinner instance = new EveryoneIsAWinner()) {
+      instance.solve();
+    }
+  }
+}
