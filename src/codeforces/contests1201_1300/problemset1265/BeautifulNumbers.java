6bcc6b9d0e6a6d0192056bd05b80afc1407d7112
kocko <konstantin.yovkov@gmail.com>
2019-12-04 16:51:12 +0200

    Solved 1265/B.

---
+++ src/codeforces/contests1201_1300/problemset1265/BeautifulNumbers.java
@@ -0,0 +1,84 @@
+package codeforces.contests1201_1300.problemset1265;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BeautifulNumbers implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      int[] index = new int[n + 1];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+        index[x[i]] = i;
+      }
+      int min = (int) 2e5 + 5, max = -1;
+      for (int i = 1; i <= n; i++) {
+        min = Math.min(min, index[i]);
+        max = Math.max(max, index[i]);
+        if (max - min + 1 == i) {
+          out.print(1);
+        } else {
+          out.print(0);
+        }
+      }
+      out.println();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BeautifulNumbers instance = new BeautifulNumbers()) {
+      instance.solve();
+    }
+  }
+}
