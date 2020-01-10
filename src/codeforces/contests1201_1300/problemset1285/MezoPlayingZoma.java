8015f82ac751ca281d7279b265eac4098dab4c3f
kocko <konstantin.yovkov@gmail.com>
2020-01-10 18:23:22 +0200

    Solved 1285/A.

---
+++ src/codeforces/contests1201_1300/problemset1285/MezoPlayingZoma.java
@@ -0,0 +1,71 @@
+package codeforces.contests1201_1300.problemset1285;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MezoPlayingZoma implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    char[] x = in.next().toCharArray();
+    int left = 0, right = 0;
+    for (char c : x) {
+      if (c == 'R') right++;
+      else left--;
+    }
+    out.println(right - left + 1);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MezoPlayingZoma instance = new MezoPlayingZoma()) {
+      instance.solve();
+    }
+  }
+}
