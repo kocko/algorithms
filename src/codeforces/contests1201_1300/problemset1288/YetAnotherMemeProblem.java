43f895568b2d3100b821314199321192f344e5d6
kocko <konstantin.yovkov@gmail.com>
2020-01-15 17:10:16 +0200

    Solved 1288/B.

---
+++ src/codeforces/contests1201_1300/problemset1288/YetAnotherMemeProblem.java
@@ -0,0 +1,72 @@
+package codeforces.contests1201_1300.problemset1288;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class YetAnotherMemeProblem implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int A = in.ni(), B = in.ni();
+      long result = 0;
+      for (int b = 9; b <= B; b = (b * 10) + 9) {
+        result += A;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (YetAnotherMemeProblem instance = new YetAnotherMemeProblem()) {
+      instance.solve();
+    }
+  }
+}
