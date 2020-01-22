2794d5cfdf4b51b8cb78b93b242185eeea241e47
kocko <konstantin.yovkov@gmail.com>
2020-01-22 15:41:11 +0200

    Solved 1287/A.

---
+++ src/codeforces/contests1201_1300/problemset1287/AngryStudents.java
@@ -0,0 +1,78 @@
+package codeforces.contests1201_1300.problemset1287;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AngryStudents implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      char[] x = in.next().toCharArray();
+      int result = 0, current = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        if (x[i] == 'P') {
+          current++;
+        } else {
+          result = Math.max(current, result);
+          current = 0;
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (AngryStudents instance = new AngryStudents()) {
+      instance.solve();
+    }
+  }
+}
