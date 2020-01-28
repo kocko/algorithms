6755d57fc16a262345255bbba5ce551ccf70e1d2
kocko <konstantin.yovkov@gmail.com>
2020-01-28 17:52:43 +0200

    Solved 1294/D.

---
+++ src/codeforces/contests1201_1300/problemset1294/MexMaximizing.java
@@ -0,0 +1,75 @@
+package codeforces.contests1201_1300.problemset1294;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MexMaximizing implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int q = in.ni(), x = in.ni();
+    int mex = 0;
+    int[] cnt = new int[x];
+    for (int i = 0; i < q; i++) {
+      int next = in.ni();
+      cnt[next % x]++;
+      while (cnt[mex % x] != 0) {
+        cnt[mex % x]--;
+        mex++;
+      }
+      out.println(mex);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MexMaximizing instance = new MexMaximizing()) {
+      instance.solve();
+    }
+  }
+}
