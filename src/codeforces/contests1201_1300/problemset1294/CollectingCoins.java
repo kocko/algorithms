02d5dda65d1977a8164976eb23b29e8522d35ac8
kocko <konstantin.yovkov@gmail.com>
2020-01-28 18:31:39 +0200

    Solved 1294/A.

---
+++ src/codeforces/contests1201_1300/problemset1294/CollectingCoins.java
@@ -0,0 +1,77 @@
+package codeforces.contests1201_1300.problemset1294;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CollectingCoins implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int a = in.ni(), b = in.ni(), c = in.ni(), n = in.ni();
+      boolean ok = true;
+      if ((a + b + c + n) % 3 == 0) {
+        int value = (a + b + c + n) / 3;
+        for (int sister : new int[]{a, b, c}) {
+          ok &= sister <= value;
+        }
+      } else {
+        ok = false;
+      }
+      out.println(ok ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CollectingCoins instance = new CollectingCoins()) {
+      instance.solve();
+    }
+  }
+}
