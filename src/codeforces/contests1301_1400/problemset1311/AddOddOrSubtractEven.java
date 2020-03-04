69d716099750abb72ec73c1b80ad50ce8c29002f
kocko <konstantin.yovkov@gmail.com>
2020-03-04 12:18:21 +0200

    Solved 1311/A.

---
+++ src/codeforces/contests1301_1400/problemset1311/AddOddOrSubtractEven.java
@@ -0,0 +1,78 @@
+package codeforces.contests1301_1400.problemset1311;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AddOddOrSubtractEven implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int a = in.ni(), b = in.ni();
+      if (a == b) {
+        out.println(0);
+      } else {
+        if (a < b) {
+          int diff = b - a;
+          out.println(2 - (diff % 2));
+        } else {
+          int diff = a - b;
+          out.println(1 + (diff % 2));
+        }
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (AddOddOrSubtractEven instance = new AddOddOrSubtractEven()) {
+      instance.solve();
+    }
+  }
+}
