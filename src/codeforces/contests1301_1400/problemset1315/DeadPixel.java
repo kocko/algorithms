bb764d68e8b609cfb8c595231a40f05e0ebd933f
kocko <konstantin.yovkov@gmail.com>
2020-03-24 19:01:13 +0200

    Solved 1315/A.

---
+++ src/codeforces/contests1301_1400/problemset1315/DeadPixel.java
@@ -0,0 +1,75 @@
+package codeforces.contests1301_1400.problemset1315;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Long.max;
+
+public class DeadPixel implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long a = in.nl(), b = in.nl(), x = in.nl(), y = in.nl();
+      long result = 0;
+      result = max(result, b * (a - x - 1));
+      result = max(result, a * (b - y - 1));
+      result = max(result, a * y);
+      result = max(result, b * x);
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (DeadPixel instance = new DeadPixel()) {
+      instance.solve();
+    }
+  }
+}
