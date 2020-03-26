db1eaa43097f1c300484c9cd9d50a5f239d15bca
kocko <konstantin.yovkov@gmail.com>
2020-03-26 19:30:20 +0200

    Solved 1328/A.

---
+++ src/codeforces/contests1301_1400/problemset1328/DivisibilityProblem.java
@@ -0,0 +1,77 @@
+package codeforces.contests1301_1400.problemset1328;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DivisibilityProblem implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int a = in.ni(), b = in.ni();
+      int result = 0;
+      if (a < b) {
+        result = b - a;
+      } else {
+        int rem = a % b;
+        if (rem != 0) {
+          result = b - rem;
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (DivisibilityProblem instance = new DivisibilityProblem()) {
+      instance.solve();
+    }
+  }
+}
