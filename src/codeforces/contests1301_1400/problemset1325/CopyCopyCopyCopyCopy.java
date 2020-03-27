b9fab7c2d5bef840e99c8bbbd0bb7a7c35c742bf
kocko <konstantin.yovkov@gmail.com>
2020-03-27 16:59:03 +0200

    Solved 1325/B.

---
+++ src/codeforces/contests1301_1400/problemset1325/CopyCopyCopyCopyCopy.java
@@ -0,0 +1,74 @@
+package codeforces.contests1301_1400.problemset1325;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class CopyCopyCopyCopyCopy implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      Set<Integer> set = new HashSet<>();
+      for (int i = 0; i < n; i++) {
+        set.add(in.ni());
+      }
+      out.println(set.size());
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CopyCopyCopyCopyCopy instance = new CopyCopyCopyCopyCopy()) {
+      instance.solve();
+    }
+  }
+}
