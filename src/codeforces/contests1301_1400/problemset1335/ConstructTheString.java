e4a63ab004c2cb3655821d46403197c57f5f7d07
kocko <konstantin.yovkov@gmail.com>
2020-04-13 19:57:43 +0300

    Solved 1335/B.

---
+++ src/codeforces/contests1301_1400/problemset1335/ConstructTheString.java
@@ -0,0 +1,79 @@
+package codeforces.contests1301_1400.problemset1335;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class ConstructTheString implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), a = in.ni(), b = in.ni();
+      char[] result = new char[n];
+      for (int i = 0; i < a; i++) {
+        int rem = i % b;
+        result[i] = (char) (rem + 'a');
+      }
+      for (int i = b; i < n; i++) {
+        result[i] = result[i - b];
+      }
+      for (int i = 0; i < n; i++) {
+        out.print(result[i]);
+      }
+      out.println();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (ConstructTheString instance = new ConstructTheString()) {
+      instance.solve();
+    }
+  }
+}
