5b9b0dfb84f73d83f913bd53a54027a02061d2af
kocko <konstantin.yovkov@gmail.com>
2020-04-13 19:57:19 +0300

    Solved 1335/A.

---
+++ src/codeforces/contests1301_1400/problemset1335/CandiesAndTwoSisters.java
@@ -0,0 +1,72 @@
+package codeforces.contests1301_1400.problemset1335;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CandiesAndTwoSisters implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      if (n % 2 == 0) {
+        out.println(n / 2 - 1);
+      } else {
+        out.println(n / 2);
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CandiesAndTwoSisters instance = new CandiesAndTwoSisters()) {
+      instance.solve();
+    }
+  }
+}
