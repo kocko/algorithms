7707798c2e805a431dfc287e2b671b175b99a4df
kocko <konstantin.yovkov@gmail.com>
2020-04-20 17:03:47 +0300

    Solved ABC043/C.

---
+++ src/atcoder/beginner/contest043/BeTogether.java
@@ -0,0 +1,77 @@
+package atcoder.beginner.contest043;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BeTogether implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    int[] x = new int[n];
+    for (int i = 0; i < n; i++) {
+      x[i] = in.ni();
+    }
+    int result = Integer.MAX_VALUE;
+    for (int target = -101; target <= 101; target++) {
+      int cost = 0;
+      for (int i = 0; i < n; i++) {
+        cost += (target - x[i]) * (target - x[i]);
+      }
+      result = Math.min(result, cost);
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BeTogether instance = new BeTogether()) {
+      instance.solve();
+    }
+  }
+}
