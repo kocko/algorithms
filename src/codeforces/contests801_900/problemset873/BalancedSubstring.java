505461468334a8ce5df963c2b5f95bd29a49ef5b
kocko <konstantin.yovkov@gmail.com>
2020-05-04 15:55:52 +0300

    Solved 873/B.

---
+++ src/codeforces/contests801_900/problemset873/BalancedSubstring.java
@@ -0,0 +1,81 @@
+package codeforces.contests801_900.problemset873;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BalancedSubstring implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    char[] x = in.next().toCharArray();
+    int current = 0, result = 0;
+    Map<Integer, Integer> balance = new HashMap<>();
+    balance.put(0, 0);
+    for (int idx = 1; idx <= n; idx++) {
+      if (x[idx - 1] == '1') {
+        current++;
+      } else {
+        current--;
+      }
+      int firstIndex = balance.getOrDefault(current, idx);
+      result = Math.max(result, idx - firstIndex);
+      balance.putIfAbsent(current, idx);
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BalancedSubstring instance = new BalancedSubstring()) {
+      instance.solve();
+    }
+  }
+}
