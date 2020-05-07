f299f16b18e85fd39d16e852ab592bf872946bae
kocko <konstantin.yovkov@gmail.com>
2020-05-07 19:43:49 +0300

    Solved 1351/B.

---
+++ src/codeforces/contests1301_1400/problemset1351/Square.java
@@ -0,0 +1,74 @@
+package codeforces.contests1301_1400.problemset1351;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Square implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int a = in.ni(), b = in.ni();
+      int c = in.ni(), d = in.ni();
+      boolean ok = false;
+      if (a == c && b + d == a) ok = true;
+      if (a == d && b + c == a) ok = true;
+      if (b == c && a + d == b) ok = true;
+      if (b == d && a + c == b) ok = true;
+      out.println(ok ? "Yes" : "No");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Square instance = new Square()) {
+      instance.solve();
+    }
+  }
+}
