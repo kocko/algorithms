fe46175aa0ceff5c2255ba0a711fb0be44b1d3cb
kocko <konstantin.yovkov@gmail.com>
2020-05-08 23:18:08 +0300

    Solved 1352/A.

---
+++ src/codeforces/contests1301_1400/problemset1352/SumOfRoundNumbers.java
@@ -0,0 +1,83 @@
+package codeforces.contests1301_1400.problemset1352;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class SumOfRoundNumbers implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      List<Integer> result = new ArrayList<>();
+      for (int d = 10; d <= 100000 && n > 0; d *= 10) {
+        if (n % d != 0) {
+          result.add(n % d);
+          n -= n % d;
+        }
+      }
+
+      out.println(result.size());
+      for (int value : result) {
+        out.print(value);
+        out.print(' ');
+      }
+      out.println();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SumOfRoundNumbers instance = new SumOfRoundNumbers()) {
+      instance.solve();
+    }
+  }
+}
