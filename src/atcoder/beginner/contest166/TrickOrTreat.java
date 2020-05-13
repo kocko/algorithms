57d6ace180222b0e8e9da343336a015dd632d2e0
kocko <konstantin.yovkov@gmail.com>
2020-05-13 15:37:37 +0300

    Solved ABC166/B.

---
+++ src/atcoder/beginner/contest166/TrickOrTreat.java
@@ -0,0 +1,77 @@
+package atcoder.beginner.contest166;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TrickOrTreat implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), k = in.ni();
+    boolean[] has = new boolean[n + 1];
+    int result = n;
+    for (int i = 0; i < k; i++) {
+      int d = in.ni();
+      for (int j = 0; j < d; j++) {
+        int next = in.ni();
+        if (!has[next]) {
+          result--;
+        }
+        has[next] = true;
+      }
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (TrickOrTreat instance = new TrickOrTreat()) {
+      instance.solve();
+    }
+  }
+}
