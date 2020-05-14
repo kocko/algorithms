658cd650b163e614096ea5f8941148d2ea3a0651
kocko <konstantin.yovkov@gmail.com>
2020-05-14 19:45:22 +0300

    Solved 1353/C.

---
+++ src/codeforces/contests1301_1400/problemset1353/BoardMoves.java
@@ -0,0 +1,74 @@
+package codeforces.contests1301_1400.problemset1353;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BoardMoves implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long n = in.nl();
+      long result = 0L;
+      long count = 4 * n - 4;
+      for (long side = n; side > 1; side -= 2) {
+        result += count;
+        count += side - 2 + side - 2 + side - 4 + side - 4;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BoardMoves instance = new BoardMoves()) {
+      instance.solve();
+    }
+  }
+}
