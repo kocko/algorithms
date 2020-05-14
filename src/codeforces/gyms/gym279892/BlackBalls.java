ffa3b26e7e164c13598b00dcc14e5fc4a7076fd9
kocko <konstantin.yovkov@gmail.com>
2020-05-14 08:33:38 +0300

    Solved Gym 279892/B.

---
+++ src/codeforces/gyms/gym279892/BlackBalls.java
@@ -0,0 +1,78 @@
+package codeforces.gyms.gym279892;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BlackBalls implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), k = in.ni(), result = 0;
+      boolean[] has = new boolean[k + 1];
+      for (int i = 1; i <= k; i++) {
+        int next = in.ni();
+        if (next <= k) {
+          has[next] = true;
+        }
+      }
+      for (int i = 1; i <= k; i++) {
+        if (!has[i]) result++;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BlackBalls instance = new BlackBalls()) {
+      instance.solve();
+    }
+  }
+}
