33af7c6c89dc3172b5e7740e70daa6181ea78c10
kocko <konstantin.yovkov@gmail.com>
2020-05-25 16:30:20 +0300

    Solved 1360/A.

---
+++ src/codeforces/contests1301_1400/problemset1360/MinimalSquare.java
@@ -0,0 +1,79 @@
+package codeforces.contests1301_1400.problemset1360;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MinimalSquare implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int a = in.ni(), b = in.ni();
+      int result = Integer.MAX_VALUE;
+      if (2 * a >= b) {
+        result = Math.min(result, 4 * a * a);
+      } else {
+        result = Math.min(result, b * b);
+      }
+      if (2 * b >= a) {
+        result = Math.min(result, 4 * b * b);
+      } else {
+        result = Math.min(result, a * a);
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MinimalSquare instance = new MinimalSquare()) {
+      instance.solve();
+    }
+  }
+}
