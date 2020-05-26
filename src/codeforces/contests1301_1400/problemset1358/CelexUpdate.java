1c05363f835f130f6838941c2b45370606e7836f
kocko <konstantin.yovkov@gmail.com>
2020-05-26 21:26:27 +0300

    Solved 1358/C.

---
+++ src/codeforces/contests1301_1400/problemset1358/CelexUpdate.java
@@ -0,0 +1,68 @@
+package codeforces.contests1301_1400.problemset1358;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CelexUpdate implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long x1 = in.nl(), y1 = in.nl(), x2 = in.nl(), y2 = in.nl();
+      out.println((x2 - x1) * (y2 - y1) + 1L);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CelexUpdate instance = new CelexUpdate()) {
+      instance.solve();
+    }
+  }
+}
