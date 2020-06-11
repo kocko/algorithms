976fecea5b3ed9c2d09c4ec35c9f3ea9d2c81dad
kocko <konstantin.yovkov@gmail.com>
2020-06-11 20:35:23 +0300

    Solved 1366/B.

---
+++ src/codeforces/contests1301_1400/problemset1366/Shuffle.java
@@ -0,0 +1,76 @@
+package codeforces.contests1301_1400.problemset1366;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Shuffle implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), x = in.ni(), m = in.ni();
+      int min = x, max = x;
+      while (m-- > 0) {
+        int left = in.ni(), right = in.ni();
+        if (left <= max && right >= min) {
+          min = Math.min(min, left);
+          max = Math.max(max, right);
+        }
+      }
+      out.println(max - min + 1);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Shuffle instance = new Shuffle()) {
+      instance.solve();
+    }
+  }
+}
