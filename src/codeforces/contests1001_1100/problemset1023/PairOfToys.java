0246297df754d46aa9d9180660f36f41e2a2ba1e
kocko <konstantin.yovkov@gmail.com>
2020-06-30 18:27:49 +0300

    Solved 1023/B.

---
+++ src/codeforces/contests1001_1100/problemset1023/PairOfToys.java
@@ -0,0 +1,67 @@
+package codeforces.contests1001_1100.problemset1023;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.min;
+
+public class PairOfToys implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    long n = in.nl(), k = in.nl();
+    out.println(min(n, k - 1) - min(n, k / 2));
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (PairOfToys instance = new PairOfToys()) {
+      instance.solve();
+    }
+  }
+}
