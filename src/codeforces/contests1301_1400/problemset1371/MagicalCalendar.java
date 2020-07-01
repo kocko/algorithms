a59431b08d577757b79d1c5d3ff3efd24e3311bf
kocko <konstantin.yovkov@gmail.com>
2020-07-01 18:35:32 +0300

    Solved 1371/B.

---
+++ src/codeforces/contests1301_1400/problemset1371/MagicalCalendar.java
@@ -0,0 +1,74 @@
+package codeforces.contests1301_1400.problemset1371;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MagicalCalendar implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long n = in.nl(), r = in.nl();
+      long result;
+      if (n <= r) {
+        result = (n - 1L) * n / 2 + 1;
+      } else {
+        result = r * (r + 1L) / 2L;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MagicalCalendar instance = new MagicalCalendar()) {
+      instance.solve();
+    }
+  }
+}
