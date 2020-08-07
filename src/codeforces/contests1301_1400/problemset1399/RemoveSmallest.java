47d6b0116f00e2b358ba04245e5d6046c88a35f3
kocko <konstantin.yovkov@gmail.com>
2020-08-07 17:25:37 +0300

    Solved 1399/A.

---
+++ src/codeforces/contests1301_1400/problemset1399/RemoveSmallest.java
@@ -0,0 +1,78 @@
+package codeforces.contests1301_1400.problemset1399;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class RemoveSmallest implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      Arrays.sort(x);
+      boolean ok = true;
+      for (int i = 1; i < n; i++) {
+        ok &= x[i] - x[i - 1] <= 1;
+      }
+      out.println(ok ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (RemoveSmallest instance = new RemoveSmallest()) {
+      instance.solve();
+    }
+  }
+}
