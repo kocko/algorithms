88ad510704bc1f1afafd2bc8336ed7aa60b0e7b1
kocko <konstantin.yovkov@gmail.com>
2020-08-14 18:19:30 +0300

    Solved 1398/A.

---
+++ src/codeforces/contests1301_1400/problemset1398/BadTriangle.java
@@ -0,0 +1,80 @@
+package codeforces.contests1301_1400.problemset1398;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BadTriangle implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      if (isTriangle(x[0], x[1], x[n - 1])) {
+        out.println(-1);
+      } else {
+        out.println("1 2 " + n);
+      }
+    }
+  }
+
+  private boolean isTriangle(int a, int b, int c) {
+    return a + b > c && a + c > b && b + c > a;
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BadTriangle instance = new BadTriangle()) {
+      instance.solve();
+    }
+  }
+}
