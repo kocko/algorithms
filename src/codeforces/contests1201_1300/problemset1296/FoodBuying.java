0ffcd3dc70c5ff9163043fc58501f89d3f22af99
kocko <konstantin.yovkov@gmail.com>
2020-08-14 17:30:00 +0300

    Solved 1296/B.

---
+++ src/codeforces/contests1201_1300/problemset1296/FoodBuying.java
@@ -0,0 +1,75 @@
+package codeforces.contests1201_1300.problemset1296;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FoodBuying implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long n = in.nl(), spent = 0;
+      while ((n - n % 10) > 0) {
+        spent += n - n % 10;
+        n = (n % 10) + (n / 10);
+      }
+      if (n > 0) {
+        spent += n;
+      }
+      out.println(spent);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (FoodBuying instance = new FoodBuying()) {
+      instance.solve();
+    }
+  }
+}
