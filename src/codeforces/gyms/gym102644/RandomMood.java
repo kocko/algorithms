69f3ac751987a988c4094f7ee50070d0678ead66
kocko <konstantin.yovkov@gmail.com>
2020-09-09 18:13:24 +0300

    Solved Gym102644/A.

---
+++ src/codeforces/gyms/gym102644/RandomMood.java
@@ -0,0 +1,74 @@
+package codeforces.gyms.gym102644;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class RandomMood implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    double p = Double.parseDouble(in.next());
+    double happy = 1d;
+    while (n > 0) {
+      if (n % 2 == 1) {
+        happy = happy * (1 - p) + (1 - happy) * p;
+      }
+      p = p * (1 - p) + (1 - p) * p;
+      n >>= 1;
+    }
+    out.println(happy);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (RandomMood instance = new RandomMood()) {
+      instance.solve();
+    }
+  }
+}
