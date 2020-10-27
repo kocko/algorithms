2415812c38ad9a5ee06085eceb2070d14282ac94
kocko <konstantin.yovkov@gmail.com>
2020-10-27 16:32:56 +0200

    Solved 1430/B.

---
+++ src/codeforces/contests1401_1500/problemset1430/Barrels.java
@@ -0,0 +1,80 @@
+package codeforces.contests1401_1500.problemset1430;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Barrels implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), k = in.ni();
+      List<Integer> list = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+        list.add(in.ni());
+      }
+      list.sort(Comparator.reverseOrder());
+      long result = list.get(0);
+      for (int i = 1; i <= k; i++) {
+        result += list.get(i);
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Barrels instance = new Barrels()) {
+      instance.solve();
+    }
+  }
+}
