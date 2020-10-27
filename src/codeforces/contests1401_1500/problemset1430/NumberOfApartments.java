0165fb9a3be8bede02079055d4880cc0650a63fb
kocko <konstantin.yovkov@gmail.com>
2020-10-27 16:32:46 +0200

    Solved 1430/A.

---
+++ src/codeforces/contests1401_1500/problemset1430/NumberOfApartments.java
@@ -0,0 +1,78 @@
+package codeforces.contests1401_1500.problemset1430;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class NumberOfApartments implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      String result = "-1";
+      out: for (int three = 0; three <= n / 3; three++) {
+        for (int five = 0; five <= n / 5; five++) {
+          int seven = n - (three * 3 + five * 5);
+          if (seven >= 0 && seven % 7 == 0) {
+            result = three + " " + five + " " + (seven / 7);
+            break out;
+          }
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (NumberOfApartments instance = new NumberOfApartments()) {
+      instance.solve();
+    }
+  }
+}
