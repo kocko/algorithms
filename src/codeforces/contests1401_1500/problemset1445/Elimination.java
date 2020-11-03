a3ed1f336414bf98095d1a103032edb7cde46f18
kocko <konstantin.yovkov@gmail.com>
2020-11-03 15:35:19 +0200

    Solved 1445/B.

---
+++ src/codeforces/contests1401_1500/problemset1445/Elimination.java
@@ -0,0 +1,71 @@
+package codeforces.contests1401_1500.problemset1445;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Elimination implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int[] x = new int[4];
+      for (int i = 0; i < 4; i++) {
+        x[i] = in.ni();
+      }
+      out.println(Math.max(x[0] + x[1], x[2] + x[3]));
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Elimination instance = new Elimination()) {
+      instance.solve();
+    }
+  }
+}
