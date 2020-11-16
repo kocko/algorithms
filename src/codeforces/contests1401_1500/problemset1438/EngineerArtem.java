261795db8a23d8e80b61c23f80e55f641368259d
kocko <konstantin.yovkov@gmail.com>
2020-11-16 13:39:37 +0200

    Solved 1438/C.

---
+++ src/codeforces/contests1401_1500/problemset1438/EngineerArtem.java
@@ -0,0 +1,83 @@
+package codeforces.contests1401_1500.problemset1438;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class EngineerArtem implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), m = in.ni();
+      int[][] a = new int[n][m];
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          a[i][j] = in.ni();
+        }
+      }
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if ((i + j) % 2 != (a[i][j] % 2)) {
+            a[i][j]++;
+          }
+          out.print(a[i][j]);
+          out.print(' ');
+        }
+        out.println();
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (EngineerArtem instance = new EngineerArtem()) {
+      instance.solve();
+    }
+  }
+}
