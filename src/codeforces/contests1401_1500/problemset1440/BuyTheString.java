da22b837cfa6eb6f19705bea5149334f7efef5c3
kocko <konstantin.yovkov@gmail.com>
2020-11-17 23:26:58 +0200

    Solved 1440/A.

---
+++ src/codeforces/contests1401_1500/problemset1440/BuyTheString.java
@@ -0,0 +1,77 @@
+package codeforces.contests1401_1500.problemset1440;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class BuyTheString implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), c0 = in.ni(), c1 = in.ni(), h = in.ni();
+      char[] x = in.next().toCharArray();
+      int result = 0;
+      for (char c : x) {
+        if (c == '1') {
+          result += Math.min(h + c0, c1);
+        } else {
+          result += Math.min(h + c1, c0);
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BuyTheString instance = new BuyTheString()) {
+      instance.solve();
+    }
+  }
+}
