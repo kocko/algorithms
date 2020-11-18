a09acf4fc6005c9b9aa10a8539a5f873e33bd2f3
kocko <konstantin.yovkov@gmail.com>
2020-11-18 09:59:37 +0200

    Solved 1440/B.

---
+++ src/codeforces/contests1401_1500/problemset1440/SumOfMedians.java
@@ -0,0 +1,82 @@
+package codeforces.contests1401_1500.problemset1440;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class SumOfMedians implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), k = in.ni();
+      List<Long> list = new ArrayList<>();
+      for (int i = 0; i < n * k; i++) {
+        list.add(in.nl());
+      }
+      list.sort(Comparator.naturalOrder());
+      long result = 0;
+      int jump = n - (n + 1) / 2, index = list.size();
+      for (int i = 0; i < k; i++) {
+        index -= jump + 1;
+        result += list.get(index);
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SumOfMedians instance = new SumOfMedians()) {
+      instance.solve();
+    }
+  }
+}
