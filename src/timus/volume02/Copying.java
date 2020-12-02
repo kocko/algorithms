d134a7aef6eb0c5460072ae3ca1a80d8ce0122a5
kocko <konstantin.yovkov@gmail.com>
2020-12-02 13:49:07 +0200

    Solved Timus/1131.

---
+++ src/timus/volume02/Copying.java
@@ -0,0 +1,76 @@
+package timus.volume02;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Copying implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), k = in.ni();
+    int result = 0;
+    int have = 1, step = 1;
+    while (have < n && step < k) {
+      have += step;
+      step = Math.min(have, k);
+      result++;
+    }
+    if (n > have) {
+      int rem = n - have;
+      result += (rem / k) + (rem % k != 0 ? 1 : 0);
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Copying instance = new Copying()) {
+      instance.solve();
+    }
+  }
+}
