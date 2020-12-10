51517131e5260112bd6de57afea93a9a39278647
kocko <konstantin.yovkov@gmail.com>
2020-12-10 12:48:15 +0200

    Solved Gym102873/C.

---
+++ src/codeforces/gyms/gym102873/SimilarArrays.java
@@ -0,0 +1,77 @@
+package codeforces.gyms.gym102873;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.abs;
+
+public class SimilarArrays implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni();
+    int[] x = new int[n];
+    for (int i = 0; i < n; i++) {
+      x[i] = in.ni();
+    }
+    Arrays.sort(x);
+    long result = 0;
+    for (int i = 0; i < n; i++) {
+      result += abs(x[i] - x[n / 2]);
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SimilarArrays instance = new SimilarArrays()) {
+      instance.solve();
+    }
+  }
+}
