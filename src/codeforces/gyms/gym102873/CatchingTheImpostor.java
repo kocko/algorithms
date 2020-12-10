71f60103cbdb6a4b16a9f7c3015182e98aa7288c
kocko <konstantin.yovkov@gmail.com>
2020-12-10 12:47:57 +0200

    Solved Gym102873/A.

---
+++ src/codeforces/gyms/gym102873/CatchingTheImpostor.java
@@ -0,0 +1,76 @@
+package codeforces.gyms.gym102873;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CatchingTheImpostor implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), k = in.ni();
+    boolean[] task = new boolean[n + 1];
+    for (int i = 0; i < k; i++) {
+      int next = in.ni();
+      task[next] = true;
+    }
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+      if (!task[i]) {
+        count++;
+      }
+    }
+    out.println(count == 1 ? "YES" : "NO");
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CatchingTheImpostor instance = new CatchingTheImpostor()) {
+      instance.solve();
+    }
+  }
+}
