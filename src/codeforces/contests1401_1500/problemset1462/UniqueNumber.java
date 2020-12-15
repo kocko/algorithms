5e642f711ebac07804eec676d596f5afcb9010d1
kocko <konstantin.yovkov@gmail.com>
2020-12-15 18:51:11 +0200

    Solved 1462/C.

---
+++ src/codeforces/contests1401_1500/problemset1462/UniqueNumber.java
@@ -0,0 +1,88 @@
+package codeforces.contests1401_1500.problemset1462;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class UniqueNumber implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int x = in.ni();
+      List<Integer> list = new ArrayList<>();
+      int next = 9, sum = 0;
+      while (next > 0) {
+        if (sum + next <= x) {
+          sum += next;
+          list.add(next);
+        }
+        next--;
+      }
+      if (sum == x) {
+        list.sort(Comparator.naturalOrder());
+        for (int value : list) {
+          out.print(value);
+        }
+        out.println();
+      } else {
+        out.println(-1);
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (UniqueNumber instance = new UniqueNumber()) {
+      instance.solve();
+    }
+  }
+}
