248a0e473344c37720db72382d19bd87638baa18
kocko <konstantin.yovkov@gmail.com>
2020-12-15 18:51:30 +0200

    Solved 1462/E1.

---
+++ src/codeforces/contests1401_1500/problemset1462/CloseTuplesEasy.java
@@ -0,0 +1,86 @@
+package codeforces.contests1401_1500.problemset1462;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class CloseTuplesEasy implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      Arrays.sort(x);
+      long result = 0;
+      int left = 0, right = 0;
+      while (right < n) {
+        while (left <= right && x[right] - x[left] > 2) {
+          left++;
+        }
+        int total = right - left;
+        if (total >= 2) {
+          result += (total - 1L) * (total) / 2L;
+        }
+        right++;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CloseTuplesEasy instance = new CloseTuplesEasy()) {
+      instance.solve();
+    }
+  }
+}
