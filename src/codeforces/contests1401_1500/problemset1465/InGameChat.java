0e2d3cd71dbd2d58006df638f51f1a577c5b9731
kocko <konstantin.yovkov@gmail.com>
2020-12-20 18:59:50 +0200

    Solved 1465/A.

---
+++ src/codeforces/contests1401_1500/problemset1465/InGameChat.java
@@ -0,0 +1,75 @@
+package codeforces.contests1401_1500.problemset1465;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class InGameChat implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      char[] x = in.next().toCharArray();
+      int b = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        if (x[i] == ')') b++;
+        else break;
+      }
+      int rem = n - b;
+      out.println(b > rem ? "Yes" : "No");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (InGameChat instance = new InGameChat()) {
+      instance.solve();
+    }
+  }
+}
