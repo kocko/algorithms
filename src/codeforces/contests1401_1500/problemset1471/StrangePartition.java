f8487165fa855b6f6c5343d527d124f2ba7746fc
kocko <konstantin.yovkov@gmail.com>
2021-01-05 19:50:11 +0200

    Solved 1471/A.

---
+++ src/codeforces/contests1401_1500/problemset1471/StrangePartition.java
@@ -0,0 +1,76 @@
+package codeforces.contests1401_1500.problemset1471;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class StrangePartition implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      long x = in.ni();
+      long max = 0, sum = 0;
+      for (int i = 0; i < n; i++) {
+        long next = in.nl();
+        sum += next;
+        max += (next / x) + (next % x != 0 ? 1 : 0);
+      }
+      long min = sum / x + (sum % x != 0 ? 1 : 0);
+      out.println(min + " " + max);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (StrangePartition instance = new StrangePartition()) {
+      instance.solve();
+    }
+  }
+}
