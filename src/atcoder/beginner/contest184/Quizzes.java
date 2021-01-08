77429c18a731e6b942c16a2b513615caac03b95c
kocko <konstantin.yovkov@gmail.com>
2021-01-08 16:08:23 +0200

    Solved ABC184/B.

---
+++ src/atcoder/beginner/contest184/Quizzes.java
@@ -0,0 +1,73 @@
+package atcoder.beginner.contest184;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Quizzes implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int n = in.ni(), x = in.ni();
+    char[] s = in.next().toCharArray();
+    for (char c : s) {
+      if (c == 'o') {
+        x++;
+      } else {
+        x = Math.max(x - 1, 0);
+      }
+    }
+    out.println(x);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Quizzes instance = new Quizzes()) {
+      instance.solve();
+    }
+  }
+}
