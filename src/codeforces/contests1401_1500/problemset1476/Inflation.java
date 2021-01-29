dacb8a55388966a4c45e6c6462f56f1c26e12efa
kocko <konstantin.yovkov@gmail.com>
2021-01-29 21:42:17 +0200

    Solved 1476/B.

---
+++ src/codeforces/contests1401_1500/problemset1476/Inflation.java
@@ -0,0 +1,80 @@
+package codeforces.contests1401_1500.problemset1476;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Inflation implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      long k = in.nl();
+      long[] p = new long[n];
+      for (int i = 0; i < n; i++) {
+        p[i] = in.nl();
+      }
+      long prev = p[0];
+      long result = 0;
+      for (int i = 1; i < n; i++) {
+        long req = (100 * p[i] + k - 1) / k;
+        result = Math.max(result, Math.max(0L, req - prev));
+        prev += p[i];
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Inflation instance = new Inflation()) {
+      instance.solve();
+    }
+  }
+}
