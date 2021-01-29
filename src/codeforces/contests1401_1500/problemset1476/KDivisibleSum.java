714a7b5605c7675a0ffdbb8ea89e2b1a5a17c9b1
kocko <konstantin.yovkov@gmail.com>
2021-01-29 21:42:11 +0200

    Solved 1476/A.

---
+++ src/codeforces/contests1401_1500/problemset1476/KDivisibleSum.java
@@ -0,0 +1,73 @@
+package codeforces.contests1401_1500.problemset1476;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class KDivisibleSum implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), k = in.ni();
+      if (n % k == 0) {
+        out.println(1);
+      } else {
+        int rem = n % k, need = k - rem;
+        out.println(1 + (need / n) + (need % n != 0 ? 1 : 0));
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (KDivisibleSum instance = new KDivisibleSum()) {
+      instance.solve();
+    }
+  }
+}
