381aab1a1743d6271a46ea8f6150fcf89e1923d3
kocko <konstantin.yovkov@gmail.com>
2021-01-31 14:20:55 +0200

    Solved 1466/C.

---
+++ src/codeforces/contests1401_1500/problemset1466/CaninePoetry.java
@@ -0,0 +1,79 @@
+package codeforces.contests1401_1500.problemset1466;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CaninePoetry implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      char[] x = in.next().toCharArray();
+      int n = x.length, result = 0;
+      for (int i = 1; i < n; i++) {
+        if (i >= 2 && x[i] == x[i - 2]) {
+          x[i] = '?';
+          result++;
+        }
+        if (x[i] == x[i - 1] && x[i - 1] != '?') {
+          x[i] = '?';
+          result++;
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CaninePoetry instance = new CaninePoetry()) {
+      instance.solve();
+    }
+  }
+}
