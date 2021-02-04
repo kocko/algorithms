748fa03c5da0c742860e3e765130b52e2baa65ee
kocko <konstantin.yovkov@gmail.com>
2021-02-04 17:31:30 +0200

    Solved 1466/B.

---
+++ src/codeforces/contests1401_1500/problemset1466/LastMinuteEnhancements.java
@@ -0,0 +1,77 @@
+package codeforces.contests1401_1500.problemset1466;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class LastMinuteEnhancements implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      Set<Integer> set = new HashSet<>();
+      for (int i = 0; i < n; i++) {
+        int next = in.ni();
+        if (!set.add(next)) {
+          set.add(next + 1);
+        }
+      }
+      out.println(set.size());
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (LastMinuteEnhancements instance = new LastMinuteEnhancements()) {
+      instance.solve();
+    }
+  }
+}
