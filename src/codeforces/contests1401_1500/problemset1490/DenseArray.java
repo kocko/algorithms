785632ff7dc2146f440401164e2533feed09cfcd
kocko <konstantin.yovkov@gmail.com>
2021-02-16 18:50:01 +0200

    Solved 1490/A.

---
+++ src/codeforces/contests1401_1500/problemset1490/DenseArray.java
@@ -0,0 +1,82 @@
+package codeforces.contests1401_1500.problemset1490;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DenseArray implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), result = 0;
+      int prev = in.ni();
+      for (int i = 1; i < n; i++) {
+        int next = in.ni();
+        while (prev * 2 < next) {
+          result++;
+          prev <<= 1;
+        }
+        int copy = next;
+        while (copy * 2 < prev) {
+          result++;
+          copy <<= 1;
+        }
+        prev = next;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (DenseArray instance = new DenseArray()) {
+      instance.solve();
+    }
+  }
+}
