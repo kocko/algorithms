1a90d352b4380ee7994a6ac8edc6bdfe9fb7e71a
kocko <konstantin.yovkov@gmail.com>
2021-02-16 18:50:12 +0200

    Solved 1490/B.

---
+++ src/codeforces/contests1401_1500/problemset1490/IdenticalRemainders.java
@@ -0,0 +1,76 @@
+package codeforces.contests1401_1500.problemset1490;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class IdenticalRemainders implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      int[] cnt = new int[3];
+      for (int i = 0; i < 3 * n; i++) {
+        cnt[in.ni()]++;
+      }
+      int result = 0;
+      for (int i = 0; i < 3; i++) {
+        result += Math.abs(cnt[i] - n);
+      }
+      out.println(result / 2);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (IdenticalRemainders instance = new IdenticalRemainders()) {
+      instance.solve();
+    }
+  }
+}
