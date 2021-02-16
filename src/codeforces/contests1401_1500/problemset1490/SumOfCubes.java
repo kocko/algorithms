ec9de78c33ed3b9918e33272cddea04827369b6d
kocko <konstantin.yovkov@gmail.com>
2021-02-16 18:50:21 +0200

    Solved 1490/C.

---
+++ src/codeforces/contests1401_1500/problemset1490/SumOfCubes.java
@@ -0,0 +1,78 @@
+package codeforces.contests1401_1500.problemset1490;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class SumOfCubes implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    Set<Long> x = new HashSet<>();
+    for (long i = 1; i <= 10000; i++) {
+      x.add(i * i * i);
+    }
+    while (t-- > 0) {
+      long n = in.nl();
+      boolean possible = false;
+      for (long a : x) {
+        possible |= x.contains(n - a);
+      }
+      out.println(possible ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SumOfCubes instance = new SumOfCubes()) {
+      instance.solve();
+    }
+  }
+}
