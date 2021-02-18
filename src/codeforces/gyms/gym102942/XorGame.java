5f36a86927d799bc3f1911d4c7377d9138e1164e
kocko <konstantin.yovkov@gmail.com>
2021-02-18 18:46:44 +0200

    Solved Gym102942/D.

---
+++ src/codeforces/gyms/gym102942/XorGame.java
@@ -0,0 +1,75 @@
+package codeforces.gyms.gym102942;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class XorGame implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int a = in.ni(), b = in.ni();
+      boolean[] can = new boolean[2];
+      for (int i = 0; (1 << i) <= b; i++) {
+        int bit = 1 << i;
+        int p = a & bit, q = b & bit;
+        can[0] |= p == 0 && q == 0;
+        can[1] |= p > 0 && q > 0;
+      }
+      out.println(can[0] && can[1] ? "Yes" : "No");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (XorGame instance = new XorGame()) {
+      instance.solve();
+    }
+  }
+}
