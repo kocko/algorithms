a9d86fb482cd7c92604034944a73273edecdc93a
kocko <konstantin.yovkov@gmail.com>
2021-02-18 18:47:18 +0200

    Solved 1486/A.

---
+++ src/codeforces/contests1401_1500/problemset1486/ShiftingStacks.java
@@ -0,0 +1,83 @@
+package codeforces.contests1401_1500.problemset1486;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class ShiftingStacks implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      long spare = 0;
+      boolean ok = true;
+      for (int i = 0; i < n; i++) {
+        long next = in.nl();
+        if (next > i) {
+          spare += next - i;
+        } else {
+          long need = i - next;
+          if (spare >= need) {
+            spare -= need;
+          } else {
+            ok = false;
+          }
+        }
+      }
+      out.println(ok ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (ShiftingStacks instance = new ShiftingStacks()) {
+      instance.solve();
+    }
+  }
+}
