bf52e9479b8f480603b3400401dcdd1fb03214ea
kocko <konstantin.yovkov@gmail.com>
2021-02-26 21:59:53 +0200

    Solved 1492/A.

---
+++ src/codeforces/contests1401_1500/problemset1492/ThreeSwimmers.java
@@ -0,0 +1,79 @@
+package codeforces.contests1401_1500.problemset1492;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class ThreeSwimmers implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      long p = in.nl();
+      long result = Long.MAX_VALUE;
+      for (int i = 0; i < 3; i++) {
+        long next = in.nl(), extra = (p % next);
+        if (extra > 0) {
+          result = Math.min(result, next - extra);
+        } else {
+          result = 0;
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (ThreeSwimmers instance = new ThreeSwimmers()) {
+      instance.solve();
+    }
+  }
+}
