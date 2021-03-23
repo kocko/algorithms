f452074d5886cfade5a5f2e0229317ba67696fd5
kocko <konstantin.yovkov@gmail.com>
2021-03-23 18:25:52 +0200

    Solved 1480/A.

---
+++ src/codeforces/contests1401_1500/problemset1480/YetAnotherStringGame.java
@@ -0,0 +1,83 @@
+package codeforces.contests1401_1500.problemset1480;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class YetAnotherStringGame implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      char[] x = in.next().toCharArray();
+      for (int i = 0; i < x.length; i++) {
+        if (i % 2 == 0) {
+          if (x[i] == 'a') {
+            out.print('b');
+          } else {
+            out.print('a');
+          }
+        } else {
+          if (x[i] == 'z') {
+            out.print('y');
+          } else {
+            out.print('z');
+          }
+        }
+      }
+      out.println();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (YetAnotherStringGame instance = new YetAnotherStringGame()) {
+      instance.solve();
+    }
+  }
+}
