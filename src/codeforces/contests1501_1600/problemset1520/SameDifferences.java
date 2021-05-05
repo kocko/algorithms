94a2624b0ba00cda1c4e8ca080a948d5c13ca8d9
kocko <konstantin.yovkov@gmail.com>
2021-05-05 17:46:30 +0300

    Solved 1520/D.

---
+++ src/codeforces/contests1501_1600/problemset1520/SameDifferences.java
@@ -0,0 +1,87 @@
+package codeforces.contests1501_1600.problemset1520;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class SameDifferences implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public SameDifferences() throws FileNotFoundException {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      Map<Long, Integer> map = new HashMap<>();
+      long result = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        long score = x[i] - i;
+        result += map.getOrDefault(score, 0);
+        map.put(score, map.getOrDefault(score, 0) + 1);
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SameDifferences instance = new SameDifferences()) {
+      instance.solve();
+    }
+  }
+}
