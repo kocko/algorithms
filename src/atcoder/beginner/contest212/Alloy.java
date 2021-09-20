33077e59546627e45393668fe247ec1736327578
kocko <konstantin.yovkov@gmail.com>
2021-09-20 15:05:42 +0300

    Solved ABC212/A.

---
+++ src/atcoder/beginner/contest212/Alloy.java
@@ -0,0 +1,86 @@
+package atcoder.beginner.contest212;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class Alloy implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public Alloy() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public Alloy(String input, String output) throws FileNotFoundException {
+    in = new InputReader(new FileInputStream(input));
+    out = new PrintWriter(new FileOutputStream(output));
+  }
+
+  public void solve() {
+    int a = in.ni(), b = in.ni();
+    if (a == 0) {
+      out.println("Silver");
+    } else if (b == 0) {
+      out.println("Gold");
+    } else {
+      out.println("Alloy");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Alloy instance = new Alloy()) {
+      instance.solve();
+    }
+  }
+}
