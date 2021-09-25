54192b5ba913f1b69b382a1eb30ec2dcad41fa3e
kocko <konstantin.yovkov@gmail.com>
2021-09-25 17:07:48 +0300

    Solved ABC069/B.

---
+++ src/atcoder/beginner/contest069/i18n.java
@@ -0,0 +1,81 @@
+package atcoder.beginner.contest069;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class i18n implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public i18n() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public i18n(String input, String output) throws FileNotFoundException {
+    in = new InputReader(new FileInputStream(input));
+    out = new PrintWriter(new FileOutputStream(output));
+  }
+
+  public void solve() {
+    char[] x = in.next().toCharArray();
+    int n = x.length;
+    out.println(x[0] + "" + (n - 2) + "" + x[n - 1]);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (i18n instance = new i18n()) {
+      instance.solve();
+    }
+  }
+}
