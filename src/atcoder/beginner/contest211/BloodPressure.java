87e2bd0a3a66db4ccd65f650073099fd396f4f9f
kocko <konstantin.yovkov@gmail.com>
2021-09-27 09:04:26 +0300

    Solved ABC211/A.

---
+++ src/atcoder/beginner/contest211/BloodPressure.java
@@ -0,0 +1,81 @@
+package atcoder.beginner.contest211;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class BloodPressure implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public BloodPressure() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public BloodPressure(String input, String output) throws FileNotFoundException {
+    in = new InputReader(new FileInputStream(input));
+    out = new PrintWriter(new FileOutputStream(output));
+  }
+
+  public void solve() {
+    int a = in.ni(), b = in.ni();
+    double result = (a - b) / 3. + b;
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BloodPressure instance = new BloodPressure()) {
+      instance.solve();
+    }
+  }
+}
