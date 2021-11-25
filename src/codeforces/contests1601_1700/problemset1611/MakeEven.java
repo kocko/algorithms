3f72f1e81495ab1e5f5a8834d508cfda2aadedaa
kocko <konstantin.yovkov@gmail.com>
2021-11-25 19:12:23 +0200

    Solved 1611/A.

---
+++ src/codeforces/contests1601_1700/problemset1611/MakeEven.java
@@ -0,0 +1,98 @@
+package codeforces.contests1601_1700.problemset1611;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class MakeEven implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public MakeEven() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public MakeEven(String input, String output) throws FileNotFoundException {
+    in = new InputReader(new FileInputStream(input));
+    out = new PrintWriter(new FileOutputStream(output));
+  }
+
+  public void solve() {
+    int t = in.ni();
+    Predicate<char[]> hasEven = x -> {
+      for (char c : x) {
+        if ((c - '0') % 2 == 0) return true;
+      }
+      return false;
+    };
+    while (t-- > 0) {
+      char[] x = in.next().toCharArray();
+      if (hasEven.test(x)) {
+        if ((x[x.length - 1] - '0') % 2 == 0) {
+          out.println(0);
+        } else if ((x[0] - '0') % 2 == 0) {
+          out.println(1);
+        } else {
+          out.println(2);
+        }
+      } else {
+        out.println(-1);
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MakeEven instance = new MakeEven()) {
+      instance.solve();
+    }
+  }
+}
