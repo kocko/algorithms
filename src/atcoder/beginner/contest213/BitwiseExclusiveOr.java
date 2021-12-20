0a4b2c0182a0ce258574a0db4e916d4ee9bf69ef
kocko <konstantin.yovkov@gmail.com>
2021-12-20 18:01:40 +0200

    Solved ABC213/A.

---
+++ src/atcoder/beginner/contest213/BitwiseExclusiveOr.java
@@ -0,0 +1,74 @@
+package atcoder.beginner.contest213;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class BitwiseExclusiveOr implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public BitwiseExclusiveOr() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    out.println(in.ni() ^ in.ni());
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BitwiseExclusiveOr instance = new BitwiseExclusiveOr()) {
+      instance.solve();
+    }
+  }
+}
