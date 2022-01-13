4b84cf5e0a381b02811ea09632232e6669b4dd40
kocko <konstantin.yovkov@gmail.com>
2022-01-13 12:42:16 +0200

    Solved 1625/A.

---
+++ src/codeforces/contests1601_1700/problemset1625/AncientCivilization.java
@@ -0,0 +1,91 @@
+package codeforces.contests1601_1700.problemset1625;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class AncientCivilization implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public AncientCivilization() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), l = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      int result = 0;
+      for (int i = 0; i < l; i++) {
+        int bit = 1 << i, one = 0, zero = 0;
+        for (int j = 0; j < n; j++) {
+          if ((x[j] & bit) == 0) {
+            zero++;
+          } else {
+            one++;
+          }
+        }
+        if (one >= zero) {
+          result |= bit;
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (AncientCivilization instance = new AncientCivilization()) {
+      instance.solve();
+    }
+  }
+}
