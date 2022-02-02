7163c6cab41771d79d7e68c87882e1caa28c87e8
kocko <konstantin.yovkov@gmail.com>
2022-02-02 15:27:50 +0200

    Solved ABC157/A.

---
+++ src/atcoder/beginner/contest157/DuplexPrinting.java
@@ -0,0 +1,70 @@
+package atcoder.beginner.contest157;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DuplexPrinting implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public DuplexPrinting() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int n = in.ni();
+    out.println((n + 1) / 2);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (DuplexPrinting instance = new DuplexPrinting()) {
+      instance.solve();
+    }
+  }
+}
