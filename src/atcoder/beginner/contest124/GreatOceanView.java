0ac01cbcfb4fe435e230f4321617f8796e2ebf43
kocko <konstantin.yovkov@gmail.com>
2022-02-02 17:40:49 +0200

    Solved ABC124/B.

---
+++ src/atcoder/beginner/contest124/GreatOceanView.java
@@ -0,0 +1,87 @@
+package atcoder.beginner.contest124;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class GreatOceanView implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public GreatOceanView() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public GreatOceanView(String input, String output) throws FileNotFoundException {
+    in = new InputReader(new FileInputStream(input));
+    out = new PrintWriter(new FileOutputStream(output));
+  }
+
+  public void solve() {
+    int n = in.ni(), max = 0, result = 0;
+    for (int i = 0; i < n; i++) {
+      int next = in.ni();
+      if (next >= max) {
+        result++;
+        max = next;
+      }
+    }
+    out.println(result);
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (GreatOceanView instance = new GreatOceanView()) {
+      instance.solve();
+    }
+  }
+}
