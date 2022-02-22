a9e1c0f70cf3a219bc3ff402073d767076d084fa
kocko <konstantin.yovkov@gmail.com>
2022-02-22 20:40:10 +0200

    Solved 1644/A.

---
+++ src/codeforces/contests1601_1700/problemset1644/DoorsAndKeys.java
@@ -0,0 +1,81 @@
+package codeforces.contests1601_1700.problemset1644;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class DoorsAndKeys implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public DoorsAndKeys() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      String s = in.next();
+      boolean possible = true;
+      for (char c : "RGB".toCharArray()) {
+        char key = (char) ('a' + (c - 'A'));
+        possible &= s.indexOf(key) < s.indexOf(c);
+      }
+      out.println(possible ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (DoorsAndKeys instance = new DoorsAndKeys()) {
+      instance.solve();
+    }
+  }
+}
