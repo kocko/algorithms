285d58f06a7d457fcddb5793d75b21f5deff330e
kocko <konstantin.yovkov@gmail.com>
2022-02-24 18:07:45 +0200

    Solved 1542/A.

---
+++ src/codeforces/contests1501_1600/problemset1542/OddSet.java
@@ -0,0 +1,84 @@
+package codeforces.contests1501_1600.problemset1542;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class OddSet implements Closeable {
+
+  private final InputReader in;
+  private final PrintWriter out;
+
+  public OddSet() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int t = in.ni();
+    while (t-- > 0) {
+      int n = in.ni(), odd = 0;
+      for (int i = 0; i < 2 * n; i++) {
+        int next = in.ni();
+        if (next % 2 == 1) {
+          odd++;
+        }
+      }
+      out.println(odd == n ? "Yes" : "No");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (OddSet instance = new OddSet()) {
+      instance.solve();
+    }
+  }
+}
