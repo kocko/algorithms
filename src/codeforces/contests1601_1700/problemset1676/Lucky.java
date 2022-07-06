6b3546c7bde4c479361d9a16b915888ba8b26bcc
kocko <konstantin.yovkov@gmail.com>
2022-07-06 18:02:33 +0300

    Solved 1676/A.

---
+++ src/codeforces/contests1601_1700/problemset1676/Lucky.java
@@ -0,0 +1,75 @@
+package codeforces.contests1601_1700.problemset1676;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class Lucky implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      char[] x = in.next().toCharArray();
+      int s = 0;
+      for (int i = 0; i < 6; i++) {
+        if (i < 3) s += x[i];
+        else s -= x[i];
+      }
+      out.println(s == 0 ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Lucky instance = new Lucky()) {
+      instance.solve();
+    }
+  }
+}
