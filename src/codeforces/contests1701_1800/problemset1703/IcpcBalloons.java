d56db5287eb66390e5e86df3a86b70b3b24f13b0
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2022-07-21 17:08:15 +0300

    Solved 1703/B.

---
+++ src/codeforces/contests1701_1800/problemset1703/IcpcBalloons.java
@@ -0,0 +1,82 @@
+package codeforces.contests1701_1800.problemset1703;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class IcpcBalloons implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      char[] x = in.next().toCharArray();
+      boolean[] solved = new boolean[26];
+      int result = 0;
+      for (char c : x) {
+        int idx = c - 'A';
+        if (solved[idx]) {
+          result++;
+        } else {
+          result += 2;
+          solved[idx] = true;
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (IcpcBalloons instance = new IcpcBalloons()) {
+      instance.solve();
+    }
+  }
+}
