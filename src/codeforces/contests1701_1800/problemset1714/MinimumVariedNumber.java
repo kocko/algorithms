1196523a943759ec150a1c6e3af91f0d95af3a05
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2022-08-17 18:09:19 +0300

    Solved 1714/C.

---
+++ src/codeforces/contests1701_1800/problemset1714/MinimumVariedNumber.java
@@ -0,0 +1,82 @@
+package codeforces.contests1701_1800.problemset1714;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class MinimumVariedNumber implements Closeable {
+
+  private InputReader in = new InputReader(System.in);
+  private PrintWriter out = new PrintWriter(System.out);
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int s = in.ni();
+      StringBuilder sb = new StringBuilder();
+      boolean[] used = new boolean[10];
+      while (s > 0) {
+        for (int digit = min(s, 9); digit >= 0; digit--) {
+          if (!used[digit]) {
+            used[digit] = true;
+            sb.append(digit);
+            s -= digit;
+            break;
+          }
+        }
+      }
+      out.println(sb.reverse());
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MinimumVariedNumber instance = new MinimumVariedNumber()) {
+      instance.solve();
+    }
+  }
+}
