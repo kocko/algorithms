b2b0db44b0a9caf931be2e657155b52357c146ea
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-02-08 16:03:48 +0200

    Solved 1742/B.

---
+++ src/codeforces/contests1701_1800/problemset1742/Increasing.java
@@ -0,0 +1,88 @@
+package codeforces.contests1701_1800.problemset1742;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class Increasing implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public Increasing() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      Arrays.sort(x);
+      boolean increasing = true;
+      for (int i = 1; i < n; i++) {
+        if (x[i] == x[i - 1]) increasing = false;
+      }
+      out.println(increasing ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Increasing instance = new Increasing()) {
+      instance.solve();
+    }
+  }
+}
