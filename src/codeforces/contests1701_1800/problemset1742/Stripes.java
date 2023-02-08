cc0c5b98b9e94b067bbec467f648256a0cc2eeff
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-02-08 16:04:00 +0200

    Solved 1742/C.

---
+++ src/codeforces/contests1701_1800/problemset1742/Stripes.java
@@ -0,0 +1,84 @@
+package codeforces.contests1701_1800.problemset1742;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class Stripes implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public Stripes() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      char last = 'B';
+      for (int i = 0; i < 8; i++) {
+        String next = in.next();
+        if ("RRRRRRRR".equals(next)) {
+          last = 'R';
+        }
+      }
+      out.println(last);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (Stripes instance = new Stripes()) {
+      instance.solve();
+    }
+  }
+}
