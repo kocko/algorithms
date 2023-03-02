c230553dabcdbd5f78d64025d1f95d713a89769f
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-03-02 16:20:02 +0200

    Solved 1760/A.

---
+++ src/codeforces/contests1701_1800/problemset1760/MediumNumber.java
@@ -0,0 +1,85 @@
+package codeforces.contests1701_1800.problemset1760;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class MediumNumber implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public MediumNumber() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public MediumNumber(String input, String output) throws FileNotFoundException {
+    in = new InputReader(new FileInputStream(input));
+    out = new PrintWriter(new FileOutputStream(output));
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int[] x = new int[]{in.ni(), in.ni(), in.ni()};
+      Arrays.sort(x);
+      out.println(x[1]);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (MediumNumber instance = new MediumNumber()) {
+      instance.solve();
+    }
+  }
+}
