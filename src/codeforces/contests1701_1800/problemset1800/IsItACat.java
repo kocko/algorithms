a7145ea05c58c2b3764db6b3d96b0788cda648be
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-03-02 18:59:47 +0200

    Solved 1800/A.

---
+++ src/codeforces/contests1701_1800/problemset1800/IsItACat.java
@@ -0,0 +1,88 @@
+package codeforces.contests1701_1800.problemset1800;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class IsItACat implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public IsItACat() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      char[] x = in.next().toLowerCase().toCharArray();
+      boolean cat = true;
+      StringBuilder sb = new StringBuilder();
+      char last = '?';
+      for (char c : x) {
+        if (c != last) {
+          sb.append(c);
+          last = c;
+        }
+      }
+      out.println("meow".equals(sb.toString()) ? "YES" : "NO");
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (IsItACat instance = new IsItACat()) {
+      instance.solve();
+    }
+  }
+}
