c7a2de156942e4403fff06f26e25d22f1f3b87b5
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-04-04 18:10:57 +0300

    Solved 1811/E.

---
+++ src/codeforces/contests1801_1900/problemset1811/LivingSequence.java
@@ -0,0 +1,82 @@
+package codeforces.contests1801_1900.problemset1811;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class LivingSequence implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public LivingSequence() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      long n = in.nl();
+      char[] x = Long.toString(n, 9).toCharArray();
+      for (char c : x) {
+        out.print(c >= '4' ? (char) ((c + 1)) : c);
+      }
+      out.println();
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (LivingSequence instance = new LivingSequence()) {
+      instance.solve();
+    }
+  }
+}
