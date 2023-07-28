eed67067ca5e087ff35d0f897de3854bd23742ff
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-07-28 18:26:42 +0300

    Solved 1843/C.

---
+++ src/codeforces/contests1801_1900/problemset1843/SumInBinaryTree.java
@@ -0,0 +1,82 @@
+package codeforces.contests1801_1900.problemset1843;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class SumInBinaryTree implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public SumInBinaryTree() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      long n = in.nl(), result = 0;
+      while (n > 0) {
+        result += n;
+        n >>= 1;
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SumInBinaryTree instance = new SumInBinaryTree()) {
+      instance.solve();
+    }
+  }
+}
