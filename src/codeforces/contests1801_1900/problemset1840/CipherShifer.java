a5d079b59dbf8548b45a170063b569e618d4e6a5
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-07-28 18:27:04 +0300

    Solved 1840/A.

---
+++ src/codeforces/contests1801_1900/problemset1840/CipherShifer.java
@@ -0,0 +1,89 @@
+package codeforces.contests1801_1900.problemset1840;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class CipherShifer implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public CipherShifer() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni();
+      char[] x = in.next().toCharArray();
+      StringBuilder result = new StringBuilder();
+      int idx = 0;
+      while (idx < n) {
+        result.append(x[idx]);
+        int next = idx + 1;
+        while (next < n && x[next] != x[idx]) {
+          next++;
+        }
+        idx = next + 1;
+      }
+      out.println(result.toString());
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (CipherShifer instance = new CipherShifer()) {
+      instance.solve();
+    }
+  }
+}
