382b6b50538220a26e1af1644e72a4e1796374b6
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-08-03 17:39:45 +0300

    Solved 1840/B.

---
+++ src/codeforces/contests1801_1900/problemset1840/BinaryCafe.java
@@ -0,0 +1,79 @@
+package codeforces.contests1801_1900.problemset1840;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class BinaryCafe implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public BinaryCafe() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n = in.ni(), k = in.ni();
+      k = Math.min(k, 30);
+      out.println(Math.min(n, (1 << k) - 1) + 1);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (BinaryCafe instance = new BinaryCafe()) {
+      instance.solve();
+    }
+  }
+}
