ad2cf0ea549c14b07706caceb3b78bf7d1dc2f4c
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-08-25 18:19:56 +0300

    Solved 1843/A.

---
+++ src/codeforces/contests1801_1900/problemset1843/SashaAndArrayColoring.java
@@ -0,0 +1,90 @@
+package codeforces.contests1801_1900.problemset1843;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class SashaAndArrayColoring implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public SashaAndArrayColoring() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    while (T-- > 0) {
+      int n  = in.ni();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.ni();
+      }
+      Arrays.sort(x);
+      int result = 0;
+      int left = 0, right = n - 1;
+      while (left < right) {
+        result += x[right--];
+        result -= x[left++];
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (SashaAndArrayColoring instance = new SashaAndArrayColoring()) {
+      instance.solve();
+    }
+  }
+}
