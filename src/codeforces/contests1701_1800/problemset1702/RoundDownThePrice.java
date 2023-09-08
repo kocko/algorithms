3b7b1a20a277026a40fb5551cb7e34f470fe45a2
Konstantin Yovkov <konstantin.yovkov@gmail.com>
2023-09-08 17:01:10 +0300

    Solved 1702/A.

---
+++ src/codeforces/contests1701_1800/problemset1702/RoundDownThePrice.java
@@ -0,0 +1,92 @@
+package codeforces.contests1701_1800.problemset1702;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class RoundDownThePrice implements Closeable {
+
+  private InputReader in;
+  private PrintWriter out;
+
+  public RoundDownThePrice() {
+    in = new InputReader(System.in);
+    out = new PrintWriter(System.out);
+  }
+
+  public void solve() {
+    int T = in.ni();
+    int[] x = new int[11];
+    x[0] = 1;
+    for (int i = 1; i <= 10; i++) {
+      x[i] = 10 * x[i - 1];
+    }
+    while (T-- > 0) {
+      int n = in.ni();
+      int result = 0;
+      if (n > 1) {
+        for (int i = 1; i <= 10; i++) {
+          if (x[i] > n) {
+            result = n - x[i - 1];
+            break;
+          }
+        }
+      }
+      out.println(result);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int ni() {
+      return Integer.parseInt(next());
+    }
+
+    public long nl() {
+      return Long.parseLong(next());
+    }
+
+    public void close() throws IOException {
+      reader.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    try (RoundDownThePrice instance = new RoundDownThePrice()) {
+      instance.solve();
+    }
+  }
+}
